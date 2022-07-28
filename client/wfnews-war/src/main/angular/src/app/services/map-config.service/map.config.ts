95abd43cc21d3ea50184ce9cb90293203d602f04
Benjamin Jubb <bjubb@vividsolutions.com>
2022-07-28 16:11:04 -0700

    update styling of map tools, cleanup map config

--- client/wfnews-war/src/main/angular/src/app/services/map-config.service/map.config.ts
+++ client/wfnews-war/src/main/angular/src/app/services/map-config.service/map.config.ts
@@ -13,47 +13,12 @@ export function mapConfig( mapServices: MapServices, serviceStatus: MapServiceSt
             },
             baseMap: 'imagery',
             minZoom: 4,
-        //     displayContext: [
-        //         {
-        //             id: 'layers',
-        //             items: LayerDisplayConfig(mapServices)
-        //         },
-        //         // {
-        //         //     id: 'current-location',
-        //         //     items: [
-        //         //         {
-        //         //             id: 'CurrentLocationTool',
-        //         //             showItem: false,
-        //         //             showLegend: false,
-        //         //             items: [
-        //         //                 {
-        //         //                     id: 'CurrentLocationTool--current-location',
-        //         //                     showItem: false,
-        //         //                     showLegend: false,
-        //         //                 }
-        //         //             ]
-        //         //         }
-        //         //     ]
-        //         // },
-        //         // {
-        //         //     id: 'identify',
-        //         //     items: [
-        //         //         {
-        //         //             id: 'IdentifyListTool',
-        //         //             showItem: false,
-        //         //             showLegend: false,
-        //         //         }
-        //         //     ]
-        //         // }
-        //     ]
         },
         tools: [
-            // {
-            //     type: "location",
-            //     title: "Location",
-            //     position: "toolbar",
-            //     enabled: false
-            // },
+            {
+                type: "location",
+                enabled: false
+            },
 			{
 				type: "layers",
 				enabled: true,
@@ -72,77 +37,10 @@ export function mapConfig( mapServices: MapServices, serviceStatus: MapServiceSt
 				order: 2,
 				display: LayerDisplayConfig( mapServices )
 			},
-
-            // {
-            //     type: "layers",
-            //     enabled: true,
-            //     showTitle: false,
-            //     position: "shortcut-menu",
-            //     glyph: {
-            //         visible: "check_box",
-            //         hidden: "check_box_outline_blank"
-            //     },
-            //     command: {
-            //         allVisibility: true,
-            //         filter: true,
-            //         legend: true,
-            //         // themes: [
-            //         //     {
-            //         //         icon: "layers",
-            //         //         title: "OFTS",
-            //         //         layers: [
-            //         //             "ofts-active",
-            //         //             "ofts-expired",
-            //         //             "whse-human-cultural-economic-emrg-order-and-alert-areas-sp"
-            //         //         ]
-            //         //     },
-            //         //     {
-            //         //         icon: "layers",
-            //         //         title: "Reports\xA0of\xA0Fire",
-            //         //         layers: [
-            //         //             "rofs",
-            //         //             "temporary-rof",
-            //         //             "wf1-transmissionlines",
-            //         //             "wf1-fire-centre-spg",
-            //         //             "wf1-fire-zone-spg",
-            //         //             "bndy-fire-departments",
-            //         //             "whse-admin-boundaries-clab-indian-reserves",
-            //         //             "whse-admin-boundaries-clab-national-parks",
-            //         //             "whse-tantalis-ta-park-ecores-pa-svw",
-            //         //             "wf1-roads",
-            //         //             "whse-basemapping-gba-railway-tracks-sp",
-            //         //             "whse-basemapping-fwa-stream-networks-sp"
-            //         //         ]
-            //         //     },
-            //         //     {
-            //         //         icon: "layers",
-            //         //         title: "Incidents",
-            //         //         layers: [
-            //         //             "incidents",
-            //         //             "temporary-incident",
-            //         //             "in-current-fire-polygons",
-            //         //             "wf1-fire-centre-spg",
-            //         //             "wf1-fire-zone-spg",
-            //         //             "whse-legal-admin-boundaries-fnt-treaty-land-sp",
-            //         //             "whse-admin-boundaries-clab-indian-reserves",
-            //         //             "whse-admin-boundaries-clab-national-parks",
-            //         //             "whse-tantalis-ta-park-ecores-pa-svw",
-            //         //             "whse-tantalis-ta-wildlife-mgmt-areas-svw",
-            //         //             "whse-tantalis-ta-conservancy-areas-svw",
-            //         //             "whse-legal-admin-boundaries-wcl-conservation-lands-sp",
-            //         //             "whse-legal-admin-boundaries-wcl-conservation-areas-ngo-sp",
-            //         //             "wf1-cadastre"
-            //         //         ]
-            //         //     }
-            //         // ]
-            //     },
-            //     legend: true,
-            //     order: 2,
-            // },
             {
                 type: "identify",
                 title: "Identify",
-                enabled: true,
+                enabled: false,
                 showTitle: false,
                 showWidget: false,
                 showPanel: true,
@@ -213,113 +111,24 @@ export function mapConfig( mapServices: MapServices, serviceStatus: MapServiceSt
                 box: true,
                 control: true
             },
+			{
+				type: "baseMaps",
+				enabled: true,
+				showTitle: false,
+				showPanel: false,
+				choices: [
+				],
+				position: "shortcut-menu",
+				mapStyle: {
+					width: "60px",
+					height: "100px"
+				},
+				order: 3
+			},
             {
-                type: "baseMaps",
-                enabled: true,
-                showTitle: false,
-                showPanel: true,
-                icon: "apps",
-                choices: [],
-                mapStyle: {
-                    width: "240px",
-                    height: "80px"
-                },
-                order: 3
+                type: "search",
+                enabled: false
             },
-            // {
-            //     type: "search",
-            //     enabled: 'mobile',
-            //     position: "toolbar",
-            //     showDropdown: true,
-            //     order: 2,
-            //     command: {
-            //         identify: false
-            //     }
-            // },
-            // {
-            //     type: "search-location",
-            //     enabled: false
-            // },
-            // {
-            //     type: "time-dimension",
-            //     enabled: false,
-            //     timeDimensionOptions: {
-            //     }
-            // },
-            // {
-            //     type: "scale",
-            //     enabled: 'desktop',
-            //     showZoom: true,
-            //     order: 2
-            // },
-            // {
-            //     type: "minimap",
-            //     enabled: 'desktop',
-            //     option: {
-            //         minimized: true
-            //     }
-            // },
-            // {
-            //     type: "coordinate",
-            //     enabled: 'desktop',
-            //     order: 3,
-            //     format: 'DDM'
-            // },
-            // {
-            //     type: "measure",
-            //     enabled: 'desktop',
-            //     unit: 'kilometers'
-            // },
-            // {
-            //     type: "current-location",
-            //     enabled: true,
-            //     order: 4
-            // },
-            // {
-            //     type: "bespoke",
-            //     instance: "full-extent",
-            //     title: "Zoom to Full Extent",
-            //     enabled: true,
-            //     position: "actionbar",
-            //     showTitle: false,
-            //     showPanel: false,
-            //     icon: "zoom_out_map",
-            //     order: 5
-            // },
-            // {
-            //     type: "bespoke",
-            //     instance: "zoom-to-box",
-            //     title: "Zoom to Box",
-            //     enabled: 'desktop',
-            //     position: "actionbar",
-            //     showTitle: false,
-            //     showPanel: false,
-            //     icon: "highlight_alt",
-            //     order: 6,
-            //     component: true
-            // },
-            // {
-            //     type: "bespoke",
-            //     instance: "time-dimension",
-            //     title: "Time Dimension",
-            //     enabled: true,
-            //     position: "toolbar",
-            //     showTitle: false,
-            //     showPanel: true,
-            //     icon: "access_time",
-            //     order: 7
-            // },
-            // {
-            //     type: "bespoke",
-            //     instance: "refresh-layers",
-            //     title: "Reload Visible Layers",
-            //     enabled: true,
-            //     position: "toolbar",
-            //     showTitle: false,
-            //     showPanel: false,
-            //     icon: "refresh",
-            //     order: 8
-            // }
         ],
         layers: LayerConfig(mapServices,serviceStatus)
     }
