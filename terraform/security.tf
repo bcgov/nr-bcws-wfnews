c22261db9e958a26ee33973969a6ab4ce939f2fb
VIVIDSOLUTIONS\cpreston <cpreston@vividsolutions.com>
2022-08-09 12:22:52 -0700

    Expanded access permissions

--- terraform/security.tf
+++ terraform/security.tf
@@ -21,22 +21,22 @@ resource "aws_security_group" "wfnews_ecs_tasks" {
     protocol = "tcp"
     from_port = 443
     to_port = 443
-    cidr_blocks = [for s in data.aws_subnet.web_subnet : s.cidr_block]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   #Permit external access for test purposes
   ingress {
-    protocol = "tcp"
-    from_port = 8080
-    to_port = 8080
-    cidr_blocks = [for s in data.aws_subnet.web_subnet : s.cidr_block]
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
     protocol    = "-1"
     from_port   = 0
     to_port     = 0
-    cidr_blocks = [for s in data.aws_subnet.web_subnet : s.cidr_block]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = local.common_tags
