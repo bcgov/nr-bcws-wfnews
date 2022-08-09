8f8e95d9838408a683862737f243851acc3ab6f1
VIVIDSOLUTIONS\cpreston <cpreston@vividsolutions.com>
2022-08-09 10:18:10 -0700

    Forced ecs task and service creation

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,7 @@
 locals {
   common_tags        = var.common_tags
-  create_ecs_service = var.server_image == "" ? 0 : 1
+  #create_ecs_service = var.server_image == "" ? 0 : 1
+  #force image creation
+  create_ecs_service = 1
 }
 
