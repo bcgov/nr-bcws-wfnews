89e4e52043cde1688f412e0fc879d5e9950e4852
vivid-cpreston <97257824+vivid-cpreston@users.noreply.github.com>
2022-08-17 09:27:26 -0700

    Update dns.tf (#14)
    * Update dns.tf
    
    * Merge latest changes (#15)
    
    * kick off these old commit messeges away... (#37)
    
    * first commit
    
    * use incident manager as a kick off template
    
    * update angular core to 12
    
    * update angular material to 12
    
    * manually address the lightGallery update
    
    * angular 12 to 13
    
    * angular 13 material update
    
    * log4j2 update
    
    * take out lightGallery
    
    * delete unused stuff part 1
    
    * delete unused stuff part 2
    
    * delete unused stuff part 3
    
    * delete unused stuff part 4
    
    * delete unused stuff part 5, skip the tokenService
    
    * rename to wfnews
    
    * clean out unused asset files
    
    * clean out unused layer configs
    
    * more clean up
    
    * sync with main
    
    * fix the useless merge differences
    
    * use wfcc new components
    
    * hide side menu
    
    * click footer navigate to related bcgov website
    
    * change spring security core to 5.5.7
    
    * change spring-security-web and spring-security-config to 5.5.7.Release
    
    * change spring-security stuff form  5.5.7.Release to 5.5.7
    
    * update spring-security-config in server pom
    
    * initial commit of hooking up localhost api
    
    * commit some state managements
    
    * save ongoing work
    
    * display active fire cournts
    
    * map layers icon
    
    * get rid of incidents store
    
    * clean up
    
    * left panel
    
    * left panel
    
    * Revert "left panel"
    
    This reverts commit dd996ae444703b73251c6b30171c22404b3748ff.
    
    * cancel the revert of last commit- left panel
    
    * more clean up
    
    * linter error fix 1
    
    * Revert "Merge branch 'linting' into main"
    
    This reverts commit bd401b4ae95c4194c8f320af49f9e20369c13788, reversing
    changes made to 8b7f907b3659823df696aa5c54c42ff75f0ae87e.
    
    * wfnews-365
    
    * add admin-panel component
    
    * add admin-panel component
    
    * admin component
    
    * table almost done
    
    * fireCentre Options
    
    * some styling adjustment
    
    * align page control
    
    * clean up
    
    * WFNEWS-256 Stages of Control accordion
    
    * clean up some console error
    
    * add sort module
    
    * add missing piece of filter
    
    * add in code changes to trigger the code scans?
    
    Co-authored-by: Dylan <dylan.hemsworth@gmail.com>
    
    * Fix config.jsp
    
    Co-authored-by: Lucas Li <35748253+yzlucas@users.noreply.github.com>
    Co-authored-by: Dylan <dylan.hemsworth@gmail.com>
    
    * Update deploy-dev.yml
    
    * Update static.properties
    
    Co-authored-by: Lucas Li <35748253+yzlucas@users.noreply.github.com>
    Co-authored-by: Dylan <dylan.hemsworth@gmail.com>

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -2,14 +2,14 @@ data "aws_route53_zone" "zone" {
     name = "bcwildfireservices.com"
 }
 
-resource "aws_route53_record" "wfnews_server" {
-    zone_id = data.aws_route53_zone.zone.id
-    name = "wfnews-${var.target_env}.bcwildfireservices.com"
-    type = "CNAME"
-    ttl = "300"
-    records = [data.aws_lb.wfnews_main.dns_name]
-
-}
+#resource "aws_route53_record" "wfnews_server" {
+#    zone_id = data.aws_route53_zone.zone.id
+#    name = "wfnews-${var.target_env}.bcwildfireservices.com"
+#    type = "CNAME"
+#    ttl = "300"
+#    records = [data.aws_lb.wfnews_main.dns_name]
+#
+#}
 
 # resource "aws_route53_record" "wfnews_client" {
 #     zone_id = data.aws_route53_zone.zone.id
