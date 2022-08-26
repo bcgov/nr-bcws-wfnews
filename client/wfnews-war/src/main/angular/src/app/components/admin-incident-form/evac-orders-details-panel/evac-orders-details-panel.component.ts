56b889b994dedd2ced08e5bdc70ee4151714c1be
Dylan <dylan.hemsworth@gmail.com>
2022-08-26 13:22:22 -0700

    Non compliance cleanup (#45)
    * Cleanup for WFNEWS-397, 398, 401
    
    * WFNEWS-403

--- client/wfnews-war/src/main/angular/src/app/components/admin-incident-form/evac-orders-details-panel/evac-orders-details-panel.component.ts
+++ client/wfnews-war/src/main/angular/src/app/components/admin-incident-form/evac-orders-details-panel/evac-orders-details-panel.component.ts
@@ -1,8 +1,7 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
-import { AppConfigService } from '@wf1/core-ui';
+import { FormGroup } from '@angular/forms';
 import { EvacOrderOption } from '../../../conversion/models';
+import { AGOLService } from '../../../services/AGOL-service';
 
 @Component({
   selector: 'evac-orders-details-panel',
@@ -15,7 +14,7 @@ export class EvacOrdersDetailsPanel implements OnInit, OnChanges {
 
   evacOrders : EvacOrderOption[] = []
 
-  constructor(private appConfigService: AppConfigService, protected http: HttpClient) {
+  constructor(private agolService: AGOLService) {
   }
 
   ngOnInit() {
@@ -25,25 +24,8 @@ export class EvacOrdersDetailsPanel implements OnInit, OnChanges {
   ngOnChanges(changes: SimpleChanges) {
   }
 
-  getEvacOrders(){
-    // need to support a spatial query here
-    // query by the fire location point with a buffer
-    // also, move to a service class
-
-    let url = this.appConfigService.getConfig().externalAppConfig['AGOLevacOrders'].toString();
-    // append query. Only search for Fire events
-    url += "query?where=EVENT_TYPE='fire'&geometryType=esriGeometryEnvelope&inSR=4326&spatialRel=esriSpatialRelIntersects&units=esriSRUnit_Meter&outFields=*&returnGeometry=false&returnCentroid=false&featureEncoding=esriDefault&outSR=4326&defaultSR=4326&returnIdsOnly=false&returnQueryGeometry=false&cacheHint=false&returnExceededLimitFeatures=true&sqlFormat=none&f=pjson&token="
-
-    if (Object.prototype.hasOwnProperty.call(this.incident, 'geometry')) {
-      // Get the incident geometry, buffer the points by x metres
-      // right now, just moving by 10 points of lat/long
-      url += `&geometry=${this.incident.geometry.x - 5},${this.incident.geometry.y - 5},${this.incident.geometry.x + 5},${this.incident.geometry.y + 5}`
-    }
-
-    let headers = new HttpHeaders();
-    headers.append('Access-Control-Allow-Origin','*');
-    headers.append('Accept','*/*');
-    this.http.get<any>(url,{headers}).subscribe(response => {
+  getEvacOrders () {
+    this.agolService.getEvacOrders(this.incident.geometry).subscribe(response => {
       if (response.features) {
         for (const element of response.features) {
           this.evacOrders.push({
