923fa57d2013d20fb7709faf5abee03bf8192287
Dylan Hemsworth <dylan.hemsworth@gmail.com>
2022-09-08 13:31:36 -0700

    Remove unused wfml content

--- client/wfnews-war/src/main/angular/src/assets/js/wfml-v0.4.0/testbed/config/config-map.js
+++
@@ -1,8 +0,0 @@
-MAP_CONFIG = {
-    center: [ -123, 55 ],
-    zoom: 5,
-    leafOpt: {},
-    baseMaps: [
-        "EsriTopo", "BCBase", "EsriImagery", "EsriStreet", "BCRoad", "EsriLightGray", "EsriHillshade"
-    ]
-};
