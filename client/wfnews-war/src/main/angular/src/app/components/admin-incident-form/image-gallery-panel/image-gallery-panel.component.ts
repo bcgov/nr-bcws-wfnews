416548804a236debdbe0bb2eae739c17d35379c4
Dylan Hemsworth <dylan.hemsworth@gmail.com>
2022-09-12 16:28:52 -0700

    Update image upload description

--- client/wfnews-war/src/main/angular/src/app/components/admin-incident-form/image-gallery-panel/image-gallery-panel.component.ts
+++ client/wfnews-war/src/main/angular/src/app/components/admin-incident-form/image-gallery-panel/image-gallery-panel.component.ts
@@ -140,7 +140,7 @@ export class ImageGalleryPanel extends BaseComponent implements OnInit, OnChange
             (self.statusBar as MatSnackBarRef<TextOnlySnackBar>).instance.data.message = self.uploadStatus
           }
         }).then(doc => {
-          self.attachmentCreator(doc.fileId, doc.filePath, result.file.mimeType, 'Perimeter Map', 'INFO').then(() => {
+          self.attachmentCreator(doc.fileId, doc.filePath, result.file.mimeType, 'Incident Photo', 'INFO').then(() => {
             this.snackbarService.open('File Uploaded Successfully', 'OK', { duration: 0, panelClass: 'snackbar-success' });
           }).catch(err => {
             this.snackbarService.open('Failed to Upload Attachment: ' + JSON.stringify(err.message), 'OK', { duration: 0, panelClass: 'snackbar-error' });
