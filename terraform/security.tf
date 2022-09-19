237460a3a6c218fb33097387d3f0843fc4ffc179
vivid-cpreston <97257824+vivid-cpreston@users.noreply.github.com>
2022-09-19 16:54:56 -0700

    Wfnews 384 (#169)
    * Fixed bad protocol

--- terraform/security.tf
+++ terraform/security.tf
@@ -33,7 +33,7 @@ resource "aws_security_group" "wfnews_ecs_tasks" {
   #necessary for postgres dev work
   #TODO: REMOVE  THIS
   ingress {
-    protocol = "postgresql"
+    protocol = "tcp"
     from_port = 0
     to_port = 5432
     security_groups = [data.aws_security_group.web.id, data.aws_security_group.app.id]
