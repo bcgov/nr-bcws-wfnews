0c00abfc81815c9217e3d52b85925f02edfe2cd2
Dylan <dylan.hemsworth@gmail.com>
2022-09-19 19:09:15 -0700

    WFNEWS-291: Add Wildfire Resources static content (#158)
    * Add Resources static content
    
    * Add URLs for social media links
    
    * Fix spelling

--- client/wfnews-war/src/main/angular/src/app/app.module.ts
+++ client/wfnews-war/src/main/angular/src/app/app.module.ts
@@ -92,6 +92,8 @@ import { ImageGalleryPanel } from './components/admin-incident-form/image-galler
 import { EditImageDialogComponent } from './components/admin-incident-form/image-gallery-panel/edit-image-dialog/edit-image-dialog.component';
 import { UploadImageDialogComponent } from './components/admin-incident-form/image-gallery-panel/upload-image-dialog/upload-image-dialog.component';
 import { ImageCardPanel } from './components/admin-incident-form/image-gallery-panel/image-card-component/image-card-panel.component';
+import { WildfirewResourcesComponent } from './components/wildfire-resources/wf-resources.component';
+import { ResourcePanelComponent } from './components/wildfire-resources/resource-panel/resource-panel.component';
 import { WFStatsComponent } from './components/wf-stats-component/wf-stats.component';
 import { NgxChartsModule } from "@swimlane/ngx-charts";
 import { LightgalleryModule } from 'lightgallery/angular/13';
@@ -136,6 +138,8 @@ export const DATE_FORMATS = {
         EditImageDialogComponent,
         UploadImageDialogComponent,
         ImageCardPanel,
+        WildfirewResourcesComponent,
+        ResourcePanelComponent,
         WFStatsComponent
     ],
     imports: [
---
+++ client/wfnews-war/src/main/angular/src/app/components/wildfire-resources/resource-panel/resource-panel.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+import { ResourcePanel } from '../../../models/ResourcePanel';
+
+@Component({
+  selector: 'resource-panel',
+  templateUrl: './resource-panel.component.html',
+  styleUrls: [ './resource-panel.component.scss' ]
+})
+export class ResourcePanelComponent {
+  @Input() panelInfo: ResourcePanel
+}
