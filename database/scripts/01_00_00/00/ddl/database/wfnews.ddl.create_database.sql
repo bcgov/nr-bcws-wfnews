85b7d58f521c47335f8fa84cc96cf0c5e76721b5
vivid-cpreston <97257824+vivid-cpreston@users.noreply.github.com>
2022-09-19 17:28:33 -0700

    Wfnews 384 (#170)
    * Removed references to nonexistent postgres user

--- database/scripts/01_00_00/00/ddl/database/wfnews.ddl.create_database.sql
+++ database/scripts/01_00_00/00/ddl/database/wfnews.ddl.create_database.sql
@@ -4,7 +4,6 @@
 
 CREATE DATABASE "wildfire"
     WITH 
-    OWNER = postgres
     ENCODING = 'UTF8'
     LC_COLLATE = 'en_US.UTF-8'
     LC_CTYPE = 'en_US.UTF-8'
@@ -15,5 +14,3 @@ COMMENT ON DATABASE "wildfire"
     IS 'wildfire database containing schemas used by wildfire applications and services.';
 
 GRANT TEMPORARY, CONNECT ON DATABASE "wildfire" TO PUBLIC;
-
-GRANT ALL ON DATABASE "wildfire" TO postgres;
