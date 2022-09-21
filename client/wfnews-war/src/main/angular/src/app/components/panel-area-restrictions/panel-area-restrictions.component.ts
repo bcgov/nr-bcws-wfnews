7c9d659c16cc04cacdfc7193efed519fe14f7846
Dylan Hemsworth <dylan.hemsworth@gmail.com>
2022-09-21 15:17:09 -0700

    Force identify on panel click

--- client/wfnews-war/src/main/angular/src/app/components/panel-area-restrictions/panel-area-restrictions.component.ts
+++ client/wfnews-war/src/main/angular/src/app/components/panel-area-restrictions/panel-area-restrictions.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AreaRestrictionsOption } from '../../conversion/models';
 import { AGOLService } from '../../services/AGOL-service';
 import { MapConfigService } from '../../services/map-config.service';
+import L from 'leaflet';
 
 @Component({
     selector: 'panel-area-restrictions',
@@ -22,7 +23,29 @@ export class PanelAreaRestrictionsComponent implements OnInit {
   zoomToArea (area) {
     this.mapConfigService.getMapConfig().then(() => {
       const SMK = window['SMK'];
-      SMK.MAP[1].$viewer.panToFeature(window['turf'].point([area.centroid.x, area.centroid.y]), 10)
+      const viewer = SMK.MAP[1].$viewer;
+      viewer.panToFeature(window['turf'].point([area.centroid.x, area.centroid.y]), 10)
+
+      const map = viewer.map;
+      let latlngPoint = new L.LatLng(area.centroid.y, area.centroid.x);
+      map.fireEvent('click', {
+        latlng: latlngPoint,
+        layerPoint: map.latLngToLayerPoint(latlngPoint),
+        containerPoint: map.latLngToContainerPoint(latlngPoint)
+      });
+
+      setTimeout(() => {
+        for (const set in viewer.identified.featureSet) {
+          if (Object.prototype.hasOwnProperty.call(viewer.identified.featureSet, set)) {
+            const feature = viewer.identified.featureSet[set]
+            if (feature.type === 'Feature' && feature.layerId === 'area-restrictions') {
+              viewer.identified.pick(feature.id)
+              break;
+            }
+          }
+        }
+      }, 1000)
+      viewer.identified.remove('weather-stations')
     })
   }
 
