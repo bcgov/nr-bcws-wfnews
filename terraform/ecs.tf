20dc6b0fe7d72e22bd374ffdec4221ba25479b41
VIVIDSOLUTIONS\cpreston <cpreston@vividsolutions.com>
2022-09-23 15:31:23 -0700

    fixed unintended variable change

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -132,7 +132,7 @@ resource "aws_ecs_task_definition" "wfnews_server" {
           value = var.WFNEWS_AGOL_QUERY_URL
         },
         {
-          name  = "DB_URL",
+          name  = "WFNEWS_DB_URL",
           value = "jdbc:postgresql://${aws_db_instance.wfnews_pgsqlDB.endpoint}/${aws_db_instance.wfnews_pgsqlDB.name}"
         },
         {
