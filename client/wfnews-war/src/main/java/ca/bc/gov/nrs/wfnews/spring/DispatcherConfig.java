a8c09f09ab61240ca8c41a4b8cbb447dfe2156cb
Dylan <dylan.hemsworth@gmail.com>
2022-09-29 19:06:02 -0700

    Add dispatch config, setUseSuffixPatternMatch true (#249)

---
+++ client/wfnews-war/src/main/java/ca/bc/gov/nrs/wfnews/spring/DispatcherConfig.java
@@ -0,0 +1,50 @@
+package ca.bc.gov.nrs.wfnews.spring;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("ca.bc.gov.nrs.wfnews.web.controller")
+public class DispatcherConfig implements WebMvcConfigurer {
+  @Override
+  public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+    configurer.enable();
+  }
+
+  @Override
+  public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+    configurer.favorPathExtension(false).favorParameter(true);
+  }
+
+  @Override
+  public void configurePathMatch(PathMatchConfigurer configurer) {
+    configurer.setUseSuffixPatternMatch(true);
+  }
+
+  @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    registry.addResourceHandler("/js/**").addResourceLocations("/js/").setCachePeriod(31556926);
+  }
+
+  @Bean
+  public ViewResolver internalResourceViewResolver() {
+    InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+
+    viewResolver.setViewClass(JstlView.class);
+    viewResolver.setPrefix("/");
+    viewResolver.setSuffix(".jsp");
+
+    return viewResolver;
+  }
+}
