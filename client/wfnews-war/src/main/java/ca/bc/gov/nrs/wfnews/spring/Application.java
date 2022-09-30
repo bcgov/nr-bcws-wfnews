f06715cba64a9149fc3e53407b3092066a59c58a
Dylan <dylan.hemsworth@gmail.com>
2022-09-30 11:25:32 -0700

    Update urlrewrite (#254)

--- client/wfnews-war/src/main/java/ca/bc/gov/nrs/wfnews/spring/Application.java
+++ client/wfnews-war/src/main/java/ca/bc/gov/nrs/wfnews/spring/Application.java
@@ -3,10 +3,14 @@ package ca.bc.gov.nrs.wfnews.spring;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import org.springframework.web.context.support.GenericWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
+import org.tuckey.web.filters.urlrewrite.UrlRewriteFilter;
+
 import javax.servlet.ServletContext;
 import javax.servlet.ServletRegistration;
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
+import java.util.EnumSet;
 
 public class Application implements WebApplicationInitializer {
     private void newAppServlet(ServletContext servletContext) {
@@ -27,5 +31,10 @@ public class Application implements WebApplicationInitializer {
         rootContext.register(AppConfig.class);
         container.addListener(new ContextLoaderListener(rootContext));
         newAppServlet(container);
+
+        // Used to allow direct url links to angular routes - otherwise you will get 404 as they don't exist as actual server resources
+        FilterRegistration.Dynamic urlReWrite = container.addFilter("UrlRewriteFilter", new UrlRewriteFilter());
+        EnumSet<DispatcherType> urlReWriteDispatcherTypes = EnumSet.of(DispatcherType.REQUEST, DispatcherType.FORWARD);
+        urlReWrite.addMappingForUrlPatterns(urlReWriteDispatcherTypes, true, "/*");
     }
 }
