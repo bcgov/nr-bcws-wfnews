6f5682fa63939f548c763c3bcaf8349ea21abd6c
vivid-cpreston <97257824+vivid-cpreston@users.noreply.github.com>
2022-09-30 17:46:32 -0700

    Update cloudfront.tf

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -46,6 +46,7 @@ resource "aws_cloudfront_distribution" "wfnews_geofencing_client" {
 
     forwarded_values {
       query_string = true
+      headers      = ["*"]
 
       cookies {
         forward = "all"
