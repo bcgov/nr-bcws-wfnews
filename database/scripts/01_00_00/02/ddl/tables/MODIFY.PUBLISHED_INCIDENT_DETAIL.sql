c20551d351a0c56754ee2b690f6b9537bc0686a3
Dylan Hemsworth <dylan.hemsworth@gmail.com>
2022-10-08 08:49:53 -0700

    Add trigger and function to auto create geometry on insert/update

--- database/scripts/01_00_00/02/ddl/tables/MODIFY.PUBLISHED_INCIDENT_DETAIL.sql
+++ database/scripts/01_00_00/02/ddl/tables/MODIFY.PUBLISHED_INCIDENT_DETAIL.sql
@@ -17,4 +17,30 @@ CREATE INDEX "pubincdtl_point_geom_idx" ON "wfnews"."published_incident_detail" 
 
 --Create index based on geography of point
 CREATE INDEX "pubincdtl_point_geog_idx" ON "wfnews"."published_incident_detail" USING gist(geography("geometry"))
-; 
+;
+
+--Create Function for trigger that sets the point_geom and point_geom_buffered columns
+CREATE OR REPLACE FUNCTION set_incident_geometries()
+  RETURNS trigger 
+  LANGUAGE PLPGSQL AS
+$$
+BEGIN
+  IF ((NEW.latitude IS NOT NULL) AND (NEW.longitude IS NOT NULL)) THEN
+    --Set Point Geometery
+    New.geometry = ST_SetSRID(ST_MakePoint(NEW.longitude::double precision, NEW.latitude::double precision), 4326);
+  END IF;
+  RETURN NEW;
+END;
+$$;
+
+--Create trigger that will set geometry columns before notification record is inserted or updated.
+DROP TRIGGER IF EXISTS "incident_changes" ON "wfnews"."published_incident_detail";
+
+CREATE TRIGGER "incident_changes"
+  BEFORE INSERT OR UPDATE 
+  OF longitude, latitude
+  ON wfnews.published_incident_detail
+  FOR EACH ROW
+  EXECUTE PROCEDURE set_incident_geometries();
+  
+ANALYZE notification;
