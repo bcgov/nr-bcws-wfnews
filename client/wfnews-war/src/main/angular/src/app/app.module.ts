4ea26f000f53bbe5fb69f1c58b3a7cbcd2739fcc
Dylan <dylan.hemsworth@gmail.com>
2022-10-08 09:36:41 -0700

    WFNEWS-310: Add local authority panel (#289)

--- client/wfnews-war/src/main/angular/src/app/app.module.ts
+++ client/wfnews-war/src/main/angular/src/app/app.module.ts
@@ -104,6 +104,7 @@ import { ErrorPageComponent } from './components/error-page/error-page.component
 import { WildFiresListComponentDesktop } from './components/wildfires-list/wildfires-list.component.desktop';
 import { WildfiresListContainerDesktop } from './containers/wildfiresList/wildfiresList-container.component.desktop';
 import { PublishedIncidentService } from './services/published-incident-service';
+import { PanelLocalAuthorityComponent } from './components/panel-local-authority/panel-local-authority.component';
 
 // Copied from im-external.module  TODO: consolidate in one place
 export const DATE_FORMATS = {
@@ -152,8 +153,8 @@ export const DATE_FORMATS = {
         PanelRoadConditionsComponent,
         ErrorPageComponent,
         WildFiresListComponentDesktop,
-        WildfiresListContainerDesktop
-
+        WildfiresListContainerDesktop,
+        PanelLocalAuthorityComponent
     ],
     imports: [
         MatSortModule,
---
+++ client/wfnews-war/src/main/angular/src/app/components/panel-local-authority/panel-local-authority.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'panel-local-authority',
+    templateUrl: './panel-local-authority.component.html',
+    styleUrls: ['./panel-local-authority.component.scss'],
+})
+export class PanelLocalAuthorityComponent {
+
+}
