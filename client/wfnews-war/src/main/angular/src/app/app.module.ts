7103958d982d06cabff33fb86755c4d03233f280
gpineiro-vivid <111597077+gpineiro-vivid@users.noreply.github.com>
2022-10-11 18:44:03 -0300

    WFNEWS-133 Contact information widget (#287)
    * WFNEWS-133 Contact information widget
    
    * removed duplicate justify content css property
    
    * noopener fix
    
    * Config.jsp changes in place with new added contact information. contact-widget renamed to sticky-widget, keeping things generics
    
    Co-authored-by: Gabril Pineiro <gpineiro@VividSolutions.com>
    Co-authored-by: Dylan <dylan.hemsworth@gmail.com>

--- client/wfnews-war/src/main/angular/src/app/app.module.ts
+++ client/wfnews-war/src/main/angular/src/app/app.module.ts
@@ -104,6 +104,8 @@ import { ErrorPageComponent } from './components/error-page/error-page.component
 import { WildFiresListComponentDesktop } from './components/wildfires-list/wildfires-list.component.desktop';
 import { WildfiresListContainerDesktop } from './containers/wildfiresList/wildfiresList-container.component.desktop';
 import { PublishedIncidentService } from './services/published-incident-service';
+import { StickyWidgetComponent } from './components/sticky-widget/sticky-widget.component';
+import { ContactWidgetDialogComponent } from './components/sticky-widget/contact-widget-dialog/contact-widget-dialog.component';
 import { PanelLocalAuthorityComponent } from './components/panel-local-authority/panel-local-authority.component';
 
 // Copied from im-external.module  TODO: consolidate in one place
@@ -145,6 +147,8 @@ export const DATE_FORMATS = {
         ImageGalleryPanel,
         EditImageDialogComponent,
         UploadImageDialogComponent,
+        StickyWidgetComponent,
+        ContactWidgetDialogComponent,
         ImageCardPanel,
         WFStatsComponent,
         WeatherPanelComponent,
