2e06ae96a517fe0ca8559f268209fac1bfd42e36
Lucas Li <yzlucas@outlook.com>
2022-10-12 16:55:38 -0700

    calculate distance

--- client/wfnews-war/src/main/angular/src/app/components/panel-wildfire-stage-of-control/panel-wildfire-stage-of-control.component.ts
+++ client/wfnews-war/src/main/angular/src/app/components/panel-wildfire-stage-of-control/panel-wildfire-stage-of-control.component.ts
@@ -14,6 +14,8 @@ export class PanelWildfireStageOfControlComponent extends CollectionComponent im
     activeWildfiresInd = true;
     wildfiresOfNoteInd = false;
     wildfiresOutInd = false;
+    currentLat;
+    currentLong;
 
     initModels() {
         this.model = new PanelWildfireStageOfControlComponentModel(this.sanitizer);
@@ -40,6 +42,18 @@ export class PanelWildfireStageOfControlComponent extends CollectionComponent im
         this.baseRoute = this.router.url;
         this.componentId = LOAD_WILDFIRES_COMPONENT_ID
         this.doSearch();
+        this.useMyCurrentLocation();
+    }
+
+    async useMyCurrentLocation() {
+        this.searchText = undefined;
+    
+        const location = await this.commonUtilityService.getCurrentLocationPromise()
+        console.log(location)
+        if( location ){
+            this.currentLat = location.coords.latitude;
+            this.currentLong = location.coords.longitude;
+        }
     }
 
     doSearch() {
@@ -77,6 +91,32 @@ export class PanelWildfireStageOfControlComponent extends CollectionComponent im
             default:
                 break;
           }
+    }
+
+    calculateDistance(lat: string, long: string){
+        if ((Number(lat) && Number(long)) && (this.currentLat && this.currentLong)) {
+            let result = (this.distanceHelper(Number(lat), Number(this.currentLat), Number(long), Number(this.currentLong)))
+            return result.toString() + ' KM'
+        }
+    }
+    
+    distanceHelper(lat1, lat2, lon1, lon2) {
+        lon1 =  lon1 * Math.PI / 180;
+        lon2 = lon2 * Math.PI / 180;
+        lat1 = lat1 * Math.PI / 180;
+        lat2 = lat2 * Math.PI / 180;
+   
+        let dlon = lon2 - lon1;
+        let dlat = lat2 - lat1;
+        let a = Math.pow(Math.sin(dlat / 2), 2)
+                 + Math.cos(lat1) * Math.cos(lat2)
+                 * Math.pow(Math.sin(dlon / 2),2);
+               
+        let c = 2 * Math.asin(Math.sqrt(a));
+        let r = 6371;
+   
+        // calculate the result
+        return((c * r).toFixed(2));
     }
 
 }
