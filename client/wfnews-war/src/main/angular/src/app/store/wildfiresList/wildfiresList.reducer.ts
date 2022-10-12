f860071627d28d0904954f88f7a1bf906bdf7de0
Lucas Li <yzlucas@outlook.com>
2022-10-12 14:29:38 -0700

    sonar clean up

--- client/wfnews-war/src/main/angular/src/app/store/wildfiresList/wildfiresList.reducer.ts
+++ client/wfnews-war/src/main/angular/src/app/store/wildfiresList/wildfiresList.reducer.ts
@@ -1,6 +1,6 @@
 import { Action } from "@ngrx/store";
 import { SearchWildfiresSuccessAction, SEARCH_WILDFIRES_SUCCESS } from "./wildfiresList.action";
-import { getDefaultWildfiresListState, SEARCH_WILDFIRES_COMPONENT_ID, WildfiresState } from "./wildfiresList.stats";
+import { getDefaultWildfiresListState, WildfiresState } from "./wildfiresList.stats";
 
 export function wildfiresListReducer (state: WildfiresState = getDefaultWildfiresListState(), action: Action): WildfiresState {
   if (action.type === SEARCH_WILDFIRES_SUCCESS) {
