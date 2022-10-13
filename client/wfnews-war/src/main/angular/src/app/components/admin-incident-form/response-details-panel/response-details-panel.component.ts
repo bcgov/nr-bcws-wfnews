d155d6e6475b6d4963f17bd981d9f9d5aaf6bf63
Gabril Pineiro <gpineiro@VividSolutions.com>
2022-10-13 11:46:33 -0700

    WFNEWS-510 polish

--- client/wfnews-war/src/main/angular/src/app/components/admin-incident-form/response-details-panel/response-details-panel.component.ts
+++ client/wfnews-war/src/main/angular/src/app/components/admin-incident-form/response-details-panel/response-details-panel.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 
 @Component({
@@ -10,22 +10,73 @@ export class ResponseDetailsPanel implements OnInit {
   @Input() public readonly formGroup: FormGroup;
   @Input() public incident;
 
-  responseDisclaimer: string = "The BC Wildfire Service relies on thousands of people each year to respond to wildfires. This includes firefighters, air crew, equipment operators, and support staff. For more information on resources assigned to this incident, please contact the information officer listed for this incident.";
-  crewsComments: string = "There are currently XX Initial Attack and XX Unit Crews responding to this wildfire.";
-  aviationComments: string = "There are currently XX helicopters and XX airtankers responding to this wildfire.";
-  incidentManagementComments: string = "An Incident Management Team has been assigned to this wildfire.";
-  heavyEquipmentComments: string = "There are currently XX pieces of heavy equipment responding to this wildfire.";
-  structureProtectionComments: string = "There are currently XX structure protection units responding to this incident.";
-
+  @ViewChild("initialAttackCrews") initialAttackCrews: ElementRef;
+  @ViewChild("unitCrews") unitCrews: ElementRef;
+  @ViewChild("helicopters") helicopters: ElementRef;
+  @ViewChild("airtankers") airtankers: ElementRef;
+  @ViewChild("pieces") pieces: ElementRef;
+  @ViewChild("structure") structure: ElementRef;
+ 
+  responseDisclaimer: string = `The BC Wildfire Service relies on thousands of people each year to respond to wildfires. This includes firefighters, air crew, equipment operators, and support staff. For more information on resources assigned to this incident, please contact the information officer listed for this incident.`;
+  incidentManagementComments: string = `An Incident Management Team has been assigned to this wildfire.`;
+  
   ngOnInit() {
     this.formGroup.controls['responseComments'].setValue(this.responseDisclaimer);
   }
 
-  onCheckedOption(event, controlName) {
+  onWildfireCrewsChecked(event) {
     if(event.checked) {
-      this.formGroup.controls[controlName].setValue(this[controlName]);
+      this.formGroup.controls["crewsComments"].setValue(this.crewCommentsValue(this.initialAttackCrews.nativeElement.value, this.unitCrews.nativeElement.value));
     } else {
-      this.formGroup.controls[controlName].setValue("");
+      this.formGroup.controls["crewsComments"].setValue("");
     }
+  }
+
+  onAviationChecked(event) {
+    if(event.checked) {
+      this.formGroup.controls["aviationComments"].setValue(this.aviationCommentsValue(this.helicopters.nativeElement.value, this.airtankers.nativeElement.value));
+    } else {
+      this.formGroup.controls["aviationComments"].setValue("");
+    }
+  }
+
+  onIncidentManagementTeamsChecked(event) {
+    if(event.checked) {
+      this.formGroup.controls["incidentManagementComments"].setValue(this.incidentManagementComments);
+    } else {
+      this.formGroup.controls["incidentManagementComments"].setValue("");
+    }
+  }
+
+  onHeavyEquipmentChecked(event) {
+    if(event.checked) {
+      this.formGroup.controls["heavyEquipmentComments"].setValue(this.heavyEquipmentCommentsValue(this.pieces.nativeElement.value));
+    } else {
+      this.formGroup.controls["heavyEquipmentComments"].setValue("");
+    }
+  }
+
+  onStructureProtectionChecked(event) {
+    if(event.checked) {
+      this.formGroup.controls["structureProtectionComments"].setValue(this.structureProtectionCommentsValue(this.structure.nativeElement.value));
+    } else {
+      this.formGroup.controls["structureProtectionComments"].setValue("");
+    }
+  }
+  
+  crewCommentsValue(initialAttack, unityCrews) {
+    return `There are currently ${initialAttack} Initial Attack and ${unityCrews} Unit Crews responding to this wildfire.`;
+  }
+
+  aviationCommentsValue(helicopters, airtankers) {
+    return `There are currently ${helicopters} helicopters and ${airtankers} airtankers responding to this wildfire.`;
+  }
+
+  heavyEquipmentCommentsValue(pieces) {
+    return `There are currently ${pieces} pieces of heavy equipment responding to this wildfire.`;
+  }
+
+  structureProtectionCommentsValue(structure) {
+    return `There are currently ${structure} structure protection units responding to this incident.`;
   }
 }
