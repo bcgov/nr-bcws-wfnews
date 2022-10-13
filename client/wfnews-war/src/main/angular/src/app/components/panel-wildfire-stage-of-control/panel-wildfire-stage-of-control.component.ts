66667263ba93dd4848fbc5dd842904342ff60a37
Dylan Hemsworth <dylan.hemsworth@gmail.com>
2022-10-13 16:05:21 -0700

    Add map event to wildfire list

--- client/wfnews-war/src/main/angular/src/app/components/panel-wildfire-stage-of-control/panel-wildfire-stage-of-control.component.ts
+++ client/wfnews-war/src/main/angular/src/app/components/panel-wildfire-stage-of-control/panel-wildfire-stage-of-control.component.ts
@@ -1,15 +1,32 @@
-import { AfterViewInit, Directive, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Overlay } from '@angular/cdk/overlay';
+import { HttpClient } from '@angular/common/http';
+import { AfterViewInit, ChangeDetectorRef, ComponentFactoryResolver, ComponentRef, Directive, Injector, Input, NgZone, OnChanges, OnInit, SimpleChanges, Type, ViewChild, ViewContainerRef } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { DomSanitizer } from '@angular/platform-browser';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { TokenService, AppConfigService } from '@wf1/core-ui';
 import * as moment from 'moment';
 import { PagedCollection } from '../../conversion/models';
+import { ApplicationStateService } from '../../services/application-state.service';
+import { CommonUtilityService } from '../../services/common-utility.service';
+import { MapConfigService } from '../../services/map-config.service';
 import { haversineDistance } from '../../services/wfnews-map.service/util';
+import { RootState } from '../../store';
 import { searchWildfires } from '../../store/wildfiresList/wildfiresList.action';
 import { LOAD_WILDFIRES_COMPONENT_ID } from '../../store/wildfiresList/wildfiresList.stats';
 import { CollectionComponent } from '../common/base-collection/collection.component';
+import { IncidentIdentifyPanelComponent } from '../incident-identify-panel/incident-identify-panel.component';
 import { PanelWildfireStageOfControlComponentModel } from './panel-wildfire-stage-of-control.component.model';
+import L from 'leaflet';
+
+const delay = t => new Promise(resolve => setTimeout(resolve, t));
 
 @Directive()
 export class PanelWildfireStageOfControlComponent extends CollectionComponent implements OnChanges, AfterViewInit, OnInit  {
-
+    @ViewChild('listIdentifyContainer', { read: ViewContainerRef }) listIdentifyContainer: ViewContainerRef;
     @Input() collection: PagedCollection
 
     activeWildfiresInd = true;
@@ -17,6 +34,18 @@ export class PanelWildfireStageOfControlComponent extends CollectionComponent im
     wildfiresOutInd = false;
     currentLat: number;
     currentLong: number;
+
+    private zone: NgZone;
+    private componentRef: ComponentRef<any>;
+    private mapPanTimer;
+    private mapPanProgressBar;
+    private progressValue = 0;
+    private lastPanned = '';
+
+    constructor (protected injector: Injector, protected componentFactoryResolver: ComponentFactoryResolver, private mapConfigService: MapConfigService, router: Router, route: ActivatedRoute, sanitizer: DomSanitizer, store: Store<RootState>, fb: FormBuilder, dialog: MatDialog, applicationStateService: ApplicationStateService, tokenService: TokenService, snackbarService: MatSnackBar, overlay: Overlay, cdr: ChangeDetectorRef, appConfigService: AppConfigService, http: HttpClient, commonUtilityService?: CommonUtilityService) {
+      super(router, route, sanitizer, store, fb, dialog, applicationStateService, tokenService, snackbarService, overlay, cdr, appConfigService, http, commonUtilityService);
+      this.zone = this.injector.get(NgZone)
+    }
 
     initModels() {
         this.model = new PanelWildfireStageOfControlComponentModel(this.sanitizer);
@@ -104,5 +133,83 @@ export class PanelWildfireStageOfControlComponent extends CollectionComponent im
 
     public Number(value: string): number {
       return Number(value);
+    }
+
+    onPanelMouseEnter (incident: any) {
+      // pan to incident location
+      const self = this;
+      const progressBarElement = document.getElementById(incident.incidentName);
+
+      this.mapPanTimer = setTimeout(() => {
+        const SMK = window['SMK'];
+        const viewer = SMK.MAP[1].$viewer;
+        const map = viewer.map;
+        viewer.panToFeature(window['turf'].point([incident.longitude + 1, incident.latitude]), map._zoom);
+        progressBarElement.style.display = 'none';
+        self.progressValue = 0;
+        self.lastPanned = incident.incidentName
+      }, 500);
+
+      if (this.lastPanned !== incident.incidentName) {
+        progressBarElement.style.display = 'block';
+        progressBarElement.style.opacity = '0.0';
+        this.mapPanProgressBar = setInterval(() => {
+          progressBarElement.style.opacity = (Number(progressBarElement.style.opacity) + 0.1).toString()
+          self.progressValue += 5;
+          self.cdr.detectChanges();
+        }, 1);
+      }
+    }
+
+    onPanelMouseExit(incident: any) {
+      if (this.mapPanTimer) {
+        clearTimeout(this.mapPanTimer);
+        this.mapPanTimer = null;
+      }
+      if (this.mapPanProgressBar) {
+        clearInterval(this.mapPanProgressBar);
+        this.mapPanProgressBar = null;
+      }
+      document.getElementById(incident.incidentName).style.display = 'none';
+      document.getElementById(incident.incidentName).style.opacity = '0.0';
+      this.progressValue = 0;
+    }
+
+    openPreview (incident: any) {
+      this.onPanelMouseEnter(incident);
+
+      incident.incident_number_label = incident.incidentNumberLabel;
+      const self = this;
+      this.zone.run(function () {
+        let compRef = self.makeComponent(IncidentIdentifyPanelComponent);
+        (compRef.instance as any).setIncident(incident, {});
+        const panel = (document.getElementsByClassName('identify-panel').item(0) as HTMLElement);
+        panel.appendChild(compRef.location.nativeElement);
+        self.cdr.detectChanges();
+        // display the panel
+        (document.getElementsByClassName('identify-panel').item(0) as HTMLElement).style.display = 'block';
+        // apply a slight debounce to clear the identify and destroy the panel
+        setTimeout(() => {
+          const identifyPanel = (document.getElementsByClassName('smk-panel').item(0) as HTMLElement)
+          if (identifyPanel) {
+            identifyPanel.remove();
+          }
+          // use smk.$viewer.identified to reset the form?
+        }, 200);
+      })
+    }
+
+    makeComponent<C>(component: Type<C>): ComponentRef<C> {
+      if (this.componentRef) {
+          this.componentRef.destroy()
+      }
+
+      if (this.listIdentifyContainer) {
+        this.listIdentifyContainer.clear();
+      }
+
+      this.componentRef = this.listIdentifyContainer.createComponent(this.componentFactoryResolver.resolveComponentFactory(component))
+
+      return this.componentRef
     }
 }
