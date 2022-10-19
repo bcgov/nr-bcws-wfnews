a3a753066b147196dabb90504d809070f90cdb72
vivid-cpreston <97257824+vivid-cpreston@users.noreply.github.com>
2022-10-19 15:27:12 -0700

    Add apisix (#66)
    * Removed for loop

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -325,9 +325,25 @@ resource "aws_ecs_task_definition" "wfnews_apisix" {
       portMappings = [
         {
           protocol = "tcp"
-          container_port = each.value
-          hostPort = each.value
+          container_port = var.apisix_ports[0]
+          hostPort = var.apisix_ports[0]
+        },
+        {
+          protocol = "tcp"
+          container_port = var.apisix_ports[1]
+          hostPort = var.apisix_ports[1]
+        },
+        {
+          protocol = "tcp"
+          container_port = var.apisix_ports[2]
+          hostPort = var.apisix_ports[2]
+        },
+        {
+          protocol = "tcp"
+          container_port = var.apisix_ports[3]
+          hostPort = var.apisix_ports[3]
         }
+
       ]
       environment = [
 
