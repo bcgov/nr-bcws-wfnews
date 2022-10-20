dea3f1deb0f50e4844a72fe71057d2dbe5f2ad0a
gpineiro-vivid <111597077+gpineiro-vivid@users.noreply.github.com>
2022-10-20 18:01:30 -0300

    Feature/wfnews 557 (#332)
    * WFNEWS-557
    
    * WFNEWS-557 learn more link
    
    Co-authored-by: Gabril Pineiro <gpineiro@VividSolutions.com>
    Co-authored-by: Dylan <dylan.hemsworth@gmail.com>

--- client/wfnews-war/src/main/angular/src/app/components/admin-incident-form/incident-details-panel/incident-details-panel.component.ts
+++ client/wfnews-war/src/main/angular/src/app/components/admin-incident-form/incident-details-panel/incident-details-panel.component.ts
@@ -18,13 +18,16 @@ export class IncidentDetailsPanel {
   sizeTypeOptions: OptionDisclaimer[] = [
     {optionValue: "Mapped", disclaimer:"Fire size based on the last known mapped size in hectares."},
     {optionValue: "Estimated", disclaimer:"Fire size based on the last known estimated size in hectares."},
-  ]
+  ];
+
   causeOptions: OptionDisclaimer[] = [
-    {optionValue: "General", disclaimer:"The cause of a wildfire is determined by professional investigations. The BC Wildfire Service employs Fire Origin and Cause Specialists to conduct investigations in accordance with international standards. Learn more about what causes wildfires here."},
+    {optionValue: "General", disclaimer:"The cause of a wildfire is determined by professional investigations. The BC Wildfire Service employs Fire Origin and Cause Specialists to conduct investigations in accordance with international standards."},
     {optionValue: "Lightning", disclaimer:"When lightning strikes an object it can release enough heat to ignite a tree or other fuels."},
     {optionValue: "Human", disclaimer:"Humans start wildfires in several ways, either by accident or intentionally."},
     {optionValue: "Under Investigation", disclaimer:"Wildfire investigations often take time and can be very complex. Investigations may be carried out by one or more agencies, including the BC Wildfire Service, the Compliance and Enforcement Branch, the RCMP, or other law enforcement agencies, and may be cross jurisdictional."}
-  ]
+  ];
+
+  showLearnMoreText:boolean;
 
   setSizeTypeDisclaimer (value) {
     this.formGroup.controls['sizeComments'].setValue(this.sizeTypeOptions.find(c=>c.optionValue===value).disclaimer);
@@ -32,5 +35,10 @@ export class IncidentDetailsPanel {
 
   setCauseDisclaimer (value) {
     this.formGroup.controls['causeComments'].setValue(this.causeOptions.find(c=>c.optionValue===value).disclaimer);
+
+    if(value == this.causeOptions[0].optionValue)
+      this.showLearnMoreText = true;
+    else
+    this.showLearnMoreText = false;
   }
 }
