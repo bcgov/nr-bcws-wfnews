a8436c9ce7875eeaa935773bea7b3c25084dd3ca
VIVIDSOLUTIONS\cpreston <cpreston@vividsolutions.com>
2022-10-25 11:58:05 -0700

    Added missing security group info

--- terraform/efs.tf
+++ terraform/efs.tf
@@ -8,6 +8,7 @@ resource "aws_efs_mount_target" "wfnews_efs_mount" {
   for_each = module.network.aws_subnet_ids.data.ids
   subnet_id = each.value
   file_system_id = aws_efs_file_system.wfnews_efs.id
+  security_groups = [data.aws_security_group.data.id]
 }
 
 resource "aws_efs_file_system_policy" "policy" {
