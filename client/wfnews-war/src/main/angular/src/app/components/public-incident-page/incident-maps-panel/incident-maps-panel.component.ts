9de89e5d491646c157ffa12656baacc61eca5353
Gabril Pineiro <gpineiro@VividSolutions.com>
2022-10-24 21:27:33 -0700

    WFNEWS-295

--- client/wfnews-war/src/main/angular/src/app/components/public-incident-page/incident-maps-panel/incident-maps-panel.component.ts
+++ client/wfnews-war/src/main/angular/src/app/components/public-incident-page/incident-maps-panel/incident-maps-panel.component.ts
@@ -1,4 +1,12 @@
-import { Component, ChangeDetectionStrategy, Input } from "@angular/core";
+import { Component, ChangeDetectionStrategy, Input, OnInit } from "@angular/core";
+import { MatSnackBar } from "@angular/material/snack-bar";
+import { HttpClient, HttpEventType, HttpRequest, HttpResponse } from "@angular/common/http";
+
+export class DownloadableMap {
+  name :string;
+  link :string;
+  date :string;
+}
 
 @Component({
   selector: 'incident-maps-panel',
@@ -6,6 +14,73 @@ import { Component, ChangeDetectionStrategy, Input } from "@angular/core";
   styleUrls: ['./incident-maps-panel.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class IncidentMapsPanel {
-  @Input() public incident
+export class IncidentMapsPanel implements OnInit {
+  @Input() public incident;
+
+  downloadableMaps: DownloadableMap[];
+
+  constructor(private snackbarService: MatSnackBar,
+              private httpClient: HttpClient) {
+    
+  }
+
+  ngOnInit() {
+    this.downloadableMaps = [
+      {
+        name: "Akokli Creek Active Evacuation Areas", link: "https://cdn.filestackcontent.com/wcrjf9qPTCKXV3hMXDwK", date: "June 24, 2022"
+      },
+      {
+        name: "Akokli Creek Active Evacuation Areas", link: "https://cdn.filestackcontent.com/wcrjf9qPTCKXV3hMXDwK", date: "June 24, 2022"
+      },
+      {
+        name: "Akokli Creek Active Evacuation Areas", link: "https://cdn.filestackcontent.com/wcrjf9qPTCKXV3hMXDwK", date: "June 24, 2022"
+      },
+      {
+        name: "Akokli Creek Active Evacuation Areas", link: "https://cdn.filestackcontent.com/wcrjf9qPTCKXV3hMXDwK", date: "June 24, 2022"
+      },
+      {
+        name: "Akokli Creek Active Evacuation Areas", link: "https://cdn.filestackcontent.com/wcrjf9qPTCKXV3hMXDwK", date: "June 24, 2022"
+      }
+    ];
+
+    this.loadMaps();
+  }
+
+  loadMaps() {
+   
+  }
+
+  downloadMap(mapLink) {
+    // Need to replace this code with real call to API to get valid attachments/maps
+    const url = mapLink;
+    let request = this.httpClient.request( new HttpRequest( 'GET', url, {
+        reportProgress: true,
+        responseType: 'blob'
+    }));
+
+    request.subscribe(
+      ( ev ) => {
+          if ( ev.type == HttpEventType.Sent ) {
+            this.snackbarService.open('Generating PDF. Please wait...', 'Close', { duration: 2000, panelClass: 'snackbar-warning' });
+          }
+          else if ( ev instanceof HttpResponse ) {
+            this.downloadFile(ev as HttpResponse<Blob>);
+            this.snackbarService.open('PDF downloaded successfully.', 'Close', { duration: 500000, panelClass: 'snackbar-success' });
+          }
+      },
+      ( err ) => console.log(err)
+    )
+  }
+
+  downloadFile (data: HttpResponse<Blob>) {
+    const downloadedFile = new Blob([data.body], { type: data.body.type });
+    const a = document.createElement('a');
+    a.setAttribute('style', 'display:none;');
+    document.body.appendChild(a);
+    a.download = "test.pdf";
+    a.href = URL.createObjectURL(downloadedFile);
+    a.target = '_blank';
+    a.click();
+    document.body.removeChild(a);
+  }
 }
