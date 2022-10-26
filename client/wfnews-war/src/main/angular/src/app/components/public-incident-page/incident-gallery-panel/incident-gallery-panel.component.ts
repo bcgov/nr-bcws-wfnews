3e66ab556aee3f2fa333ce20cedafe8aed4f48d1
Gabril Pineiro <gpineiro@VividSolutions.com>
2022-10-25 20:35:19 -0700

    WFNEWS-294

--- client/wfnews-war/src/main/angular/src/app/components/public-incident-page/incident-gallery-panel/incident-gallery-panel.component.ts
+++ client/wfnews-war/src/main/angular/src/app/components/public-incident-page/incident-gallery-panel/incident-gallery-panel.component.ts
@@ -1,4 +1,4 @@
-import { Component, ChangeDetectionStrategy, Input } from "@angular/core";
+import { Component, ChangeDetectionStrategy, Input, OnInit } from "@angular/core";
 
 @Component({
   selector: 'incident-gallery-panel',
@@ -6,6 +6,23 @@ import { Component, ChangeDetectionStrategy, Input } from "@angular/core";
   styleUrls: ['./incident-gallery-panel.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class IncidentGalleryPanel {
-  @Input() public incident
+export class IncidentGalleryPanel implements OnInit {
+  @Input() public incident;
+
+  currentMediaType: string;
+  mediaTypeOptions: string[] = ["All","Images","Videos"];
+  imagesAndVideosStub: any[];
+
+  ngOnInit() {
+    this.loadPage();
+  }
+
+  loadPage() {
+    this.imagesAndVideosStub = [
+      {title:"Lorem Ipsum dolor sit amet", uploadedDate:"June 23, 2022", fileName: "Incident-V25425-image.jpg", type:"image", href: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnSMUVjNA_9dKxmNk3_ZQNOejzZtiix1EkkQ&usqp=CAU" },
+      {title:"Lorem Ipsum dolor sit amet", uploadedDate:"June 23, 2022", fileName: "Incident-V25425-image.jpg", type:"image", href: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnSMUVjNA_9dKxmNk3_ZQNOejzZtiix1EkkQ&usqp=CAU" },
+      {title:"Lorem Ipsum dolor sit amet", uploadedDate:"June 23, 2022", fileName: "Incident-V25425-image.jpg", type:"image", href: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnSMUVjNA_9dKxmNk3_ZQNOejzZtiix1EkkQ&usqp=CAU" },
+      {title:"Lorem Ipsum dolor sit amet", uploadedDate:"June 23, 2022", fileName: "Incident-V25425-image.jpg", type:"video", href: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnSMUVjNA_9dKxmNk3_ZQNOejzZtiix1EkkQ&usqp=CAU" },
+    ];
+  }
 }
