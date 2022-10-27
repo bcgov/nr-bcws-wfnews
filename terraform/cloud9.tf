f7136ba0d13eb3c8411c6542139f87406e857d57
vivid-cpreston <97257824+vivid-cpreston@users.noreply.github.com>
2022-10-27 12:21:33 -0700

    Add apisix (#115)
    * fixed typo

--- terraform/cloud9.tf
+++ terraform/cloud9.tf
@@ -1,5 +1,5 @@
-resource aws_cloud9_environment_ec2 wfnews-cloud9 {
-    name = wfnews-cloud9-${var.target_env}
+resource aws_cloud9_environment_ec2 wfnews_cloud9 {
+    name = "wfnews-cloud9-${var.target_env}"
     instance_type = "t2.micro"
     connection_type = "CONNECT_SSM"
     image_id = "amazonlinux-2-x86_64"
