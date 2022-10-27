4f4ac2f0c41aef4aced58722d5edb0ece2301e3c
Gabril Pineiro <gpineiro@VividSolutions.com>
2022-10-27 12:39:22 -0700

    WFNEWS-294

--- client/wfnews-war/src/main/angular/src/app/components/public-incident-page/incident-gallery-panel/incident-gallery-panel.component.ts
+++ client/wfnews-war/src/main/angular/src/app/components/public-incident-page/incident-gallery-panel/incident-gallery-panel.component.ts
@@ -1,4 +1,6 @@
 import { Component, ChangeDetectionStrategy, Input, OnInit } from "@angular/core";
+import lgZoom from 'lightgallery/plugins/zoom';
+import { BeforeSlideDetail } from 'lightgallery/lg-events';
 
 @Component({
   selector: 'incident-gallery-panel',
@@ -13,6 +15,16 @@ export class IncidentGalleryPanel implements OnInit {
   mediaTypeOptions: string[] = ["All","Images","Videos"];
   imagesAndVideosStub: any[];
 
+  settings = {
+    counter: false,
+    plugins: [lgZoom]
+  };
+
+  onBeforeSlide = (detail: BeforeSlideDetail): void => {
+    const { index, prevIndex } = detail;
+    console.log(index, prevIndex);
+  };
+
   ngOnInit() {
     this.loadPage();
   }
@@ -22,7 +34,7 @@ export class IncidentGalleryPanel implements OnInit {
       {title:"Lorem Ipsum dolor sit amet", uploadedDate:"June 23, 2022", fileName: "Incident-V25425-image.jpg", type:"image", href: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnSMUVjNA_9dKxmNk3_ZQNOejzZtiix1EkkQ&usqp=CAU" },
       {title:"Lorem Ipsum dolor sit amet", uploadedDate:"June 23, 2022", fileName: "Incident-V25425-image.jpg", type:"image", href: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnSMUVjNA_9dKxmNk3_ZQNOejzZtiix1EkkQ&usqp=CAU" },
       {title:"Lorem Ipsum dolor sit amet", uploadedDate:"June 23, 2022", fileName: "Incident-V25425-image.jpg", type:"image", href: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnSMUVjNA_9dKxmNk3_ZQNOejzZtiix1EkkQ&usqp=CAU" },
-      {title:"Lorem Ipsum dolor sit amet", uploadedDate:"June 23, 2022", fileName: "Incident-V25425-image.jpg", type:"video", href: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnSMUVjNA_9dKxmNk3_ZQNOejzZtiix1EkkQ&usqp=CAU" },
+      {title:"Lorem Ipsum dolor sit amet", uploadedDate:"June 23, 2022", fileName: "Incident-V25425-image.jpg", type:"video", href: "http://www.youtube.com/embed/M7lc1UVf-VE?enablejsapi=1&origin=http://example.com" },
     ];
   }
 }
