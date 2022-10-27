3b04ce9be0bba25c46299046c5a71fd02af381ca
VIVIDSOLUTIONS\cpreston <cpreston@vividsolutions.com>
2022-10-27 13:57:30 -0700

    Syntax fix

--- terraform/cloud9.tf
+++ terraform/cloud9.tf
@@ -3,5 +3,5 @@ resource aws_cloud9_environment_ec2 wfnews_cloud9 {
     instance_type = "t2.micro"
     connection_type = "CONNECT_SSM"
     image_id = "amazonlinux-2-x86_64"
-    subnet_id = module.network.aws_subnet_ids.app.ids[0]
+    subnet_id = tolist(module.network.aws_subnet_ids.app.ids)[0]
 }
