7ff1e6c7ab2563f3ea04c0caf6ca5ee729065c34
Dylan Hemsworth <dylan.hemsworth@gmail.com>
2022-10-28 08:55:23 -0700

    Update booleans

--- client/wfnews-war/src/main/angular/src/app/components/incident-identify-panel/incident-identify-panel.component.ts
+++ client/wfnews-war/src/main/angular/src/app/components/incident-identify-panel/incident-identify-panel.component.ts
@@ -69,14 +69,14 @@ export class IncidentIdentifyPanelComponent {
       this.incident.discoveryDate = new Date(this.incident.discoveryDate).toLocaleTimeString("en-US", options);
       this.incident.declaredOutDate = this.incident.declaredOutDate ? new Date(this.incident.declaredOutDate).toLocaleTimeString("en-US", options) : new Date(this.incident.discoveryDate).toLocaleTimeString("en-US", options);
       this.incident.lastUpdatedTimestamp = new Date(this.incident.lastUpdatedTimestamp).toLocaleTimeString("en-US", options);
-      this.incident.fireOfNoteInd = this.incident.fireOfNoteInd.trim().toUpperCase() === 'T' || this.incident.fireOfNoteInd.trim().toUpperCase() === '1';
+      this.incident.fireOfNoteInd = this.incident.fireOfNoteInd;
 
       // set T/1 to True, otherwise False
-      this.incident.heavyEquipmentResourcesInd = this.incident.heavyEquipmentResourcesInd.trim().toUpperCase() === 'T' || this.incident.heavyEquipmentResourcesInd.trim().toUpperCase() === '1';
-      this.incident.incidentMgmtCrewRsrcInd = this.incident.incidentMgmtCrewRsrcInd.trim().toUpperCase() === 'T' || this.incident.incidentMgmtCrewRsrcInd.trim().toUpperCase() === '1';
-      this.incident.structureProtectionRsrcInd = this.incident.structureProtectionRsrcInd.trim().toUpperCase() === 'T' || this.incident.structureProtectionRsrcInd.trim().toUpperCase() === '1';
-      this.incident.wildfireAviationResourceInd = this.incident.wildfireAviationResourceInd.trim().toUpperCase() === 'T' || this.incident.wildfireAviationResourceInd.trim().toUpperCase() === '1';
-      this.incident.wildfireCrewResourcesInd = this.incident.wildfireCrewResourcesInd.trim().toUpperCase() === 'T' || this.incident.wildfireCrewResourcesInd.trim().toUpperCase() === '1';
+      this.incident.heavyEquipmentResourcesInd = this.incident.heavyEquipmentResourcesInd;
+      this.incident.incidentMgmtCrewRsrcInd = this.incident.incidentMgmtCrewRsrcInd;
+      this.incident.structureProtectionRsrcInd = this.incident.structureProtectionRsrcInd;
+      this.incident.wildfireAviationResourceInd = this.incident.wildfireAviationResourceInd;
+      this.incident.wildfireCrewResourcesInd = this.incident.wildfireCrewResourcesInd
 
       // load evac orders nearby
       this.getEvacOrders();
