46772452452d60f3045b1fc15acdf72cb684ec50
VIVIDSOLUTIONS\cpreston <cpreston@vividsolutions.com>
2022-11-01 11:02:27 -0700

    Added additional security group to permit HTTP traffic

--- terraform/security.tf
+++ terraform/security.tf
@@ -73,3 +73,11 @@ resource "aws_security_group" "wfnews_efs_access" {
     self = true
   }
 }
+
+resource "aws_security_group_rule" "example" {
+  type              = "ingress"
+  from_port         = 80
+  protocol          = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id =  data.aws_security_group.Web_sg.id
+}
