908b2e1b8fb8420f34abbde7147854dc6161109c
VIVIDSOLUTIONS\cpreston <cpreston@vividsolutions.com>
2022-11-01 15:48:39 -0700

    Reverted non-ssl usage

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -348,7 +348,7 @@ resource "aws_ecs_task_definition" "wfnews_apisix" {
       environment = [
         {
           name: "ETCD_URL",
-          value: "http://wfnews-etcd.${var.license_plate}-${var.target_env}.nimbus.cloud.gov.bc.ca"
+          value: "https://wfnews-etcd.${var.license_plate}-${var.target_env}.nimbus.cloud.gov.bc.ca"
         },
         {
           name: "API_KEY",
@@ -412,11 +412,11 @@ resource "aws_ecs_task_definition" "wfnews_etcd" {
         },
         {
           name: "ETCD_ADVERTISE_CLIENT_URLS",
-          value: "http://0.0.0.0:2379, https://0.0.0.0:2379, http://wfnews-etcd.${var.license_plate}-${var.target_env}.nimbus.cloud.gov.bc.ca:80"
+          value: "http://0.0.0.0:2379, https://0.0.0.0:2379, https://wfnews-etcd.${var.license_plate}-${var.target_env}.nimbus.cloud.gov.bc.ca:443"
         },
         {
           name: "ETCD_LISTEN_CLIENT_URLS",
-          value: "http://0.0.0.0:2379"
+          value: "https://0.0.0.0:2379"
         },
         {
           name: "ETCD_ROOT_PASSWORD",
@@ -495,7 +495,7 @@ resource "aws_ecs_task_definition" "wfnews_apisix_gui" {
       environment = [
         {
           name: "ETCD_URL",
-          value: "http://wfnews-etcd.${var.license_plate}-${var.target_env}.nimbus.cloud.gov.bc.ca"
+          value: "https://wfnews-etcd.${var.license_plate}-${var.target_env}.nimbus.cloud.gov.bc.ca"
         },
         {
           name: "API_KEY",
