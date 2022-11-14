cc7607f08d0c87379a4b52e663f4e6bec6cd6c90
vivid-cpreston <97257824+vivid-cpreston@users.noreply.github.com>
2022-11-13 21:12:16 -0800

    Apisix standalone (#172)
    * Fixed reference to removed resource

--- terraform/security.tf
+++ terraform/security.tf
@@ -84,5 +84,5 @@ resource "aws_security_group" "wfnews_efs_access" {
 # }
 
 resource "aws_iam_access_key" "wfnews_iam_access_key" {
-  user = data.aws_caller_identity.current.name
+  user = element(split("/", data.aws_caller_identity.current.arn), length(split("/", data.aws_caller_identity.current.arn) - 1))
 }
