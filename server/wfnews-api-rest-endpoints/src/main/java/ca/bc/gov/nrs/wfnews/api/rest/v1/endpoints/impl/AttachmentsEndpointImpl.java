1cdd0293c3bf8fa9d8fa079d3bf58487a6232ff5
VIVIDSOLUTIONS\cpreston <cpreston@vividsolutions.com>
2022-11-18 12:08:20 -0800

    Update SNS and S3 access to use

--- server/wfnews-api-rest-endpoints/src/main/java/ca/bc/gov/nrs/wfnews/api/rest/v1/endpoints/impl/AttachmentsEndpointImpl.java
+++ server/wfnews-api-rest-endpoints/src/main/java/ca/bc/gov/nrs/wfnews/api/rest/v1/endpoints/impl/AttachmentsEndpointImpl.java
@@ -24,6 +24,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.core.io.ByteArrayResource;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.InstanceProfileCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.EnvironmentVariableCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.core.ResponseInputStream;
@@ -171,12 +172,12 @@ public class AttachmentsEndpointImpl extends BaseEndpointsImpl implements Attach
 		logRequest();
 
 	  	Region region =  Region.of(attachmentsAwsConfig.getRegionName());
-		AwsBasicCredentials awsBasicCredentials = AwsBasicCredentials.create(
-				attachmentsAwsConfig.getAccessKeyId(),
-				attachmentsAwsConfig.getSecretAccessKey());
+		// AwsBasicCredentials awsBasicCredentials = AwsBasicCredentials.create(
+		// 		attachmentsAwsConfig.getAccessKeyId(),
+		// 		attachmentsAwsConfig.getSecretAccessKey());
 
 		S3Client s3Client = S3Client.builder()
-				.credentialsProvider(StaticCredentialsProvider.create(awsBasicCredentials))
+				.credentialsProvider(new InstanceProfileCredentialsProvider(false))
 				.region(region)
 				.build();
 
