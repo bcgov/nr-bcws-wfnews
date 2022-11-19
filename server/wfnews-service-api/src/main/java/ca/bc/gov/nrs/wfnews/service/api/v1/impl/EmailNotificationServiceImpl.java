5a23d308659fbdb61785247e6067398d7ebb45be
VIVIDSOLUTIONS\cpreston <cpreston@vividsolutions.com>
2022-11-18 16:01:21 -0800

    removed duplicate import

--- server/wfnews-service-api/src/main/java/ca/bc/gov/nrs/wfnews/service/api/v1/impl/EmailNotificationServiceImpl.java
+++ server/wfnews-service-api/src/main/java/ca/bc/gov/nrs/wfnews/service/api/v1/impl/EmailNotificationServiceImpl.java
@@ -23,7 +23,6 @@ import ca.bc.gov.nrs.wfnews.service.api.v1.config.EmailNotificationConfig;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.InstanceProfileCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
-import software.amazon.awssdk.auth.credentials.InstanceProfileCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.sns.SnsClient;
 import software.amazon.awssdk.services.sns.model.MessageAttributeValue;
