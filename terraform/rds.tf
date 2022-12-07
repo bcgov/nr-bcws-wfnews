57fe08c957f67557181b6e93df7518555f0d39b4
vivid-cpreston <97257824+vivid-cpreston@users.noreply.github.com>
2022-12-07 15:44:22 -0800

    Update rds.tf

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -15,7 +15,9 @@ resource "aws_db_instance" "wfnews_pgsqlDB"{
     allocated_storage = 10
     username = var.WFNEWS_USERNAME
     password = var.db_pass
+    publicly_accessible = false
     skip_final_snapshot = true
+    storage_encrypted = false
     vpc_security_group_ids = [data.aws_security_group.app.id, aws_security_group.wfnews_ecs_tasks.id]
     tags = local.common_tags
     db_subnet_group_name = aws_db_subnet_group.wfnews_db_subnet_group.name
