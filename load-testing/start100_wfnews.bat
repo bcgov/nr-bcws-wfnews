d5e31c8ddbc6d6e1f46ccaee515bf2c91388afc8
VIVIDSOLUTIONS\cpreston <cpreston@vividsolutions.com>
2022-12-22 09:13:01 -0800

    Updated variable names for clarity

--- load-testing/start100_wfnews.bat
+++ load-testing/start100_wfnews.bat
@@ -3,4 +3,4 @@ set JMETER_HOME=..\apache-jmeter-5.4.3
 set JMETER_WFNEWS_THREADS=100
 set TEST_ENVIRONMENT=%1
 set ITERATIONS=-1
-start cmd.exe /c %JMETER_HOME%\bin\jmeter -n -Jthreads=%JMETER_WFNEWS_THREADS% -Jurlsuffix=%TEST_ENVIRONMENT% -Jiterations=%ITERATIONS% -t .\WFNEWS_Load_Test_2.jmx -l WFNEWS_Test_2_Results.jtl
+start cmd.exe /c %JMETER_HOME%\bin\jmeter -n -Jthreads=%JMETER_WFNEWS_THREADS% -Jenvironment=%TEST_ENVIRONMENT% -Jiterations=%ITERATIONS% -t .\WFNEWS_Load_Test_2.jmx -l WFNEWS_Test_2_Results.jtl
