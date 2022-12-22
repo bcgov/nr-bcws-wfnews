db1b5906a2a57ef312ee9e5fab6d810d5fddb897
VIVIDSOLUTIONS\cpreston <cpreston@vividsolutions.com>
2022-12-22 09:06:41 -0800

    added additional load test, matching scenario

--- load-testing/start100_wfnews.bat
+++ load-testing/start100_wfnews.bat
@@ -1,7 +1,6 @@
 @echo on
 set JMETER_HOME=..\apache-jmeter-5.4.3
 set JMETER_WFNEWS_THREADS=100
-set JMETER_THREADS=50
 set TEST_ENVIRONMENT=%1
 set ITERATIONS=-1
-start cmd.exe /c %JMETER_HOME%\bin\jmeter -n -Jthreads=%JMETER_WFNEWS_THREADS% -Jhost=%TEST_ENVIRONMENT% -Jiterations=%ITERATIONS% -t .\WFNEWS_Load_Test.jmx -l WFNEWS_Test_Results.jtl
+start cmd.exe /c %JMETER_HOME%\bin\jmeter -n -Jthreads=%JMETER_WFNEWS_THREADS% -Jurlsuffix=%TEST_ENVIRONMENT% -Jiterations=%ITERATIONS% -t .\WFNEWS_Load_Test_2.jmx -l WFNEWS_Test_2_Results.jtl
