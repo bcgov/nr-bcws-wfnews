71eef58811f7160cdb7c16a8f3afc1ad8357c48b
Lucas Li <yzlucas@outlook.com>
2023-01-11 14:32:08 -0800

    dont display comments if resources counts < 1

--- client/wfnews-war/src/main/angular/src/app/components/admin-incident-form/response-details-panel/response-details-panel.component.ts
+++ client/wfnews-war/src/main/angular/src/app/components/admin-incident-form/response-details-panel/response-details-panel.component.ts
@@ -65,19 +65,27 @@ export class ResponseDetailsPanel implements OnInit {
   }
   
   crewCommentsValue(initialAttack, unityCrews) {
-    return `There are currently ${initialAttack} Initial Attack and ${unityCrews} Unit Crews responding to this wildfire.`;
+    if (initialAttack || unityCrews){
+      return `There are currently ${initialAttack && initialAttack > 0 ? initialAttack : 0} Initial Attack and ${unityCrews && unityCrews > 0 ? unityCrews : 0} Unit Crews responding to this wildfire.`;
+    }
   }
 
   aviationCommentsValue(helicopters, airtankers) {
-    return `There are currently ${helicopters} helicopters and ${airtankers} airtankers responding to this wildfire.`;
+    if (helicopters || airtankers) {
+      return `There are currently ${helicopters && helicopters > 0 ? helicopters : 0} helicopters and ${airtankers && airtankers > 0 ? airtankers : 0} airtankers responding to this wildfire.`;
+    }
   }
 
   heavyEquipmentCommentsValue(pieces) {
-    return `There are currently ${pieces} pieces of heavy equipment responding to this wildfire.`;
+    if (pieces && pieces > 0) {
+      return `There are currently ${pieces} pieces of heavy equipment responding to this wildfire.`;
+    }
   }
 
   structureProtectionCommentsValue(structure) {
-    return `There are currently ${structure} structure protection units responding to this incident.`;
+    if (structure && structure > 0) {
+      return `There are currently ${structure} structure protection units responding to this incident.`;
+    }
   }
 
   crewsValueChange() {
