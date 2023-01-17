3f683b5fdb5f892c455b1f2a7961bb39c86bfcdf
Lucas Li <35748253+yzlucas@users.noreply.github.com>
2023-01-17 11:16:44 -0800

    footer is behaving weird and there is a also a future ticket to remove it, (#544)
    * remove the footer
    
    * remove the footer
    
    * call Agol for area restions data only after incident got fetched
    
    * put footer back until decision being made
    
    * clean up
    
    * remove unused import
    
    * change to use ViewChild
    
    * delete console.log
    
    Co-authored-by: Dylan <dylan.hemsworth@gmail.com>

--- client/wfnews-war/src/main/angular/src/app/components/admin-incident-form/area-restrictions-details-panel/area-restrictions-details-panel.component.ts
+++ client/wfnews-war/src/main/angular/src/app/components/admin-incident-form/area-restrictions-details-panel/area-restrictions-details-panel.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit} from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { AreaRestrictionsOption } from '../../../conversion/models';
 import { AGOLService } from '../../../services/AGOL-service';
@@ -6,7 +6,8 @@ import { AGOLService } from '../../../services/AGOL-service';
 @Component({
   selector: 'area-restrictions-details-panel',
   templateUrl: './area-restrictions-details-panel.component.html',
-  styleUrls: ['./area-restrictions-details-panel.component.scss']
+  styleUrls: ['./area-restrictions-details-panel.component.scss'],
+
 })
 export class AreaRestrictionsDetailsPanel implements OnInit {
   @Input() public readonly formGroup: FormGroup
@@ -18,23 +19,26 @@ export class AreaRestrictionsDetailsPanel implements OnInit {
   }
 
   ngOnInit() {
-    this.getAreaRestrictions();
+    this.getAreaRestrictions()
   }
 
+
   getAreaRestrictions () {
-    this.agolService.getAreaRestrictions(this.incident.geometry).subscribe(response => {
-      if (response.features) {
-        for (const element of response.features) {
-          this.areaRestrictions.push({
-            protRsSysID: element.attributes.PROT_RA_SYSID,
-            name: element.attributes.NAME,
-            accessStatusEffectiveDate: element.attributes.ACCESS_STATUS_EFFECTIVE_DATE,
-            fireCentre: element.attributes.FIRE_CENTRE_NAME,
-            fireZone: element.attributes.FIRE_ZONE_NAME,
-            bulletinUrl: element.attributes.BULLETIN_URL
-          })
+    if(this.incident.geometry.x && this.incident.geometry.y){
+      this.agolService.getAreaRestrictions({ x: this.incident.geometry.x, y: this.incident.geometry.y}).subscribe(response => {
+        if (response.features) {
+          for (const element of response.features) {
+            this.areaRestrictions.push({
+              protRsSysID: element.attributes.PROT_RA_SYSID,
+              name: element.attributes.NAME,
+              accessStatusEffectiveDate: element.attributes.ACCESS_STATUS_EFFECTIVE_DATE,
+              fireCentre: element.attributes.FIRE_CENTRE_NAME,
+              fireZone: element.attributes.FIRE_ZONE_NAME,
+              bulletinUrl: element.attributes.BULLETIN_URL
+            })
+          }
         }
-      }
-    })
+      })
+    }
   }
 }
