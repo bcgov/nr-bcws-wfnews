febbe27dc0ff01aa83a2930b573e88bbf01691f3
Dylan <dylan.hemsworth@gmail.com>
2023-02-01 11:42:01 -0800

    Add configuration source to spring security (#601)

--- client/wfnews-war/src/main/angular/src/app/store/wildfiresList/wildfiresList.effects.ts
+++ client/wfnews-war/src/main/angular/src/app/store/wildfiresList/wildfiresList.effects.ts
@@ -98,7 +98,12 @@ export class WildfiresListEffect {
                 headers.append('Access-Control-Allow-Origin', '*');
                 headers.append('Accept', '*/*');
                 headers.append('apikey', this.appConfigService.getConfig().application['wfnewsApiKey']);
-                return this.http.get<any>(url, {headers})
+
+                return this.http.get<any>(url, { headers: {
+                  'Access-Control-Allow-Origin': '*',
+                  'Accept': '*/*',
+                  'apikey': this.appConfigService.getConfig().application['wfnewsApiKey']
+                } })
                 .pipe(
                     map((response: any) => {
                       typedaction.callback()
