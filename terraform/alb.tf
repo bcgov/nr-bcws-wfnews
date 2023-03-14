97c7d33a022808d0d1a61d6a30732db3af71a2ff
vivid-cpreston <97257824+vivid-cpreston@users.noreply.github.com>
2023-03-14 09:02:22 -0700

    Prod preparation (#650)
    * Updated condition syntax

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -236,9 +236,11 @@ resource "aws_lb_listener_rule" "wfnews_host_based_weighted_routing" {
     host_header {
       values = [for sn in var.server_names : "${sn}.*"]
     }
+  }
+  condition {
     http_header {
-      http_header_name: "X-Cloudfront-Header"
-      http_header_value: "${var.cloudfront_header}"
+      http_header_name = "X-Cloudfront-Header"
+      http_header_value = "${var.cloudfront_header}"
     }
   }
   condition {
@@ -262,9 +264,11 @@ resource "aws_lb_listener_rule" "wfnews_host_based_weighted_routing_client" {
     host_header {
       values = [for sn in var.client_names : "${sn}.*"]
     }
+  }
+  condition {
     http_header {
-      http_header_name: "X-Cloudfront-Header"
-      http_header_value: "${var.cloudfront_header}"
+      http_header_name = "X-Cloudfront-Header"
+      http_header_value = "${var.cloudfront_header}"
     }
   }
   condition {
@@ -288,9 +292,12 @@ resource "aws_lb_listener_rule" "wfnews_host_based_weighted_routing_liquibase" {
     host_header {
       values = [for sn in var.liquibase_names : "${sn}.*"]
     }
+  }
+
+  condition {
     http_header {
-      http_header_name: "X-Cloudfront-Header"
-      http_header_value: "${var.cloudfront_header}"
+      http_header_name = "X-Cloudfront-Header"
+      http_header_value = "${var.cloudfront_header}"
     }
   }
   condition {
@@ -314,9 +321,11 @@ resource "aws_lb_listener_rule" "wfnews_host_based_weighted_routing_apisix" {
     host_header {
       values = [for sn in var.apisix_names : "${sn}.*"]
     }
+  }
+  condition {
     http_header {
-      http_header_name: "X-Cloudfront-Header"
-      http_header_value: "${var.cloudfront_header}"
+      http_header_name = "X-Cloudfront-Header"
+      http_header_value = "${var.cloudfront_header}"
     }
   }
   condition {
