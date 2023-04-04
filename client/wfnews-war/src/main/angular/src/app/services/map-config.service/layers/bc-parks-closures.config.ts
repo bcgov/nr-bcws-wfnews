346d69419ef945c78f7591b2e43d2325f506e459
Dylan <dylan.hemsworth@gmail.com>
2023-04-04 16:05:12 -0700

    Add protected Lands popup (#672)
    Add an identify popup for the protected lands layer

--- client/wfnews-war/src/main/angular/src/app/services/map-config.service/layers/bc-parks-closures.config.ts
+++ client/wfnews-war/src/main/angular/src/app/services/map-config.service/layers/bc-parks-closures.config.ts
@@ -3,46 +3,22 @@ import { layerSettings } from '.';
 export function ProtectedLandsAccessRestrictionsLayerConfig(res: layerSettings) {
   return [
     {
-      "type": "wms",
-      "id": "protected-lands-access-restrictions",
-      "title": "Protected Lands Access Restrictions",
-      "serviceUrl": res.openmapsBaseUrl,
-      "layerName": "pub:WHSE_PARKS.PA_PRTCTD_LND_RSTRCTNS_SV",
-      "styleName": "",
-      "titleAttribute": "PROTECTED_LANDS_NAME",
-      "attributes": [
+      type: "wms",
+      id: "protected-lands-access-restrictions",
+      title: "Protected Lands Access Restrictions",
+      serviceUrl: res.openmapsBaseUrl,
+      layerName: "pub:WHSE_PARKS.PA_PRTCTD_LND_RSTRCTNS_SV",
+      styleName: "",
+      titleAttribute: "PROTECTED_LANDS_NAME",
+      attributes: [
         {
           "name": "PROTECTED_LANDS_NAME",
           "title": "Protected Lands Name"
-        },
-        {
-          "name": "ACCESS_STATUS",
-          "title": "Access Status"
-        },
-        {
-          "name": "ACCESS_DETAILS",
-          "title": "Access Details"
-        },
-        {
-          "name": "FACILITIES_CAMPFIRES_IND",
-          "title": "Facilities - Campfire Indicator"
-        },
-        {
-          "name": "CAMPFIRE_BAN_IND",
-          "title": "Campfire Ban"
-        },
-        {
-          "title": "Access Effective Date",
-          "value": "<%= this.asDate( 'ACCESS_STATUS_EFFECTIVE_DATE' ) %>"
-        },
-        {
-          "name": "ACCESS_STATUS_RESCINDED_DATE",
-          "title": "Access Rescinded Date"
-        },
+        }
       ],
-      "where": "EVENT_TYPE = 'Wildfire'",
-      "geometryAttribute": "SHAPE",
-      "popupTemplate": "@wf-feature"
+      where: "EVENT_TYPE = 'Wildfire'",
+      geometryAttribute: "SHAPE",
+      popupTemplate: '<div class="smk-popup"><div class="popup-header"><span class="title-label" style="margin-left: 0px !important;">Protected Lands Access Restrictions</span></div><div class="popup-title" style="margin-top: 15px;">{{feature.properties["PROTECTED_LANDS_NAME"]}}</div><div class="popup-attributes"><div class="label">Access Status:</div><div class="attribute">{{feature.properties["ACCESS_STATUS"]}}</div></div><div class="popup-attributes"><div class="label">Access Details:</div><div class="attribute">{{feature.properties["ACCESS_DETAILS"]}}</div></div><div class="popup-attributes"><div class="label">Campfires:</div><div class="attribute">{{feature.properties["FACILITIES_CAMPFIRES_IND"]}}</div></div><div class="popup-attributes"><div class="label">Campfire Ban:</div><div class="attribute">{{feature.properties["CAMPFIRE_BAN_IND"]}}</div></div><div class="popup-attributes"><div class="label">Effective Date:</div><div class="attribute">{{feature.properties["ACCESS_STATUS_EFFECTIVE_DATE"] ? feature.properties["ACCESS_STATUS_EFFECTIVE_DATE"].slice(0, -1) : ""}}</div></div><div class="popup-attributes"><div class="label">Rescinded Date:</div><div class="attribute">{{feature.properties["ACCESS_STATUS_RESCINDED_DATE"] ? feature.properties["ACCESS_STATUS_RESCINDED_DATE"].slice(0, -1) : ""}}</div></div></div>'
     }
   ]
 }
