f1740c422a03c6982869b6fc1430c88ec15ab489
Lucas Li <yzlucas@outlook.com>
2023-06-02 16:15:59 -0700

    wip, tab itself is done. Still need to create contact us pop up

--- client/wfnews-war/src/main/angular/src/app/components/public-incident-page/incident-info-panel-mobile/incident-info-panel-mobile.component.ts
+++ client/wfnews-war/src/main/angular/src/app/components/public-incident-page/incident-info-panel-mobile/incident-info-panel-mobile.component.ts
@@ -7,8 +7,17 @@ import { IncidentInfoPanel } from '../incident-info-panel/incident-info-panel.co
   styleUrls: ['./incident-info-panel-mobile.component.scss']
 })
 export class IncidentInfoPanelMobileComponent extends IncidentInfoPanel implements OnInit {
-
   ngOnInit(): void {
   }
 
+  getTooltipText() {
+    return `What is a Hecture?
+
+    A hectare is a unit of area equal to 10,000 square meters or around 2.5 acres of land.
+    `;
+  }
+
+  navigateToMap(){
+    //to do, need to wait for the mobile map screen ticket
+  }
 }
