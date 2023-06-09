e137e7f15bd5edd263c16ab8202a63dc0ef5d572
Sean Sylver <ssylver@VividSolutions.com>
2023-06-09 11:16:11 -0700

    Resotre file changes in prod

--- server/wfnews-api-rest-endpoints/src/main/java/ca/bc/gov/nrs/wfnews/api/rest/v1/endpoints/impl/AttachmentsEndpointImpl.java
+++ server/wfnews-api-rest-endpoints/src/main/java/ca/bc/gov/nrs/wfnews/api/rest/v1/endpoints/impl/AttachmentsEndpointImpl.java
@@ -23,8 +23,6 @@ import ca.bc.gov.nrs.common.service.NotFoundException;
 import ca.bc.gov.nrs.wfone.common.service.api.ValidationFailureException;
 import software.amazon.awssdk.core.ResponseInputStream;
 import software.amazon.awssdk.core.sync.RequestBody;
-import software.amazon.awssdk.core.exception.SdkClientException;
-import software.amazon.awssdk.awscore.exception.AwsServiceException;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
@@ -259,7 +257,7 @@ public class AttachmentsEndpointImpl extends BaseEndpointsImpl implements Attach
 			} else {
 				response = Response.status(404).build();
 			}
-		} catch (AwsServiceException | SdkClientException e) {
+		} catch (NoSuchKeyException e) {
 			response = Response.status(404).build();
 		} catch (IOException e) {
 			response = getInternalServerErrorResponse(e);
