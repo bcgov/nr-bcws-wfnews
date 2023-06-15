274348265fb3e08807d1c59e316d618d0dd01b8f
Lucas Li <yzlucas@outlook.com>
2023-06-14 23:01:15 -0700

    map tab

---
+++ client/wfnews-war/src/main/angular/src/app/components/public-incident-page/incident-maps-panel-mobile/incident-maps-panel-mobile.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { IncidentMapsPanel } from '../incident-maps-panel/incident-maps-panel.component';
+
+@Component({
+  selector: 'incident-maps-panel-mobile',
+  templateUrl: './incident-maps-panel-mobile.component.html',
+  styleUrls: ['./incident-maps-panel-mobile.component.scss']
+})
+export class IncidentMapsPanelMobileComponent extends IncidentMapsPanel {
+
+  convertDate(dateString) {
+    if (dateString) {
+      const date = new Date(dateString);
+      const options: Intl.DateTimeFormatOptions = { month: "short", day: "numeric", year: "numeric" };
+      const formattedDate: string = date.toLocaleDateString("en-US", options);
+      return formattedDate
+    }
+  }
+  
+}
