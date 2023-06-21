495c9112ecb2dc3343ed1aa5d96678ff849fa569
Sukhpal <6563909+sukhpalp@users.noreply.github.com>
2023-06-21 09:46:59 -0700

    Rebase

--- client/wfnews-war/src/main/angular/src/app/components/admin-incident-form/admin-incident-form.component.ts
+++ client/wfnews-war/src/main/angular/src/app/components/admin-incident-form/admin-incident-form.component.ts
@@ -237,7 +237,7 @@ export class AdminIncidentForm implements OnInit, OnChanges {
               self.incident.incidentOverview = response.incidentOverview;
 
               this.evacOrdersDetailsPanel.getEvacOrders();
-              this.incidentForm.patchValue(self.incident);
+              this.incidentForm.patchValue(this.incident);
             }, (error) => {
               console.log('No published data found...');
               console.error(error);
