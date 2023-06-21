8878bff64b7fa4127f0d5186603e8f08d0edbed0
Sean Sylver <ssylver@vividsolutions.com>
2023-06-21 13:35:44 -0700

    add new image and video panel components to mitigate code duplication

---
+++ client/wfnews-war/src/main/angular/src/app/components/public-incident-page/image-panel/image-panel.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+import { convertToMobileFormat } from '../../../utils';
+
+@Component({
+  selector: 'image-panel',
+  templateUrl: './image-panel.component.html',
+  styleUrls: ['./image-panel.component.scss']
+})
+export class ImagePanelComponent {
+  @Input() public item;
+  @Input() public settings;
+  @Input() public onBeforeSlide;
+  @Input() public onInit;
+  @Input() public i;
+  convertToMobileFormat = convertToMobileFormat;
+}
