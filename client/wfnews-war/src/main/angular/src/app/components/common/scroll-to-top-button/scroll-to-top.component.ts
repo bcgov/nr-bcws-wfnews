12bd257d7fd50f64794c5ec84b1621bd9928246d
Sukh <6563909+sukhpalp@users.noreply.github.com>
2023-06-26 10:40:07 -0700

    Reusable scroll to top button

---
+++ client/wfnews-war/src/main/angular/src/app/components/common/scroll-to-top-button/scroll-to-top.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { isMobileView } from '../../../utils';
+import { ViewportScroller } from '@angular/common';
+import { style } from '@angular/animations';
+
+
+@Component({
+    // eslint-disable-next-line @angular-eslint/component-selector
+    selector: 'scroll-to-top-button',
+    templateUrl: './scroll-to-top.component.html',
+    styleUrls: ['./scroll-to-top.component.scss']
+})
+
+export class ScrollToTopComponent implements OnInit, OnDestroy {
+
+    isMobileView = isMobileView;
+    isButtonVisible = false;
+
+    constructor(private viewportScroller: ViewportScroller) { }
+
+    ngOnInit() {
+        window.addEventListener('scroll', this.scrollEvent, true);
+    }
+
+    ngOnDestroy() {
+        window.removeEventListener('scroll', this.scrollEvent, true);
+    }
+
+    onScrollToTop(): void {
+        document.getElementsByClassName('top')[0].scrollIntoView({ behavior: 'smooth' });
+    }
+
+    scrollEvent = (): void => {
+        const offset = document.getElementsByClassName('top')[0].getBoundingClientRect().y;
+        this.isButtonVisible = offset < -100 && isMobileView();
+    };
+
+}
