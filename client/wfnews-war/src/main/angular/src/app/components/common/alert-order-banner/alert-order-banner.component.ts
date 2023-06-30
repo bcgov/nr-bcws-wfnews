17f9b8584f36791136cf3ea82bac70deb4b91d65
Sukh <6563909+sukhpalp@users.noreply.github.com>
2023-06-30 12:45:40 -0700

    Handle area restriction banners and some style fix

--- client/wfnews-war/src/main/angular/src/app/components/common/alert-order-banner/alert-order-banner.component.ts
+++ client/wfnews-war/src/main/angular/src/app/components/common/alert-order-banner/alert-order-banner.component.ts
@@ -1,14 +1,45 @@
 import { Component, Input } from '@angular/core';
-import { EvacOrderOption } from '../../../conversion/models';
+import { AreaRestrictionsOption, EvacOrderOption } from '../../../conversion/models';
 
 @Component({
   selector: 'alert-order-banner',
   templateUrl: './alert-order-banner.component.html',
   styleUrls: ['./alert-order-banner.component.scss']
 })
-export class AlertBannerComponent {
-  @Input() evac: EvacOrderOption;
-  @Input() card: boolean;
+export class AlertOrderBannerComponent {
+  @Input() evacuation: EvacOrderOption;
+  @Input() areaRestriction: AreaRestrictionsOption;
+  @Input() isCard: boolean;
 
-  isOrder = () => this.evac?.orderAlertStatus === 'Order'
+  isOrder = () => this.evacuation?.orderAlertStatus === 'Order'
+  isArea = () => !!this.areaRestriction
+
+  color = () => {
+    if (this.isOrder() || this.isArea()) {
+      return 'red';
+    }
+    return 'yellow';
+  }
+
+  shape = () => this.isCard ? 'card' : 'banner';
+  
+  icon = () => {
+    if (this.isArea()) {
+      return 'signpost';
+    }
+    if (this.isOrder()) {
+      return 'error';
+    }
+    return 'warning';
+  }
+
+  message = () => {
+    if (this.isArea()) {
+      return `Area Restriction: ${this.areaRestriction?.name}`;
+    }
+    if (this.isOrder()) {
+      return `Evacuation Order: ${this.evacuation?.eventName} issued by ${this.evacuation?.issuingAgency}`;
+    }
+    return `Evacuation Alert: ${this.evacuation?.eventName} issued by ${this.evacuation?.issuingAgency}`;
+  }
 }
