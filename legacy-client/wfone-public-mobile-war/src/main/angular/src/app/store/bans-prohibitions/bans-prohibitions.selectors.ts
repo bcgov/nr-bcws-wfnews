f036df18ff94cb1a8924a8a821062accbcdd9650
Dylan <dylan.hemsworth@gmail.com>
2023-07-07 09:37:03 -0700

    Delete legacy-client directory
    Remove legacy client

--- legacy-client/wfone-public-mobile-war/src/main/angular/src/app/store/bans-prohibitions/bans-prohibitions.selectors.ts
+++
@@ -1,5 +0,0 @@
-import {RootState} from "../index";
-import {VmBanProhibition} from "../../conversion/models";
-
-export const selectCurrentBansProhibitions = () => (state: RootState): VmBanProhibition[] =>
-    ((state.bansProhibitions) ? state.bansProhibitions.currentBansProhibitions : undefined);
--- legacy-client/wfone-public-mobile-war/src/main/angular/src/app/test/components/fake/fake-activated-route.ts
+++
@@ -1,6 +0,0 @@
-import {Subject} from "rxjs";
-
-export class FakeActivatedRoute {
-    queryParams = new Subject<any>();
-    paramMap = new Subject<any>();
-}
