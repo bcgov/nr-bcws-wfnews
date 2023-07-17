381ecbdbfd15f0680b3fea04892c23381d985d55
Sukh <6563909+sukhpalp@users.noreply.github.com>
2023-07-17 11:26:23 -0700

    WFNEWS-1076 : Add wfone-notifications-api to terraform (#857)
    * Android dev build
    
    * Add names and production steps
    
    * iOS script and cleanup
    
    * Update scripts
    
    * update scripts
    
    * Add branch temporarily for testing
    
    * Add caching back to android
    
    * Fix package-lock location
    
    * fix paths
    
    * add npmrc
    
    * fix path of npmrc
    
    * fix secret name
    
    * Remove package-lock and cache
    
    * Missed a line
    
    * comment slack message
    
    * quotes around echo
    
    * Fix echo on mac
    
    * debug npmrc
    
    * remove param from echo
    
    * Tweak echo command
    
    * Put npmrc in home on macos
    
    * mac npmrc debugging
    
    * more debug
    
    * more debugging
    
    * clean up
    
    * debug debug
    
    * put npmrc into env
    
    * Add iOS again with proper settings
    
    * Android dev build
    
    * Add names and production steps
    
    * iOS script and cleanup
    
    * Update scripts
    
    * update scripts
    
    * Add branch temporarily for testing
    
    * Add caching back to android
    
    * Fix package-lock location
    
    * fix paths
    
    * add npmrc
    
    * fix path of npmrc
    
    * fix secret name
    
    * Remove package-lock and cache
    
    * Missed a line
    
    * comment slack message
    
    * quotes around echo
    
    * Fix echo on mac
    
    * debug npmrc
    
    * remove param from echo
    
    * Tweak echo command
    
    * Put npmrc in home on macos
    
    * mac npmrc debugging
    
    * more debug
    
    * more debugging
    
    * clean up
    
    * debug debug
    
    * put npmrc into env
    
    * Add iOS again with proper settings
    
    * Add google services file
    
    * Uncomment Slack
    
    * Add Android back with correct settings
    
    * Fix working directory
    
    * Use APK for build
    
    * Fix message formatting
    
    * Message formating
    
    * Control flow using inputs
    
    * Get the workflow running for the first time
    
    * Add push to master so workflow runs once
    
    * Add more detail to slack
    
    * Add branch to push
    
    * bump build numbers for android releases
    
    * Remove the push trigger (can use CLI)
    
    * Make a single config for mobile
    
    * run deploy
    
    * Add branch to run workflow
    
    * Move config stuff up file
    
    * fix directory
    
    * fix directory pt 2
    
    * Add config file
    
    * Try other syntax
    
    * quotes to make string
    
    * change env to vars
    
    * version extraction
    
    * Add working directory
    
    * Change mvn command
    
    * Use env approach
    
    * Use xml parsing
    
    * Remove dot in path
    
    * Change directory
    
    * Change to parent path
    
    * print pom file
    
    * Remove xpath
    
    * update xpath
    
    * Simplify xpath
    
    * Remove text()
    
    * xpath stuff
    
    * Remove POM print
    
    * Implement environments and clean up
    
    * Clean up and fix ENVs
    
    * Fix typo
    
    * Add ENVs to terragrunt_deploy
    
    * Remove remnants
    
    * Update terraform
    
    * Clean up after merges
    
    * Fix names
    
    * Fix ECS for notifications-api and change configs
    
    * Remove commented section
    
    * Fix missing end comment
    
    * Remove redundant comment

--- terraform/autoscaling.tf
+++ terraform/autoscaling.tf
@@ -368,3 +368,100 @@ resource "aws_cloudwatch_metric_alarm" "wfss_pointid_service_cpu_high" {
 
   tags = local.common_tags
 }
+
+# __________________________________________________________
+# 
+#                 WFONE-NOTIFICATION-API
+# __________________________________________________________
+
+resource "aws_appautoscaling_target" "wfone_notifications_api_target" {
+  service_namespace  = "ecs"
+  resource_id        = "service/${aws_ecs_cluster.wfnews_main.name}/${aws_ecs_service.wfone_notifications_api.name}"
+  scalable_dimension = "ecs:service:DesiredCount"
+  min_capacity       = var.app_count
+  max_capacity       = 10
+}
+
+# Automatically scale capacity up by one
+resource "aws_appautoscaling_policy" "wfone_notifications_api_up" {
+  name               = "wfone_notifications_api_scale_up"
+  service_namespace  = "ecs"
+  resource_id        = "service/${aws_ecs_cluster.wfnews_main.name}/${aws_ecs_service.wfone_notifications_api.name}"
+  scalable_dimension = "ecs:service:DesiredCount"
+
+  step_scaling_policy_configuration {
+    adjustment_type         = "ChangeInCapacity"
+    cooldown                = 60
+    metric_aggregation_type = "Maximum"
+
+    step_adjustment {
+      metric_interval_lower_bound = 0
+      scaling_adjustment          = 1
+    }
+  }
+
+  depends_on = [aws_appautoscaling_target.wfone_notifications_api_target]
+}
+
+# Automatically scale capacity down by one
+resource "aws_appautoscaling_policy" "wfone_notifications_api_down" {
+  name               = "wfone_notifications_api_scale_down"
+  service_namespace  = "ecs"
+  resource_id        = "service/${aws_ecs_cluster.wfnews_main.name}/${aws_ecs_service.wfone_notifications_api.name}"
+  scalable_dimension = "ecs:service:DesiredCount"
+
+  step_scaling_policy_configuration {
+    adjustment_type         = "ChangeInCapacity"
+    cooldown                = 60
+    metric_aggregation_type = "Maximum"
+
+    step_adjustment {
+      metric_interval_upper_bound = 0
+      scaling_adjustment          = -1
+    }
+  }
+
+  depends_on = [aws_appautoscaling_target.wfone_notifications_api_target]
+}
+
+# CloudWatch alarm that triggers the autoscaling down policy
+resource "aws_cloudwatch_metric_alarm" "wfone_notifications_api_service_cpu_low" {
+  alarm_name          = "wfone_notifications_api_cpu_utilization_low"
+  comparison_operator = "LessThanOrEqualToThreshold"
+  evaluation_periods  = "2"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/ECS"
+  period              = "60"
+  statistic           = "Average"
+  threshold           = "10"
+
+  dimensions = {
+    ClusterName = aws_ecs_cluster.wfnews_main.name
+    ServiceName = aws_ecs_service.wfone_notifications_api.name
+  }
+
+  alarm_actions = [aws_appautoscaling_policy.wfone_notifications_api_down.arn]
+
+  tags = local.common_tags
+}
+
+# CloudWatch alarm that triggers the autoscaling up policy
+resource "aws_cloudwatch_metric_alarm" "wfone_notifications_api_service_cpu_high" {
+  alarm_name          = "wfone_notifications_api_cpu_utilization_high"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/ECS"
+  period              = "60"
+  statistic           = "Average"
+  threshold           = "50"
+
+  dimensions = {
+    ClusterName = aws_ecs_cluster.wfnews_main.name
+    ServiceName = aws_ecs_service.wfone_notifications_api.name
+  }
+
+  alarm_actions = [aws_appautoscaling_policy.wfone_notifications_api_up.arn]
+
+  tags = local.common_tags
+}
