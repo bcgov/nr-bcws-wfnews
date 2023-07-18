2d56894c2782ee0416d4f3537c43ca904f01665e
Sukh <6563909+sukhpalp@users.noreply.github.com>
2023-07-18 08:04:53 -0700

    Rough dialog for unsaved changes

---
+++ client/wfnews-war/src/main/angular/src/app/components/common/unsaved-changes-dialog/unsaved-changes-dialog.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'unsaved-changes-dialog',
+    templateUrl: 'unsaved-changes-dialog.component.html',
+    styleUrls: ['./unsaved-changes-dialog.component.scss']
+})
+export class UnsavedChangesDialogComponent {
+    constructor (
+    ) {
+
+      console.log('test');
+    }
+}
