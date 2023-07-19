f28e6add5b2312dee839e428a8fee691cec70ca5
Sukh <6563909+sukhpalp@users.noreply.github.com>
2023-07-19 12:02:55 -0700

    WFNEWS-1262 : Fix publish cancel and add dialog (#874)
    * Fix original problem
    
    * Fix buttons to match TAC
    
    * Rough dialog for unsaved changes
    
    * Fix dialog and guard
    
    * Events to listen for form dirtying and styling
    
    ---------
    
    Co-authored-by: Dylan <dylan.hemsworth@gmail.com>

---
+++ client/wfnews-war/src/main/angular/src/app/components/common/guards/unsaved-changes.guard.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { CanDeactivate } from '@angular/router'
+import { Observable, of } from 'rxjs'
+
+export interface CheckUnsavedChanges {
+  changesSaved: () => boolean | Observable<boolean>
+  confirmDialog: () => boolean | Observable<boolean>
+}
+
+@Injectable()
+export class CanDeactivateGuard implements CanDeactivate<CheckUnsavedChanges> {
+  constructor(
+    public dialog: MatDialog,
+  ) { }
+
+  canDeactivate(component: CheckUnsavedChanges): boolean | Observable<boolean> {
+    if (!component.changesSaved()) {
+      return component.confirmDialog();
+    }
+
+    return of(true);
+  }
+}
