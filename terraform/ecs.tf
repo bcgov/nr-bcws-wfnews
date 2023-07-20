ef1e82fa21ddb64952d54848e52e47e52ec3bc8b
vivid-cpreston <97257824+vivid-cpreston@users.noreply.github.com>
2023-07-19 17:51:13 -0700

    Terraform cleanup (#884)
    * Temporarily removed notifications api config settings

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -1558,7 +1558,8 @@ resource "aws_ecs_task_definition" "wfnews_client" {
         #   sourceVolume = "work"
         #   containerPath = "/usr/local/tomcat/work"
         #   readOnly = false
-        # }]
+        # }
+        ]
       volumesFrom = []
     }
   ])
@@ -2180,114 +2181,114 @@ resource "aws_ecs_task_definition" "wfone_notifications_api" {
             hostPort      = var.wfone_notifications_api_port
       }]
       environment = [
-          {
-            name  = "DATASOURCE_MAX_CONNECTIONS",
-            value = var.WFONE_NOTIFICATIONS_API_DATASOURCE_MAX_CONNECTIONS
-          },
-          {
-            name  = "DATASOURCE_PASSWORD",
-            value = var.WFONE_NOTIFICATIONS_API_DATASOURCE_PASSWORD
-          },
-          {
-            name  = "DATASOURCE_URL",
-            value = var.WFONE_NOTIFICATIONS_API_DATASOURCE_URL
-          },
-          {
-            name  = "DATASOURCE_USER",
-            value = var.WFONE_NOTIFICATIONS_API_DATASOURCE_USER
-          },
-          {
-            name  = "DEFAULT_APPLICATION_ENVIRONMENT",
-            value = var.DEFAULT_APPLICATION_ENVIRONMENT
-          },
-          {
-            name  = "EMAIL_ADMIN_EMAIL",
-            value = var.WFONE_NOTIFICATIONS_API_EMAIL_ADMIN_EMAIL
-          },
-          {
-            name  = "EMAIL_FROM_EMAIL",
-            value = var.WFONE_NOTIFICATIONS_API_EMAIL_FROM_EMAIL
-          },
-          {
-            name  = "EMAIL_NOTIFICATIONS_ENABLED",
-            value = var.WFONE_NOTIFICATIONS_API_EMAIL_NOTIFICATIONS_ENABLED
-          },
-          {
-            name  = "EMAIL_SYNC_SEND_ERROR_FREQ",
-            value = var.WFONE_NOTIFICATIONS_API_EMAIL_SYNC_SEND_ERROR_FREQ
-          },
-          {
-            name  = "EMAIL_SYNC_SEND_ERROR_SUBJECT",
-            value = var.WFONE_NOTIFICATIONS_API_EMAIL_SYNC_SEND_ERROR_SUBJECT
-          },
-          {
-            name  = "EMAIL_SYNC_SEND_FREQ",
-            value = var.WFONE_NOTIFICATIONS_API_EMAIL_SYNC_SEND_FREQ
-          },
-          {
-            name  = "EMAIL_SYNC_SUBJECT",
-            value = var.WFONE_NOTIFICATIONS_API_EMAIL_SYNC_SUBJECT
-          },
-          {
-            name  = "PUSH_ITEM_EXPIRE_HOURS",
-            value = var.WFONE_NOTIFICATIONS_API_PUSH_ITEM_EXPIRE_HOURS
-          },
-          {
-            name  = "QUARTZ_CONSUMER_INTERVAL_SECONDS",
-            value = var.WFONE_NOTIFICATIONS_API_QUARTZ_CONSUMER_INTERVAL_SECONDS
-          },
-          {
-            name  = "SMTP_CREDENTIALS_PASSWORD",
-            value = var.WFONE_NOTIFICATIONS_API_SMTP_CREDENTIALS_PASSWORD
-          },
-          {
-            name  = "SMTP_CREDENTIALS_USER",
-            value = var.WFONE_NOTIFICATIONS_API_SMTP_CREDENTIALS_USER
-          },
-          {
-            name  = "SMTP_HOST_NAME",
-            value = var.WFONE_NOTIFICATIONS_API_SMTP_HOST_NAME
-          },
-          {
-            name  = "WEBADE_OAUTH2_CHECK_TOKEN_URL"
-            value = var.WEBADE-OAUTH2_CHECK_TOKEN_URL
-          },
-          {
-            name  = "WEBADE_OAUTH2_CLIENT_ID",
-            value = var.WFONE_NOTIFICATIONS_API_WEBADE_OAUTH2_CLIENT_ID
-          },
-          {
-            name  = "WEBADE_OAUTH2_REST_CLIENT_SECRET",
-            value = var.WFONE_NOTIFICATIONS_API_WEBADE_OAUTH2_REST_CLIENT_SECRET
-          },
-          {
-            name  = "WEBADE_OAUTH2_TOKEN_CLIENT_URL",
-            value = var.WEBADE-OAUTH2_TOKEN_CLIENT_URL
-          },
-          {
-            name  = "WEBADE_OAUTH2_TOKEN_URL",
-            value = var.WEBADE-OAUTH2_TOKEN_URL
-          },
-          {
-            name  = "WEBADE_OAUTH2_WFIM_CLIENT_ID",
-            value = var.WFONE_NOTIFICATIONS_API_WEBADE_OAUTH2_WFIM_CLIENT_ID
-          },
-          {
-            name  = "WFDM_REST_URL",
-            value = var.WFDM_REST_URL
-          },
-          {
-            name  = "WFIM_CLIENT_URL",
-            value = var.WFIM_CLIENT_URL
-          },
-          {
-            name  = "WFIM_CODE_TABLES_URL",
-            value = var.WFIM_CODE_TABLES_URL
-          },
-          {
-            name  = "WFSS_POINTID_URL",
-            value = var.WFSS_POINTID_URL
-          }
+          # {
+          #   name  = "DATASOURCE_MAX_CONNECTIONS",
+          #   value = var.WFONE_NOTIFICATIONS_API_DATASOURCE_MAX_CONNECTIONS
+          # },
+          # {
+          #   name  = "DATASOURCE_PASSWORD",
+          #   value = var.WFONE_NOTIFICATIONS_API_DATASOURCE_PASSWORD
+          # },
+          # {
+          #   name  = "DATASOURCE_URL",
+          #   value = var.WFONE_NOTIFICATIONS_API_DATASOURCE_URL
+          # },
+          # {
+          #   name  = "DATASOURCE_USER",
+          #   value = var.WFONE_NOTIFICATIONS_API_DATASOURCE_USER
+          # },
+          # {
+          #   name  = "DEFAULT_APPLICATION_ENVIRONMENT",
+          #   value = var.DEFAULT_APPLICATION_ENVIRONMENT
+          # },
+          # {
+          #   name  = "EMAIL_ADMIN_EMAIL",
+          #   value = var.WFONE_NOTIFICATIONS_API_EMAIL_ADMIN_EMAIL
+          # },
+          # {
+          #   name  = "EMAIL_FROM_EMAIL",
+          #   value = var.WFONE_NOTIFICATIONS_API_EMAIL_FROM_EMAIL
+          # },
+          # {
+          #   name  = "EMAIL_NOTIFICATIONS_ENABLED",
+          #   value = var.WFONE_NOTIFICATIONS_API_EMAIL_NOTIFICATIONS_ENABLED
+          # },
+          # {
+          #   name  = "EMAIL_SYNC_SEND_ERROR_FREQ",
+          #   value = var.WFONE_NOTIFICATIONS_API_EMAIL_SYNC_SEND_ERROR_FREQ
+          # },
+          # {
+          #   name  = "EMAIL_SYNC_SEND_ERROR_SUBJECT",
+          #   value = var.WFONE_NOTIFICATIONS_API_EMAIL_SYNC_SEND_ERROR_SUBJECT
+          # },
+          # {
+          #   name  = "EMAIL_SYNC_SEND_FREQ",
+          #   value = var.WFONE_NOTIFICATIONS_API_EMAIL_SYNC_SEND_FREQ
+          # },
+          # {
+          #   name  = "EMAIL_SYNC_SUBJECT",
+          #   value = var.WFONE_NOTIFICATIONS_API_EMAIL_SYNC_SUBJECT
+          # },
+          # {
+          #   name  = "PUSH_ITEM_EXPIRE_HOURS",
+          #   value = var.WFONE_NOTIFICATIONS_API_PUSH_ITEM_EXPIRE_HOURS
+          # },
+          # {
+          #   name  = "QUARTZ_CONSUMER_INTERVAL_SECONDS",
+          #   value = var.WFONE_NOTIFICATIONS_API_QUARTZ_CONSUMER_INTERVAL_SECONDS
+          # },
+          # {
+          #   name  = "SMTP_CREDENTIALS_PASSWORD",
+          #   value = var.WFONE_NOTIFICATIONS_API_SMTP_CREDENTIALS_PASSWORD
+          # },
+          # {
+          #   name  = "SMTP_CREDENTIALS_USER",
+          #   value = var.WFONE_NOTIFICATIONS_API_SMTP_CREDENTIALS_USER
+          # },
+          # {
+          #   name  = "SMTP_HOST_NAME",
+          #   value = var.WFONE_NOTIFICATIONS_API_SMTP_HOST_NAME
+          # },
+          # {
+          #   name  = "WEBADE_OAUTH2_CHECK_TOKEN_URL"
+          #   value = var.WEBADE-OAUTH2_CHECK_TOKEN_URL
+          # },
+          # {
+          #   name  = "WEBADE_OAUTH2_CLIENT_ID",
+          #   value = var.WFONE_NOTIFICATIONS_API_WEBADE_OAUTH2_CLIENT_ID
+          # },
+          # {
+          #   name  = "WEBADE_OAUTH2_REST_CLIENT_SECRET",
+          #   value = var.WFONE_NOTIFICATIONS_API_WEBADE_OAUTH2_REST_CLIENT_SECRET
+          # },
+          # {
+          #   name  = "WEBADE_OAUTH2_TOKEN_CLIENT_URL",
+          #   value = var.WEBADE-OAUTH2_TOKEN_CLIENT_URL
+          # },
+          # {
+          #   name  = "WEBADE_OAUTH2_TOKEN_URL",
+          #   value = var.WEBADE-OAUTH2_TOKEN_URL
+          # },
+          # {
+          #   name  = "WEBADE_OAUTH2_WFIM_CLIENT_ID",
+          #   value = var.WFONE_NOTIFICATIONS_API_WEBADE_OAUTH2_WFIM_CLIENT_ID
+          # },
+          # {
+          #   name  = "WFDM_REST_URL",
+          #   value = var.WFDM_REST_URL
+          # },
+          # {
+          #   name  = "WFIM_CLIENT_URL",
+          #   value = var.WFIM_CLIENT_URL
+          # },
+          # {
+          #   name  = "WFIM_CODE_TABLES_URL",
+          #   value = var.WFIM_CODE_TABLES_URL
+          # },
+          # {
+          #   name  = "WFSS_POINTID_URL",
+          #   value = var.WFSS_POINTID_URL
+          # }
         ]
       logConfiguration = {
         logDriver = "awslogs"
