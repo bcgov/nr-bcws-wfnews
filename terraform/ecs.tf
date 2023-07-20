cb1da8f0722e024206ecfd3c2d5ddb5d76a45f2c
vivid-cpreston <97257824+vivid-cpreston@users.noreply.github.com>
2023-07-19 22:45:32 -0700

    Update ecs.tf
    typo fix

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -2063,7 +2063,7 @@ resource "aws_ecs_task_definition" "wfss_pointid" {
           },
           {
             name  = "WEBADE_OATH2_TOKEN_URL",
-            value = var.WEBADE-OATH2_TOKEN_URL
+            value = var.WEBADE_OAUTH2_TOKEN_URL
           },
           {
             name  = "WEBADE_OAUTH2_CLIENT_SCOPES",
