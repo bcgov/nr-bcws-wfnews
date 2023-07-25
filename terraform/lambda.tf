bb8980fd261e180495b990e280683e0242c9196a
vivid-cpreston <cpreston@vividsolutions.com>
2023-07-25 15:03:06 -0700

    fix naming

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -39,7 +39,7 @@ resource "aws_lambda_function" "monitor-bans-prohibitions" {
   layers  = [aws_lambda_layer_version.wfnews_lambda_layer.arn]
   environment {
     variables = {
-      QUEUE_URL   = aws_sqs_queue.queues["bans_prohibitions"].url
+      QUEUE_URL   = aws_sqs_queue.queues["bans-prohibitions"].url
       S3_BUCKET   = aws_s3_bucket.wfnews-monitor-queue-bucket.id
       SECRET_NAME = var.SECRET_NAME
       WFNEWS_API  = aws_route53_record.wfnews_apisix.name
@@ -59,7 +59,7 @@ resource "aws_lambda_function" "monitor-active-fires" {
   environment {
     variables = {
       UNIQUE_DEPLOY_ID = var.UNIQUE_DEPLOY_ID
-      QUEUE_URL   = aws_sqs_queue.queues["active_fires"].url
+      QUEUE_URL   = aws_sqs_queue.queues["active-fires"].url
       S3_BUCKET   = aws_s3_bucket.wfnews-monitor-queue-bucket.id
       SECRET_NAME = var.SECRET_NAME
       WFNEWS_API  = aws_route53_record.wfnews_apisix.name
@@ -97,7 +97,7 @@ resource "aws_lambda_function" "monitor-evacuation" {
   layers  = [aws_lambda_layer_version.wfnews_lambda_layer.arn]
   environment {
     variables = {
-      QUEUE_URL   = aws_sqs_queue.queues["evac_orders"].url
+      QUEUE_URL   = aws_sqs_queue.queues["evac-orders"].url
       S3_BUCKET   = aws_s3_bucket.wfnews-monitor-queue-bucket.id
       SECRET_NAME = var.SECRET_NAME
       WFNEWS_API  = aws_route53_record.wfnews_apisix.name
