c6cb495d27d332a6e7986d30713719d27f71762d
Lucas Li <yzlucas@outlook.com>
2023-07-27 16:14:42 -0700

    wip

--- client/wfnews-war/src/main/angular/src/app/components/report-of-fire/location-page/rof-location-page.component.ts
+++ client/wfnews-war/src/main/angular/src/app/components/report-of-fire/location-page/rof-location-page.component.ts
@@ -1,6 +1,11 @@
-import { Component, ChangeDetectionStrategy } from "@angular/core";
+import { Component, ChangeDetectionStrategy, ChangeDetectorRef, OnInit } from "@angular/core";
 import { RoFPage } from "../rofPage";
 import { ReportOfFire } from "../reportOfFireModel";
+import { AppConfigService } from "@wf1/core-ui";
+import { MapConfigService } from '../../../services/map-config.service';
+import { Position } from "../../../services/common-utility.service";
+import { CompassHeading } from "../../../services/capacitor-service";
+import { CommonUtilityService } from '../../../services/common-utility.service';
 
 @Component({
   selector: 'rof-location-page',
@@ -8,18 +13,85 @@ import { ReportOfFire } from "../reportOfFireModel";
   styleUrls: ['./rof-location-page.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class RoFLocationPage extends RoFPage {
-  public constructor() {
+export class RoFLocationPage extends RoFPage implements OnInit {
+  mapConfig = null;
+  SMK: any;
+  location?: any;
+  distanceEstimateMeter: number = 100;
+  heading: CompassHeading;
+
+
+
+  public constructor(
+    private appConfig: AppConfigService,
+    private mapConfigService: MapConfigService,        
+    private cdr: ChangeDetectorRef,
+    private commonUtilityService: CommonUtilityService
+  ) {
     super()
   }
 
+  ngOnInit(){
+    this.useMyCurrentLocation();
+  }
+
+
   initialize (data: any, index: number, reportOfFire: ReportOfFire) {
     super.initialize(data, index, reportOfFire)
+    this.useMyCurrentLocation();
+
+    this.appConfig.configEmitter.subscribe((config) => {
+      const mapConfig = [];
+
+      this.mapConfigService.getMapConfig()
+        .then((mapState) => {
+          debugger
+
+          let turf = window[ 'turf' ],
+            loc = [ this.location.coords.longitude, this.location.coords.latitude],
+            dist = this.distanceEstimateMeter / 1000, //km
+            head = this.currentHeading,
+            photo = turf.destination( loc, dist, head ),
+            poly = turf.circle( photo.geometry.coordinates, dist ),
+            exp = turf.transformScale( poly, 1.10 ),
+            bbox = turf.bbox( exp ),
+            view = { viewer: { location: { extent: bbox } } }
+
+          this.SMK = window['SMK'];
+          this.mapConfig = [ mapState, view ]
+        })
+        .then(() => {
+          debugger
+
+          const deviceConfig = { viewer: { device: 'desktop' } };
+
+          this.mapConfig = [...mapConfig, deviceConfig, 'theme=wf', '?'];
+        });
+    });
+  }
+
+  get currentHeading() {
+    if ( !this.hasHeading ) return 0
+    return this.heading.trueHeading || 0
+  }
+  get hasHeading() {
+    if ( !this.heading ) return false
+    if ( this.heading.error ) return false
+    return true
   }
 
   setLocation () {
     // the map should trigger this once configured
     this.reportOfFire.estimatedDistance = 0;
     this.reportOfFire.fireLocation = [-112, 50];
+  }
+
+  async useMyCurrentLocation() {
+
+     this.location = await this.commonUtilityService.getCurrentLocationPromise()
+    // if (location) {
+    //   this.currentLat = Number(location.coords.latitude);
+    //   this.currentLong = Number(location.coords.longitude);
+    // }
   }
 }
