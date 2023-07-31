783b84782089e7da0971ab3ba8facd715fb21342
vivid-cpreston <97257824+vivid-cpreston@users.noreply.github.com>
2023-07-31 14:52:56 -0700

    Add lambdas to VPC (#992)
    * Fix issue when IS_HOTFIX is false
    
    * deployment updates
    
    * Syntax fix for properties
    
    * Removed unused properties, added manual terragrunt deploy option
    
    * Update variable and property names
    
    * Updates to resource names
    
    * fix naming
    
    * Added dependency between queues
    
    * Cast number to string
    
    * Fixed mismatched variable use
    
    * Added mandatory listener rule
    
    * Removed unused resource definitions from code, uncommented environment variables for wfone-notifications-api
    
    * Updated variable names
    
    * Updated properties to account for each instance only monitoring one queue instead of all queues
    
    * pick up missing variable
    
    * Added hash checking for lambdas
    
    * updated value used for pointid api listener
    
    * Added missing call to systemProperties()
    
    * endpoint fix for health checks
    
    * Added firebase config settings
    
    * Updated manual deploy
    
    * Move secret name to secret instead of input
    
    * Added missing environment statement
    
    * Switched to inherited secrets
    
    * removed bad change
    
    * Updated zip args and steps to fix path and hashsum issues
    
    * DDL Changes
    
    * moved changelog file
    
    * moved main changelog for wfnews, updated lambda secret value
    
    * Added lambdas to VPC
    
    ---------
    
    Co-authored-by: Dylan <dylan.hemsworth@gmail.com>

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -43,6 +43,10 @@ resource "aws_lambda_function" "monitor-bans-prohibitions" {
       WFNEWS_API  = aws_route53_record.wfnews_apisix.name
     }
   }
+  vpc_config {
+    subnet_ids         = module.network.aws_subnet.web.ids
+    security_group_ids = [module.network.aws_security_group.web.id]
+  }
 }
 
 resource "aws_lambda_function" "monitor-active-fires" {
@@ -62,6 +66,10 @@ resource "aws_lambda_function" "monitor-active-fires" {
       SECRET_NAME = var.SECRET_NAME
       WFNEWS_API  = aws_route53_record.wfnews_apisix.name
     }
+  }
+  vpc_config {
+    subnet_ids         = module.network.aws_subnet.web.ids
+    security_group_ids = [module.network.aws_security_group.web.id]
   }
 }
 
@@ -83,6 +91,10 @@ resource "aws_lambda_function" "monitor-area-restrictions" {
       WFNEWS_API  = aws_route53_record.wfnews_apisix.name
     }
   }
+  vpc_config {
+    subnet_ids         = module.network.aws_subnet.web.ids
+    security_group_ids = [module.network.aws_security_group.web.id]
+  }
 }
 
 resource "aws_lambda_function" "monitor-evacuation" {
@@ -102,5 +114,9 @@ resource "aws_lambda_function" "monitor-evacuation" {
       SECRET_NAME = var.SECRET_NAME
       WFNEWS_API  = aws_route53_record.wfnews_apisix.name
     }
+  }
+  vpc_config {
+    subnet_ids         = module.network.aws_subnet.web.ids
+    security_group_ids = [module.network.aws_security_group.web.id]
   }
 }
