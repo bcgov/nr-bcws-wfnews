1d689d9c4499d30aaac2bf716501c8f9a955a5c4
Chris Preston <cpreston@vividsolutions.com>
2023-08-28 11:29:35 -0700

    Removed unused apisix-related resources

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -56,30 +56,6 @@ resource "aws_route53_record" "wfnews_nginx" {
   }
 }
 
-/*
- resource "aws_route53_record" "wfnews_nginx_admin" {
-     zone_id = data.aws_route53_zone.zone.id
-     name = "wfnews-api-admin.${var.target_env}.bcwildfireservices.com"
-     type = "A"
-     alias { 
-        name = aws_cloudfront_distribution.wfnews_geofencing_nginx_admin[0].domain_name
-        zone_id = aws_cloudfront_distribution.wfnews_geofencing_nginx_admin[0].hosted_zone_id
-        evaluate_target_health = true
-    }
- }
-
-  resource "aws_route53_record" "wfnews_nginx_gui" {
-     zone_id = data.aws_route53_zone.zone.id
-     name = "wfnews-api-gui.${var.target_env}.bcwildfireservices.com"
-     type = "A"
-     alias { 
-        name = aws_cloudfront_distribution.wfnews_geofencing_nginx_gui[0].domain_name
-        zone_id = aws_cloudfront_distribution.wfnews_geofencing_nginx_gui[0].hosted_zone_id
-        evaluate_target_health = true
-    }
- }
-*/
-
 resource "aws_route53_record" "wfss_pointid" {
   //We will eventually phase out old URLs, but use them for now
   //count = var.target_env == "prod" ? 0 : 1
