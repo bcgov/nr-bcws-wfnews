f77c322794a5d40559a2320a7dd85a391891f318
Lucas Li <yzlucas@outlook.com>
2023-08-30 20:50:00 -0700

    wip

--- client/wfnews-war/src/main/angular/src/app/components/report-of-fire/review-page/rof-review-page.component.ts
+++ client/wfnews-war/src/main/angular/src/app/components/report-of-fire/review-page/rof-review-page.component.ts
@@ -2,6 +2,7 @@ import { Component, ChangeDetectionStrategy } from "@angular/core";
 import { RoFPage } from "../rofPage";
 import { ReportOfFire } from "../reportOfFireModel";
 import ConfigJson from '../report-of-fire.config.json';
+import { startWith } from "rxjs/operators";
 
 
 @Component({
@@ -33,8 +34,46 @@ export class RoFReviewPage extends RoFPage {
     this.reportOfFirePages = this.reportOfFirePages.filter(page => !pagesToRemove.includes(page.id));
   }
 
+  selectedAnswer(page:any) {
+    switch(page.id){
+      case 'contact-page' : 
+        if (this.reportOfFire.consentToCall) {
+          
+        }
+        return this.reportOfFire.consentToCall ? 'Yes' : 'No';
+      case 'location-page' :
+        return this.reportOfFire.fireLocation
+    }
+    console.log(page);
+    // return 'Value001'
+  }
+
+  selectedAnswerPart2(page: any) {
+    switch(page.id) {
+      case 'contact-page' :
+        return (this.reportOfFire.fullName) + '\n' +(this.reportOfFire.phoneNumber)
+    }
+  }
+
+  twoPartsQuestions(page:any) {
+    switch(page.id) {
+      case 'contact-page' :
+        if (this.reportOfFire.consentToCall && this.reportOfFire.fullName && this.reportOfFire.phoneNumber) {
+          debugger
+          return true
+        }
+      default:
+        return false;
+    }
+  }
+
   parseJson () {
     console.log(ConfigJson)
     return JSON.stringify(this.reportOfFire);
+  }
+
+  edit() {
+    console.log('EDIT')
+    //todo: navigate to the step page
   }
 }
