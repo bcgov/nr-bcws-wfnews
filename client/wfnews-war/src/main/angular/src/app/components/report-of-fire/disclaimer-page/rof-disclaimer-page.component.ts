7bc7f3fa95e145cddd1be3a47a164edef5f4239b
Sean Sylver <ssylver@VividSolutions.com>
2023-09-19 12:23:38 -0700

    remove unused close attribute and set smaller height for image

--- client/wfnews-war/src/main/angular/src/app/components/report-of-fire/disclaimer-page/rof-disclaimer-page.component.ts
+++ client/wfnews-war/src/main/angular/src/app/components/report-of-fire/disclaimer-page/rof-disclaimer-page.component.ts
@@ -9,13 +9,11 @@ import { ReportOfFire } from '../reportOfFireModel';
 })
 export class RoFDisclaimerPage extends RoFPage {
   public imageUrl: string
-  public closeButton: boolean
   public message: string;
 
   initialize (data: any, index: number, reportOfFire: ReportOfFire) {
     super.initialize(data, index, reportOfFire);
     this.imageUrl = data.imageUrl;
-    this.closeButton = data.closeButton;
     this.message = data.message.split('\n');
     
   }
