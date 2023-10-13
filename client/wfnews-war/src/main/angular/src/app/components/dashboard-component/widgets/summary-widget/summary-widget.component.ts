a32a8fc8649eeac6a191281704fa66c6962c4e3a
Dylan Hemsworth <dylan.hemsworth@gmail.com>
2023-10-13 14:46:54 -0700

    WFNEWS-1503

--- client/wfnews-war/src/main/angular/src/app/components/dashboard-component/widgets/summary-widget/summary-widget.component.ts
+++ client/wfnews-war/src/main/angular/src/app/components/dashboard-component/widgets/summary-widget/summary-widget.component.ts
@@ -1,12 +1,39 @@
-import { Component } from "@angular/core"
+import { AfterViewInit, Component } from "@angular/core"
+import { PublishedIncidentService } from "@app/services/published-incident-service"
 
 @Component({
   selector: 'summary-widget',
   templateUrl: './summary-widget.component.html',
   styleUrls: ['./summary-widget.component.scss']
 })
-export class SummaryWidget {
+export class SummaryWidget implements AfterViewInit {
   public startupComplete = false
-  
-  constructor() { }
+
+  public activeFires: string
+  public starts24hour: string
+  public starts7Day: string
+  public out7Day: string
+
+  constructor(private publishedIncidentService: PublishedIncidentService) { }
+
+  ngAfterViewInit (): void {
+    Promise.all([
+      this.publishedIncidentService.getActiveFireCount(),
+      this.publishedIncidentService.fetchPublishedIncidents().toPromise(),
+      this.publishedIncidentService.fetchPublishedIncidents(0, 9999, true, false).toPromise(),
+      this.publishedIncidentService.fetchOutIncidents(0, 9999).toPromise()
+    ]).then(([activeCount, activeIncidents, activeFoNIncidents,outIncidents ]) => {
+      const outFires = outIncidents.collection.filter(f => f.stageOfControlCode === 'OUT')
+      const fires = activeIncidents.collection.filter(f => f.stageOfControlCode !== 'OUT').concat(activeFoNIncidents.collection.filter(f => f.stageOfControlCode !== 'OUT'))
+
+      this.activeFires = '' + activeCount
+      this.starts24hour = '' + (fires.filter(f => f.discoveryDate > Date.now() - 86400000).length + outFires.filter(f => f.discoveryDate > Date.now() - 86400000).length)
+      this.starts7Day = '' + (fires.filter(f => f.discoveryDate > Date.now() - 604800000).length + outFires.filter(f => f.discoveryDate > Date.now() - 604800000).length)
+      this.out7Day = '' + (outFires.filter(f => f.discoveryDate > Date.now() - 604800000).length)
+
+      this.startupComplete = true
+    }).catch(err => {
+      console.error(err)
+    })
+  }
 }
