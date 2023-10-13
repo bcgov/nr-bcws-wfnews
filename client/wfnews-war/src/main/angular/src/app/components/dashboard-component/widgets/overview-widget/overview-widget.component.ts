98a5cf7577fdee6954f571dae6059b82ab518b89
Dylan Hemsworth <dylan.hemsworth@gmail.com>
2023-10-13 13:52:35 -0700

    WFNEWS-1491

--- client/wfnews-war/src/main/angular/src/app/components/dashboard-component/widgets/overview-widget/overview-widget.component.ts
+++ client/wfnews-war/src/main/angular/src/app/components/dashboard-component/widgets/overview-widget/overview-widget.component.ts
@@ -1,12 +1,76 @@
-import { Component } from "@angular/core"
+import { AfterViewInit, ChangeDetectorRef, Component, ElementRef, OnInit } from "@angular/core"
+import * as L from 'leaflet'
+import { AppConfigService } from "@wf1/core-ui"
+import { HttpClient } from "@angular/common/http"
 
 @Component({
   selector: 'overview-widget',
   templateUrl: './overview-widget.component.html',
   styleUrls: ['./overview-widget.component.scss']
 })
-export class OverviewWidget {
+export class OverviewWidget implements OnInit, AfterViewInit {
   public startupComplete = false
-  
-  constructor() { }
+  public map: any = null
+
+  constructor(private appConfigService: AppConfigService, private cdr: ChangeDetectorRef, private httpClient: HttpClient, private el: ElementRef) { }
+
+  ngOnInit (): void {
+    const observer = new ResizeObserver(() => {
+      if (this.map) {
+        this.map.invalidateSize()
+      }
+    }).observe(this.el.nativeElement)
+  }
+
+  ngAfterViewInit (): void {
+    console.log('After View Init')
+    // load the incident points from the API
+    const url = `${this.appConfigService.getConfig().rest['wfnews']}/publicPublishedIncident/features?stageOfControl=`
+    this.httpClient.get(url, { headers: { apikey: this.appConfigService.getConfig().application['wfnewsApiKey']} }).toPromise()
+
+    Promise.all([
+      this.httpClient.get(url + 'FIRE_OF_NOTE', { headers: { apikey: this.appConfigService.getConfig().application['wfnewsApiKey']} }).toPromise(),
+      this.httpClient.get(url + 'OUT_CNTRL', { headers: { apikey: this.appConfigService.getConfig().application['wfnewsApiKey']} }).toPromise(),
+      this.httpClient.get(url + 'HOLDING', { headers: { apikey: this.appConfigService.getConfig().application['wfnewsApiKey']} }).toPromise(),
+      this.httpClient.get(url + 'UNDR_CNTRL', { headers: { apikey: this.appConfigService.getConfig().application['wfnewsApiKey']} }).toPromise()
+    ]).then(([firesOfNote, outOfControl, holding, underControl]) => {
+      // set startupComplete to true
+      this.startupComplete = true
+      this.cdr.detectChanges()
+
+      // Create map and append data to the map component
+      const southWest = L.latLng(60.2, -116),
+            northEast = L.latLng(49, -136.3),
+            bounds = L.latLngBounds(southWest, northEast),
+            location = [Number(55), Number(-126)]
+
+      this.map = L.map('dashboard-map', {
+        maxBounds: bounds,
+        attributionControl: false,
+        zoomControl: false,
+        dragging: false,
+        doubleClickZoom: false,
+        boxZoom: false,
+        trackResize: false,
+        scrollWheelZoom: false,
+        maxZoom: 5
+      }).setView(location, 5)
+      // configure map data
+      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      }).addTo(this.map)
+
+      const fonIcon = L.icon({iconUrl: "/assets/images/local_fire_department.png", iconSize: [20, 20], shadowAnchor: [4, 42], shadowSize: [31, 31]})
+      const ucIcon = L.icon({iconUrl: "/assets/images/svg-icons/under-control.svg", iconSize: [10, 10], shadowAnchor: [4, 32], shadowSize: [10, 10] })
+      const holdIcon = L.icon({iconUrl: "/assets/images/svg-icons/being-held.svg", iconSize: [10, 10], shadowAnchor: [4, 32], shadowSize: [10, 10] })
+      const oocIcon = L.icon({iconUrl: "/assets/images/svg-icons/out-of-control.svg", iconSize: [10, 10], shadowAnchor: [4, 32], shadowSize: [10, 10] })
+
+      L.geoJSON(underControl, { pointToLayer: function(feature, latlng) { return L.marker(latlng, { icon: ucIcon })}}).addTo(this.map)
+      L.geoJSON(holding, { pointToLayer: function(feature, latlng) { return L.marker(latlng, { icon: holdIcon })}}).addTo(this.map)
+      L.geoJSON(outOfControl, { pointToLayer: function(feature, latlng) { return L.marker(latlng, { icon: oocIcon })}}).addTo(this.map)
+      L.geoJSON(firesOfNote, { pointToLayer: function(feature, latlng) { return L.marker(latlng, { icon: fonIcon })}}).addTo(this.map)
+
+      this.cdr.detectChanges()
+    })
+  }
 }
