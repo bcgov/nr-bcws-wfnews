a5a89ab5d23d6a37e04918d2347fc26998c4c083
Dylan Hemsworth <dylan.hemsworth@gmail.com>
2023-10-13 19:07:46 -0700

    Charts, button css

--- client/wfnews-war/src/main/angular/src/app/components/dashboard-component/widgets/active-fires-widget/active-fires-widget.component.ts
+++ client/wfnews-war/src/main/angular/src/app/components/dashboard-component/widgets/active-fires-widget/active-fires-widget.component.ts
@@ -16,13 +16,19 @@ export class ActiveFiresWidget implements AfterViewInit {
   public activeOutOfControl: number
   public activeBeingHeld: number
   public activeUnderControl: number
+  public activeOutOfControlPct: number
+  public activeBeingHeldPct: number
+  public activeUnderControlPct: number
   public outOfControlData = []
   public beingHeldData = []
   public underControlData = []
 
+
   public outOfControlScheme = { domain: ['#FF0000', '#C2C2C2'] }
   public beingHeldScheme = { domain: ['#DADA19', '#C2C2C2'] }
   public underControlScheme = { domain: ['#98E600', '#C2C2C2'] }
+
+  public chartSize = 180
 
   constructor(private publishedIncidentService: PublishedIncidentService) { }
 
@@ -51,6 +57,10 @@ export class ActiveFiresWidget implements AfterViewInit {
       this.outOfControlData = [{ name: "Out of Control", value: this.activeOutOfControl }, { name: "All Fires", value: this.activeFires }]
       this.beingHeldData = [{ name: "Being Held", value: this.activeBeingHeld }, { name: "All Fires", value: this.activeFires }]
       this.underControlData = [{ name: "Under Control", value: this.activeUnderControl }, { name: "All Fires", value: this.activeFires }]
+
+      this.activeOutOfControlPct = Math.round((this.activeOutOfControl / this.activeFires) * 100) || 0
+      this.activeBeingHeldPct = Math.round((this.activeBeingHeld / this.activeFires) * 100) || 0
+      this.activeUnderControlPct = Math.round((this.activeUnderControl / this.activeFires) * 100) || 0
 
       this.startupComplete = true
     }).catch(err => {
