d3a9ed197d4c2c7c93f6e61a90d0876d71e55e03
Dylan Hemsworth <dylan.hemsworth@gmail.com>
2023-10-13 20:15:35 -0700

    Evac widget content

--- client/wfnews-war/src/main/angular/src/app/components/dashboard-component/widgets/evacuations-widget/evacuations-widget.component.ts
+++ client/wfnews-war/src/main/angular/src/app/components/dashboard-component/widgets/evacuations-widget/evacuations-widget.component.ts
@@ -1,12 +1,56 @@
-import { Component } from "@angular/core"
+import { AfterViewInit, Component } from "@angular/core"
+import { AGOLService } from "@app/services/AGOL-service"
+import moment from "moment"
 
 @Component({
   selector: 'evacuations-widget',
   templateUrl: './evacuations-widget.component.html',
   styleUrls: ['./evacuations-widget.component.scss']
 })
-export class EvacuationsWidget {
+export class EvacuationsWidget implements AfterViewInit {
   public startupComplete = false
-  
-  constructor() { }
+  public evacOrders = 0
+  public evacAlerts = 0
+  public evacList = []
+
+  constructor(private agolService: AGOLService) { }
+
+  ngAfterViewInit (): void {
+    this.agolService.getEvacOrders(null, null, { returnCentroid: false, returnGeometry: false})
+    .toPromise().then(evacs => {
+      if (evacs && evacs.features) {
+
+        this.evacOrders = evacs.features.filter(e => e.attributes.ORDER_ALERT_STATUS === 'Order').length
+        this.evacAlerts = evacs.features.filter(e => e.attributes.ORDER_ALERT_STATUS === 'Alerts').length
+
+        for (const element of evacs.features) {
+          this.evacList.push({
+            name: element.attributes.EVENT_NAME,
+            eventType: element.attributes.EVENT_TYPE,
+            status: element.attributes.ORDER_ALERT_STATUS,
+            agency: element.attributes.ISSUING_AGENCY,
+            preOcCode: element.attributes.PREOC_CODE,
+            emrgOAAsysID: element.attributes.EMRG_OAA_SYSID,
+            issuedOn: this.convertToDate(element.attributes.DATE_MODIFIED),
+            issuedOnRaw: element.attributes.DATE_MODIFIED
+          })
+        }
+
+        // sort the list by date
+        this.evacList.sort((a,b) =>(a.issuedOnRaw > b.issuedOnRaw) ? 1 : ((b.issuedOnRaw > a.issuedOnRaw) ? -1 : 0))
+        // only keep the first 4 or 5
+        if (this.evacList.length > 5) {
+          this.evacList = this.evacList.slice(0, 5);
+        }
+      }
+
+      this.startupComplete = true
+    })
+  }
+
+  convertToDate(value: string) {
+    if (value) {
+      return moment(value).format('YYYY-MM-DD HH:mm:ss')
+    }
+  }
 }
