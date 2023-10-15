074bbb70a596179d1fbb05d674577d8c2fbe1bda
Dylan Hemsworth <dylan.hemsworth@gmail.com>
2023-10-15 14:09:50 -0700

    Blog widget content

--- client/wfnews-war/src/main/angular/src/app/components/dashboard-component/widgets/blog-widget/blog-widget.component.ts
+++ client/wfnews-war/src/main/angular/src/app/components/dashboard-component/widgets/blog-widget/blog-widget.component.ts
@@ -1,12 +1,17 @@
-import { Component } from "@angular/core"
+import { AfterViewInit, Component } from "@angular/core"
 
 @Component({
   selector: 'blog-widget',
   templateUrl: './blog-widget.component.html',
   styleUrls: ['./blog-widget.component.scss']
 })
-export class BlogWidget {
+export class BlogWidget implements AfterViewInit{
   public startupComplete = false
-  
+
   constructor() { }
+
+  ngAfterViewInit (): void {
+    // We probably have nothing to do for this widget
+    this.startupComplete = true
+  }
 }
