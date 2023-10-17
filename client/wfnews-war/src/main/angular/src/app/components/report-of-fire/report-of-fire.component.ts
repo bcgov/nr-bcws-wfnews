30a7e32f5c506c0e5909c6381164094e9b1891d5
Lucas Li <yzlucas@outlook.com>
2023-10-17 09:21:08 -0700

    revert final page dialog back

--- client/wfnews-war/src/main/angular/src/app/components/report-of-fire/report-of-fire.component.ts
+++ client/wfnews-war/src/main/angular/src/app/components/report-of-fire/report-of-fire.component.ts
@@ -292,9 +292,6 @@ export class ReportOfFirePage implements OnInit, AfterContentInit {
       this.isEditMode = false;
       this.cdr.detectChanges();
     }else {
-      if(this.currentPage.instance.id === 'final-page') {
-        this.router.navigateByUrl('/map')
-      } else {
         let dialogRef = this.dialog.open(DialogExitComponent, {
           autoFocus: false,
           width: '80vw',
@@ -305,7 +302,6 @@ export class ReportOfFirePage implements OnInit, AfterContentInit {
             this.router.navigateByUrl('/map')
           }
         });
-      }
     }
   }
 
