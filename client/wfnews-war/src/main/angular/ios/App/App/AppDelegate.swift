bda53801e9ff6f1316eb84e23fc9fdbc3583fe54
Sean Sylver <ssylver@VividSolutions.com>
2023-10-19 12:08:06 -0700

    Update AppDelegate.swift

--- client/wfnews-war/src/main/angular/ios/App/App/AppDelegate.swift
+++ client/wfnews-war/src/main/angular/ios/App/App/AppDelegate.swift
@@ -22,6 +22,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        var finished = false;
+        var bgTask: UIBackgroundTaskIdentifier = 0;
+        bgTask = application.beginBackgroundTask(expirationHandler: {() -> Void in
+            while bgTask != UIBackgroundTaskInvalid {
+                // continue
+            }
+            application.endBackgroundTask(bgTask)
+            bgTask = UIBackgroundTaskInvalid
+        })
+
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
