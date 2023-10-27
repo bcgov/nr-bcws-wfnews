acdb70983b62f86b3b340204b44d7db99fe45fcf
Sukhpal <6563909+sukhpalp@users.noreply.github.com>
2023-10-27 01:16:17 -0700

    Update styling and add drag breakpoints

--- client/wfnews-war/src/main/angular/src/app/components/common/mobile-sliding-drawer/mobile-sliding-drawer.component.ts
+++ client/wfnews-war/src/main/angular/src/app/components/common/mobile-sliding-drawer/mobile-sliding-drawer.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectorRef, Component } from '@angular/core';
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'mobile-sliding-drawer',
@@ -6,13 +6,81 @@ import { ChangeDetectorRef, Component } from '@angular/core';
   styleUrls: ['./mobile-sliding-drawer.component.scss']
 })
 export class MobileSlidingDrawerComponent {
-  showPanel: boolean;
+  @Input() isVisible: boolean;
+  @Input() title: string;
 
-  constructor(
-    protected cdr: ChangeDetectorRef,
-  ) {}
+  @ViewChild('drawerElement')
+  drawerElement: ElementRef;
+
+  @ViewChild('drawerHeaderElement')
+  drawerHeaderElement: ElementRef;
+
+  dragPosition = { x: 0, y: 0 };
 
   closePanel() {
-    this.showPanel = false;
+    this.isVisible = false;
+  }
+
+  calculatePosition(endDragEvent) {
+    const deltaY = endDragEvent.distance.y;
+    if ((deltaY < -100 && this.isDefaultPosition()) || (deltaY < (-1 * this.getDrawerHeight())) ) {
+      this.setToFullScreen();
+    } else if ((deltaY > 100 && this.isDefaultPosition()) || (deltaY > (this.getDrawerHeight())) ) {
+      this.setToMinimized();
+    } else if ((deltaY < -100 && this.isMinimized()) || (deltaY > 100 && this.isFullScreen())) {
+      this.setToDefaultPosition();
+    }
+  }
+
+  setPosition() {
+    this.dragPosition = { x: 0, y: (this.isFullScreen() ? 0 : this.getTopPosition()) };
+  }
+
+  closeDrawer() {
+    this.setToMinimized();
+  }
+
+  getHeaderHeight() {
+    return this.drawerHeaderElement?.nativeElement.offsetHeight as number;
+  }
+
+  getDrawerHeight() {
+    return this.drawerElement?.nativeElement.offsetHeight as number;
+  }
+
+  getDefaultPosition() {
+    return 0;
+  }
+
+  getTopPosition() {
+    return (window.innerHeight * -1) + this.getDrawerHeight();
+  }
+
+  getMinimizedPosition() {
+    return this.getDrawerHeight() - this.getHeaderHeight();
+  }
+
+  isDefaultPosition() {
+    return this.dragPosition.y === this.getDefaultPosition();
+  }
+
+  isFullScreen() {
+    return this.dragPosition.y === this.getTopPosition();
+  }
+
+  isMinimized() {
+    return this.dragPosition.y === this.getMinimizedPosition();
+  }
+
+  setToDefaultPosition() {
+    this.dragPosition = { x: 0, y: this.getDefaultPosition() };
+  }
+
+  setToFullScreen() {
+    this.dragPosition = { x: 0, y: this.getTopPosition() };
+  }
+
+  setToMinimized() {
+    this.dragPosition = { x: 0, y: this.getMinimizedPosition() };
   }
 }
