10ca6a5b81d043b39ced18ddc2c8133af99ee0fd
Sukh <6563909+sukhpalp@users.noreply.github.com>
2023-11-01 15:09:26 -0700

    Fix hide based on platform

--- client/wfnews-war/src/main/angular/src/app/components/report-of-fire/dialog-location/dialog-location.component.ts
+++ client/wfnews-war/src/main/angular/src/app/components/report-of-fire/dialog-location/dialog-location.component.ts
@@ -1,18 +1,26 @@
-import { Component } from '@angular/core';
-import { MatDialogRef } from '@angular/material/dialog';
-import { CapacitorService } from '@app/services/capacitor-service';
-import { AndroidSettings, IOSSettings, NativeSettings } from 'capacitor-native-settings';
-
+import { Component } from "@angular/core";
+import { MatDialogRef } from "@angular/material/dialog";
+import { CapacitorService } from "@app/services/capacitor-service";
+import {
+  AndroidSettings,
+  IOSSettings,
+  NativeSettings,
+} from "capacitor-native-settings";
 
 @Component({
-  selector: 'wfnews-dialog-location',
-  templateUrl: './dialog-location.component.html',
-  styleUrls: ['./dialog-location.component.scss']
+  selector: "wfnews-dialog-location",
+  templateUrl: "./dialog-location.component.html",
+  styleUrls: ["./dialog-location.component.scss"],
 })
 export class DialogLocationComponent {
   showSettingsButton = false;
 
-  constructor(private dialogRef: MatDialogRef<DialogLocationComponent>, private capacitorService: CapacitorService) { }
+  constructor(
+    private dialogRef: MatDialogRef<DialogLocationComponent>,
+    private capacitorService: CapacitorService
+  ) {
+    this.checkOperatingSystem();
+  }
 
   closeDialog() {
     this.dialogRef.close();
@@ -20,21 +28,22 @@ export class DialogLocationComponent {
 
   async checkOperatingSystem() {
     const device = await this.capacitorService.checkDeviceSystem();
-    this.showSettingsButton =  (device.operatingSystem === 'ios' || device.operatingSystem === 'android');
+    this.showSettingsButton =
+      (device.operatingSystem === "ios" || device.operatingSystem === "android") &&
+      device.platform !== "web";
   }
-  
+
   async goToSetting() {
-    const device = (await this.capacitorService.checkDeviceSystem())
-    if (device.operatingSystem === 'ios') {
+    const device = await this.capacitorService.checkDeviceSystem();
+    if (device.operatingSystem === "ios") {
       NativeSettings.openIOS({
         option: IOSSettings.App,
       });
-    }
-    else if (device.operatingSystem === 'android') {
+    } else if (device.operatingSystem === "android") {
       NativeSettings.openAndroid({
         option: AndroidSettings.ApplicationDetails,
       });
     }
-    this.dialogRef.close()
+    this.dialogRef.close();
   }
 }
