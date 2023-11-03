3607c85d7ecf4ee10ad91206e13ee14bce1aee0f
Lucas Li <35748253+yzlucas@users.noreply.github.com>
2023-11-02 22:34:18 -0700

    Wfnews 1582 (#1277)
    * wfnews-1582
    
    * reafactor the zoom time out
    
    * 1609
    
    * change mat-icon to img
    
    * 1610 1611
    
    * clear identify icon when close panel
    
    * change self to this
    
    * change back to self
    
    ---------
    
    Co-authored-by: Dylan <dylan.hemsworth@gmail.com>

--- client/wfnews-war/src/main/angular/src/app/components/wf-map-container/wf-map-container.component.ts
+++ client/wfnews-war/src/main/angular/src/app/components/wf-map-container/wf-map-container.component.ts
@@ -119,7 +119,7 @@ export class WFMapContainerComponent implements OnDestroy, OnChanges {
     
     // Function to handle the timeout logic
     function handleTimeout() {
-      this.selectIncidents.emit(identified.featureSet);
+      self.selectIncidents.emit(identified.featureSet);
     }
     
     // Set a timeout to emit selectIncidents event after 500 milliseconds
