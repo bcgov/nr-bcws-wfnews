9eafd0aa6b4c3538d0ee7f15ec827e1bda180d10
Lucas Li <35748253+yzlucas@users.noreply.github.com>
2023-11-06 16:10:55 -0800

    Wfnews 1552 (#1284)
    * styling done
    
    * router

--- client/wfnews-war/src/main/angular/src/app/components/more/more.component.ts
+++ client/wfnews-war/src/main/angular/src/app/components/more/more.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ResourcesRoutes } from '@app/utils';
 
 @Component({
   selector: 'wfnews-more',
@@ -7,4 +9,34 @@ import { Component } from '@angular/core';
 })
 export class MoreComponent {
 
+  constructor(private router: Router) {
+    
+  }
+
+  navigate(menu){
+    switch (menu){
+      case 'wildfire-list' :
+        this.router.navigate([ResourcesRoutes.WILDFIRESLIST]);
+        break;
+      case 'resources' :
+        this.router.navigate([ResourcesRoutes.RESOURCES]);
+        break;
+      case 'contact-us' :
+        // contact-us component has not implemented yet
+        break;
+      case 'blog':
+        window.open('https://blog.gov.bc.ca/bcwildfire/', '_blank');
+        break;
+      case 'facebook':
+        window.open('https://www.facebook.com/BCForestFireInfo/', '_blank');
+        break;
+      case 'youtube':
+        window.open('https://www.youtube.com/@BCWildfireService', '_blank');
+        break;
+      case 'twitter':
+        window.open('https://twitter.com/BCGovFireInfo', '_blank');
+        break;
+    }
+
+  }
 }
