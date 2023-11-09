8711af0e8033c755722b8f8384eea8313f94b89b
ssylver93 <107515688+ssylver93@users.noreply.github.com>
2023-11-09 11:56:01 -0800

    WFNEWS-1397 routing for mobile full details (#1295)
    * WFNEWS-1397 Full Details Overview Mobile
    
    * Remove logging
    
    * WFNEWS-1397 updates
    
    * WFNEWS-1397 uppdates
    
    * Fixes fro styling after code review, more missing icons
    
    * Full details css adjustments
    
    * CSS updates
    
    * CSS updates
    
    * WFNEWS-1397 routing for mobile full details
    
    * WFNEWS-1397 routing for mobile full details
    
    * WFNEWS-1397 routing for mobile full details

--- client/wfnews-war/src/main/angular/src/app/components/draggable-panel/draggable-panel.component.ts
+++ client/wfnews-war/src/main/angular/src/app/components/draggable-panel/draggable-panel.component.ts
@@ -1,5 +1,5 @@
-import { ChangeDetectorRef, Component,OnChanges, OnInit, SimpleChanges} from '@angular/core';
-import {Input} from '@angular/core';
+import { ChangeDetectorRef, Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Input } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { PublishedIncidentService } from '@app/services/published-incident-service';
 import { MapConfigService } from '@app/services/map-config.service';
@@ -16,7 +16,7 @@ import { ResourcesRoutes, convertToDateYear } from '@app/utils';
 export class DraggablePanelComponent implements OnInit, OnChanges {
   resizeHeight: string = '10vh'; // Initial height of the panel
   @Input() incidentRefs: any[];
-  storedIncidentRefs: any [];
+  storedIncidentRefs: any[];
   filteredWildfires: any[];
   filteredFirePerimeters: any[];
   filteredEvacs: any[];
@@ -33,7 +33,7 @@ export class DraggablePanelComponent implements OnInit, OnChanges {
   filteredIndianReserve: any[];
   showPanel: boolean;
   allowBackToIncidentsPanel: boolean;
-  identifyItem:any;
+  identifyItem: any;
   identifyIncident: any = {};
   wildfireLayerIds: string[] = [
     'active-wildfires-fire-of-note',
@@ -52,15 +52,15 @@ export class DraggablePanelComponent implements OnInit, OnChanges {
     protected http: HttpClient,
     private mapConfigService: MapConfigService,
     private router: Router
-    ) {
-    }
+  ) {
+  }
 
 
   ngOnInit(): void {
 
   }
 
-  ngOnChanges(changes: SimpleChanges){
+  ngOnChanges(changes: SimpleChanges) {
     this.showPanel = false;
     this.identifyIncident = null;
     this.handleLayersSelection()
@@ -71,7 +71,7 @@ export class DraggablePanelComponent implements OnInit, OnChanges {
       // clicked back from preiview panel
       this.incidentRefs = this.storedIncidentRefs
     }
-    if (this.incidentRefs.length === 1){
+    if (this.incidentRefs.length === 1) {
       this.zoomIn(8)
       // single feature within clicked area
       this.showPanel = true;
@@ -121,7 +121,7 @@ export class DraggablePanelComponent implements OnInit, OnChanges {
     else {
       if (wildfire.properties.incident_name) {
         return wildfire.properties.incident_name + ' (' + wildfire.properties.incident_number_label + ')';
-      }else {
+      } else {
         return wildfire.properties.incident_number_label + ' Wildfire';
       }
     }
@@ -130,7 +130,7 @@ export class DraggablePanelComponent implements OnInit, OnChanges {
   closePanel() {
     const SMK = window['SMK'];
     SMK.MAP[1].$viewer.identified.clear();
-    SMK.MAP[1].$sidepanel.setExpand( 0 )
+    SMK.MAP[1].$sidepanel.setExpand(0)
     this.cdr.detectChanges();
 
     this.showPanel = false;
@@ -140,78 +140,78 @@ export class DraggablePanelComponent implements OnInit, OnChanges {
 
 
   convertFirePerimeterFireStatus(status) {
-    switch(status) {
+    switch (status) {
       case 'Out of Control':
-          return 'active-wildfires-out-of-control'
+        return 'active-wildfires-out-of-control'
       case 'Being Held':
-          return 'active-wildfires-holding'
+        return 'active-wildfires-holding'
       case 'Under Control':
-          return 'active-wildfires-under-control'
+        return 'active-wildfires-under-control'
       case 'Out':
-          return 'bcws-activefires-publicview-inactive'
+        return 'bcws-activefires-publicview-inactive'
       default:
-          break;
+        break;
     }
   }
 
   displayItemTitle(identifyItem) {
-    switch(identifyItem.layerId) {
+    switch (identifyItem.layerId) {
       case 'active-wildfires-fire-of-note':
-        return  'Wildfire of Note';
+        return 'Wildfire of Note';
       case 'active-wildfires-out-of-control':
       case 'active-wildfires-under-control':
       case 'bcws-activefires-publicview-inactive':
       case 'active-wildfires-holding':
         return 'Wildfire'
-      }
+    }
   }
 
   displayTitleIcon(identifyItem) {
-    switch(identifyItem.layerId) {
+    switch (identifyItem.layerId) {
       case 'active-wildfires-out-of-control':
       case 'active-wildfires-under-control':
       case 'bcws-activefires-publicview-inactive':
       case 'active-wildfires-holding':
         return 'report'
-      }
+    }
   }
 
-  getStageOfControlLabel (code: string) {
+  getStageOfControlLabel(code: string) {
     if (code) {
       if (code.toUpperCase().trim() === 'OUT') return 'Out'
-        else if (code.toUpperCase().trim() === 'OUT_CNTRL') return 'Out of Control'
-        else if (code.toUpperCase().trim() === 'HOLDING') return 'Being Held'
-        else if (code.toUpperCase().trim() === 'UNDR_CNTRL') return 'Under Control'
-        else return 'Unknown'
+      else if (code.toUpperCase().trim() === 'OUT_CNTRL') return 'Out of Control'
+      else if (code.toUpperCase().trim() === 'HOLDING') return 'Being Held'
+      else if (code.toUpperCase().trim() === 'UNDR_CNTRL') return 'Under Control'
+      else return 'Unknown'
     }
   }
 
-  getStageOfControlIcon (code: string) {
+  getStageOfControlIcon(code: string) {
     if (code) {
       if (code.toUpperCase().trim() === 'OUT') return 'bcws-activefires-publicview-inactive'
-        else if (code.toUpperCase().trim() === 'OUT_CNTRL') return 'active-wildfires-out-of-control'
-        else if (code.toUpperCase().trim() === 'HOLDING') return 'active-wildfires-holding'
-        else if (code.toUpperCase().trim() === 'UNDR_CNTRL') return 'active-wildfires-under-control'
-        else return 'Unknown'
+      else if (code.toUpperCase().trim() === 'OUT_CNTRL') return 'active-wildfires-out-of-control'
+      else if (code.toUpperCase().trim() === 'HOLDING') return 'active-wildfires-holding'
+      else if (code.toUpperCase().trim() === 'UNDR_CNTRL') return 'active-wildfires-under-control'
+      else return 'Unknown'
     }
   }
 
-  getDescription (code: string) {
+  getDescription(code: string) {
     if (code) {
       if (code.toUpperCase().trim() === 'OUT') return "This wildfire is extinguished. Suppression efforts are complete."
-        else if (code.toUpperCase().trim() === 'OUT_CNTRL') return "This wildfire is continuing to spread and is not responding to suppression efforts."
-        else if (code.toUpperCase().trim() === 'HOLDING') return "This wildfire is not likely to spread beyond predetermined boundaries under current conditions."
-        else if (code.toUpperCase().trim() === 'UNDR_CNTRL') return "This wildfire will not spread any further due to suppression efforts."
-        else return 'Unknown'
+      else if (code.toUpperCase().trim() === 'OUT_CNTRL') return "This wildfire is continuing to spread and is not responding to suppression efforts."
+      else if (code.toUpperCase().trim() === 'HOLDING') return "This wildfire is not likely to spread beyond predetermined boundaries under current conditions."
+      else if (code.toUpperCase().trim() === 'UNDR_CNTRL') return "This wildfire will not spread any further due to suppression efforts."
+      else return 'Unknown'
     }
   }
 
-  zoomIn(level?:number) {
+  zoomIn(level?: number) {
     let long;
     let lat;
     if (this.identifyIncident && this.identifyIncident.longitude && this.identifyIncident.latitude) {
-       long = Number(this.identifyIncident.longitude);
-       lat = Number(this.identifyIncident.latitude);
+      long = Number(this.identifyIncident.longitude);
+      lat = Number(this.identifyIncident.latitude);
     } else if (this.identifyItem && this.identifyItem._identifyPoint.longitude && this.identifyItem._identifyPoint.latitude) {
       long = Number(this.identifyItem._identifyPoint.longitude);
       lat = Number(this.identifyItem._identifyPoint.latitude);
@@ -225,7 +225,7 @@ export class DraggablePanelComponent implements OnInit, OnChanges {
             viewer = SMK.MAP[smkMap].$viewer;
           }
         }
-        viewer.panToFeature(window['turf'].point([long, lat]), level? level: 12)
+        viewer.panToFeature(window['turf'].point([long, lat]), level ? level : 12)
       })
     }
   }
@@ -233,7 +233,7 @@ export class DraggablePanelComponent implements OnInit, OnChanges {
   openPreviewPanel(item) {
     this.allowBackToIncidentsPanel = true;
     this.storedIncidentRefs = this.incidentRefs
-        // capture the identify panel list;
+    // capture the identify panel list;
     this.identifyItem = item;
     this.incidentRefs = [item];
     this.cdr.detectChanges();
@@ -259,6 +259,10 @@ export class DraggablePanelComponent implements OnInit, OnChanges {
         this.router.navigate([ResourcesRoutes.FULL_DETAILS], { queryParams: { type: 'bans-prohibitions', id: item.properties.PROT_BAP_SYSID, source: [ResourcesRoutes.ACTIVEWILDFIREMAP]} });
       } else if (this.identifyItem.layerId === 'danger-rating'){
         this.router.navigate([ResourcesRoutes.FULL_DETAILS], { queryParams: { type: 'danger-rating', id: item.properties.DANGER_RATING_DESC, location: JSON.stringify(location), source: [ResourcesRoutes.ACTIVEWILDFIREMAP]} });
+      } else if (item.layerId === 'active-wildfires-fire-of-note' || item.layerId === 'active-wildfires-out-of-control'
+      || item.layerId === 'active-wildfires-holding' || item.layerId === 'active-wildfires-under-control' && (item.properties.fire_year && item.properties.incident_number_label)) {
+      this.router.navigate([ResourcesRoutes.PUBLIC_INCIDENT],
+        { queryParams: { fireYear: item.properties.fire_year, incidentNumber: item.properties.incident_number_label, source: [ResourcesRoutes.ACTIVEWILDFIREMAP] } })
       }
     }
   }
@@ -285,7 +289,7 @@ export class DraggablePanelComponent implements OnInit, OnChanges {
   }
 
   displayDangerRatingDes(danger) {
-    switch(danger) {
+    switch (danger) {
       case 'Extreme':
         return "Extremely dry forest fuels and the fire risk is very serious. New fires will start easily, spread rapidly, and challenge fire suppression efforts."
       case 'High':
@@ -309,10 +313,9 @@ export class DraggablePanelComponent implements OnInit, OnChanges {
         this.identifyItem.layerId === 'bc-fsr' ||
         this.identifyItem.layerId === 'abms-regional-districts' ||
         this.identifyItem.layerId === 'clab-indian-reserves' ||
-        this.identifyItem.layerId === 'abms-municipalities'))
-        {
-          return true
-        }
+        this.identifyItem.layerId === 'abms-municipalities')) {
+      return true
+    }
   }
 
   displayForestServiceRoadsAlert(item) {
@@ -328,22 +331,22 @@ export class DraggablePanelComponent implements OnInit, OnChanges {
     }
   }
 
-  showLocationIcon(layerId: string){
+  showLocationIcon(layerId: string) {
     if (layerId !== 'drive-bc-active-events'
       && layerId !== 'protected-lands-access-restrictions'
       && layerId !== 'bc-fsr'
       && layerId !== 'abms-regional-districts'
       && layerId !== 'clab-indian-reserves'
       && layerId !== 'abms-municipalities') {
-        return true;
-      }
+      return true;
+    }
   }
 
   showCalendarIcon(layerId: string) {
     if (layerId !== 'bc-fsr'
-    && layerId !== 'abms-regional-districts'
-    && layerId !== 'clab-indian-reserves'
-    && layerId !== 'abms-municipalities') {
+      && layerId !== 'abms-regional-districts'
+      && layerId !== 'clab-indian-reserves'
+      && layerId !== 'abms-municipalities') {
       return true;
     }
   }
@@ -355,13 +358,13 @@ export class DraggablePanelComponent implements OnInit, OnChanges {
   }
 
   displayLocalAuthorityType(layerId: string) {
-    if (layerId === 'abms-regional-districts' ) {
+    if (layerId === 'abms-regional-districts') {
       return 'Regional District'
     }
-    if (layerId === 'clab-indian-reserves' ) {
+    if (layerId === 'clab-indian-reserves') {
       return 'Indian Reserve'
     }
-    if (layerId === 'abms-municipalities' ) {
+    if (layerId === 'abms-municipalities') {
       return 'Municipality'
     }
   }
