1c6952e6f7db4ce71daf0dd4ba18fee39d9b6442
Lucas Li <yzlucas@outlook.com>
2023-11-14 11:51:30 -0800

    1607

--- client/wfnews-war/src/main/angular/src/app/components/report-of-fire/disclaimer-page/rof-disclaimer-page.component.ts
+++ client/wfnews-war/src/main/angular/src/app/components/report-of-fire/disclaimer-page/rof-disclaimer-page.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { RoFPage } from '../rofPage';
 import { ReportOfFire } from '../reportOfFireModel';
+import { CommonUtilityService } from '@app/services/common-utility.service';
+
 
 @Component({
   selector: 'rof-disclaimer-page',
@@ -10,11 +12,43 @@ import { ReportOfFire } from '../reportOfFireModel';
 export class RoFDisclaimerPage extends RoFPage {
   public imageUrl: string
   public message: string;
+  public currentBrowser: string;
+  public hideButtons : boolean;
+
+  public constructor(
+    private commonUtilityService: CommonUtilityService,
+    ) {
+    super();
+  }
 
   initialize (data: any, index: number, reportOfFire: ReportOfFire) {
     super.initialize(data, index, reportOfFire);
     this.imageUrl = data.imageUrl;
     this.message = data.message.split('\n');
-    
+    const userAgent = window.navigator.userAgent.toLowerCase();
+
+    // Check for the presence of certain keywords in the user agent to determine the browser
+    if (userAgent.includes('chrome')) {
+      this.currentBrowser = 'Google Chrome';
+    } else if (userAgent.includes('firefox')) {
+      this.currentBrowser = 'Mozilla Firefox';
+    } else if (userAgent.includes('safari') && !userAgent.includes('chrome')) {
+      this.currentBrowser = 'Apple Safari';
+    } else if (userAgent.includes('edge')) {
+      this.currentBrowser = 'Microsoft Edge';
+    } else if (userAgent.includes('trident') || userAgent.includes('msie')) {
+      this.currentBrowser = 'Internet Explorer';
+    } else {
+      this.currentBrowser = 'Unknown Browser';
+    }
+
+    if (this.currentBrowser === 'Unknown Browser' ) {
+      this.commonUtilityService.checkOnline().then((result) => {
+        if(!result) {
+          this.message = 'Offline reporting is only available in the BC Wildfire Service app which you can find on the Apple App Store or Google Play Store.'
+          this.hideButtons = true;
+        }
+      })
+    }
   }
 }
