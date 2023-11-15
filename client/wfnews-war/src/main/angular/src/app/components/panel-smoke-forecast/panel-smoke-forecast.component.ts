fce7fcaff8574f32010d945d00d2d9fd4cdf14f4
Dylan Hemsworth <dylan.hemsworth@gmail.com>
2023-11-15 14:41:15 -0800

    legend panel cleanup

--- client/wfnews-war/src/main/angular/src/app/components/panel-smoke-forecast/panel-smoke-forecast.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'panel-smoke-forecast',
-    templateUrl: './panel-smoke-forecast.component.html',
-    styleUrls: ['./panel-smoke-forecast.component.scss'],
-})
-export class PanelSmokeForecastComponent {
-}
--- client/wfnews-war/src/main/angular/src/app/containers/panelWildfireStageOfControl/panelWildfireStageOfControl-container.component.desktop.ts
+++ client/wfnews-war/src/main/angular/src/app/containers/panelWildfireStageOfControl/panelWildfireStageOfControl-container.component.desktop.ts
@@ -1,19 +1,20 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
 import {Location, LocationStrategy, PathLocationStrategy} from "@angular/common";
 import { PanelWildfireStageOfControlContainer } from "./panelWildfireStageOfControl-container.component";
 
 
 @Component({
-    selector: "panel-wildfire-stage-of-control-container-desktop",
+    selector: "desktop-legend-panel",
     template: `
         <panel-wildfire-stage-of-control
             [collection]="collection$ | async"
             [searchState]="searchState$ | async"
             [loadState]="loadState$ | async"
             [errorState]="errorState$ | async"
+            [selectedPanel]="selectedPanel"
         ></panel-wildfire-stage-of-control>`,
     providers: [Location, {provide: LocationStrategy, useClass: PathLocationStrategy}]
 })
 export class PanelWildfireStageOfControlContainerDesktop extends PanelWildfireStageOfControlContainer{
-
+  @Input() selectedPanel: string
 }
