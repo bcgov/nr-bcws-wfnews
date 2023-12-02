f1379c5692d6ff171016ed769aec300c84f3b838
Sean Sylver <ssylver@VividSolutions.com>
2023-12-01 17:31:01 -0800

    Register JDK8Module

--- server/wfss-pointid-api/wfss-pointid-api-rest-endpoints/src/main/java/ca/bc/gov/mof/wfpointid/PointIdServiceApplication.java
+++ server/wfss-pointid-api/wfss-pointid-api-rest-endpoints/src/main/java/ca/bc/gov/mof/wfpointid/PointIdServiceApplication.java
@@ -2,10 +2,14 @@ package ca.bc.gov.mof.wfpointid;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 
 import ca.bc.gov.mof.wfpointid.util.SSLDisabler;
 
@@ -14,14 +18,13 @@ import ca.bc.gov.mof.wfpointid.util.SSLDisabler;
 @ComponentScan
 public class PointIdServiceApplication {
 
-	public  static final String HTTPS_PROTOCOLS = "TLSv1,TLSv1.1,TLSv1.2";
-	
+	public static final String HTTPS_PROTOCOLS = "TLSv1,TLSv1.1,TLSv1.2";
+
 	private static Logger LOG = LoggerFactory.getLogger(PointIdServiceApplication.class);
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(PointIdServiceApplication.class, args);
 	}
-
 
 	public PointIdServiceApplication() {
 		LOG.info("==================  Point ID Service started  ======================");
@@ -29,9 +32,14 @@ public class PointIdServiceApplication {
 		disableSSL();
 	}
 
+	@Autowired
+	void configureObjectMapper(final ObjectMapper mapper) {
+		mapper.registerModule(new Jdk8Module());
+	}
+
 	/**
 	 * 
-	 * Disable SSL verification to allow contacting GeoServers which may not have 
+	 * Disable SSL verification to allow contacting GeoServers which may not have
 	 * accepted keycerts.
 	 * 
 	 * This should be safe, since the only access this service makes to them is:
@@ -46,5 +54,5 @@ public class PointIdServiceApplication {
 	private static void disableSSL() {
 		SSLDisabler.disableSSLViaTrustManager();
 	}
-	
-}  
+
+}
