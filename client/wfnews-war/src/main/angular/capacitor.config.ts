19b452cbdebd6e44ed4cd6dc2339d4314a53b2ea
Lucas Li <35748253+yzlucas@users.noreply.github.com>
2023-12-12 11:58:43 -0800

    Browser detect (#1481)
    * change app name

--- client/wfnews-war/src/main/angular/capacitor.config.ts
+++ client/wfnews-war/src/main/angular/capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'ca.bc.gov.WildfireInformation',
-  appName: 'WFNEWS',
+  appName: 'BC Wildfire',
   webDir: 'dist/wfnews',
   server: {
     androidScheme: 'https'
