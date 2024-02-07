56928390b819bb577dfd8f109dfceca599efdf07
Sean Sylver <ssylver@VividSolutions.com>
2024-02-07 09:48:42 -0800

    WFNEWS-1676 Add explicit geolocation check for firefox

--- client/wfnews-war/src/main/angular/src/app/services/common-utility.service.ts
+++ client/wfnews-war/src/main/angular/src/app/services/common-utility.service.ts
@@ -169,10 +169,19 @@ valueMatch = trimmedAddress.substring(0, valueLength);
     const timeoutPromise = new Promise<boolean>((resolve) => {
       setTimeout(() => resolve(false), timeoutDuration);
     });
-
-    const locationPromise = Geolocation.getCurrentPosition()
+    
+    let locationPromise = Geolocation.getCurrentPosition()
       .then(() => Promise.resolve(true))
       .catch(() => Promise.resolve(false));
+
+    // resolve for firefox
+    if (window?.navigator?.userAgent?.search("Firefox") && window?.navigator?.userAgent?.search("Firefox") > -1
+        && window?.navigator?.geolocation){
+      window.navigator.geolocation.getCurrentPosition(function(position) {
+        if(position) locationPromise = Promise.resolve(true);
+        else (locationPromise = Promise.resolve(true));
+      });
+    }
 
     return Promise.race([timeoutPromise, locationPromise]);
   }
