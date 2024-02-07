877455a61a4b39b4bd21dde4add9a5f457230d85
Sean Sylver <ssylver@VividSolutions.com>
2024-02-07 15:38:22 -0800

    WFNEWS-1927 CapacitorHttp for PDF download

--- client/wfnews-war/src/main/angular/src/app/components/public-incident-page/incident-maps-panel/incident-maps-panel.component.ts
+++ client/wfnews-war/src/main/angular/src/app/components/public-incident-page/incident-maps-panel/incident-maps-panel.component.ts
@@ -15,6 +15,8 @@ import {
 import { PublishedIncidentService } from '../../../services/published-incident-service';
 import { AppConfigService } from '@wf1/core-ui';
 import { ActivatedRoute, ParamMap } from '@angular/router';
+import { CapacitorHttp } from '@capacitor/core';
+import { CapacitorService } from '@app/services/capacitor-service';
 
 export class DownloadableMap {
   name: string;
@@ -42,20 +44,19 @@ export class IncidentMapsPanel implements OnInit {
     private appConfigService: AppConfigService,
     protected cdr: ChangeDetectorRef,
     private router: ActivatedRoute,
-  ) {}
+    private capacitorService: CapacitorService
+  ) { }
 
   ngOnInit() {
     const self = this;
     this.loadMaps().then((docs) => {
       self.maps = docs.map((doc) => ({
-          name: doc.attachmentTitle,
-          link: `${
-            this.appConfigService.getConfig().rest['wfnews']
-          }/publicPublishedIncidentAttachment/${
-            self.incident.incidentNumberLabel
+        name: doc.attachmentTitle,
+        link: `${this.appConfigService.getConfig().rest['wfnews']
+          }/publicPublishedIncidentAttachment/${self.incident.incidentNumberLabel
           }/attachments/${doc.attachmentGuid}/bytes`,
-          date: new Date(doc.createdTimestamp).toDateString(),
-        }));
+        date: new Date(doc.createdTimestamp).toDateString(),
+      }));
       this.cdr.detectChanges();
     });
 
@@ -102,14 +103,37 @@ export class IncidentMapsPanel implements OnInit {
   }
 
   downloadMap(mapLink, fileName) {
-    const url = mapLink;
-    const request = this.httpClient.request(
-      new HttpRequest('GET', url, {
-        reportProgress: true,
-        responseType: 'blob',
-      }),
-    );
+    const request = this.generateMapRequest(mapLink)
+    this.fetchMapResponse(request, fileName)
+  }
 
+  generateMapRequest(mapLink) {
+    const url = mapLink;
+    let isMobile = false;
+
+    this.capacitorService.isMobile.then(response => {
+      isMobile = response;
+    });
+
+    if (isMobile) {
+      const options = {
+        url,
+        params: null   
+      };
+      const resp = CapacitorHttp.request(options);
+      return resp;
+    } else {
+      const resp = this.httpClient.request(
+        new HttpRequest('GET', url, {
+          reportProgress: true,
+          responseType: 'blob',
+        }),
+      );
+      return resp;
+    }
+  }
+
+  fetchMapResponse(request, fileName) {
     request.subscribe(
       (ev) => {
         if (ev.type === HttpEventType.Sent) {
@@ -125,12 +149,12 @@ export class IncidentMapsPanel implements OnInit {
           });
         }
       },
-      (err) =>
+      (err) =>{
         this.snackbarService.open('PDF downloaded failed.', 'Close', {
           duration: 10000,
           panelClass: 'snackbar-error',
-        }),
-    );
+        })}
+      );
   }
 
   downloadFile(data: HttpResponse<Blob>, fileName: string) {
