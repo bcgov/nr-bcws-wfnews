a803456bf6b906d9e055a639405701150142fe54
Lucas Li <yzlucas@outlook.com>
2024-02-27 14:09:43 -0800

    exter external to download files

--- client/wfnews-war/src/main/angular/src/app/components/public-incident-page/incident-maps-panel/incident-maps-panel.component.ts
+++ client/wfnews-war/src/main/angular/src/app/components/public-incident-page/incident-maps-panel/incident-maps-panel.component.ts
@@ -181,7 +181,7 @@ export class IncidentMapsPanel implements OnInit {
       const download = await Filesystem.downloadFile({
         path: fileName,
         url: url,
-        directory: Directory.Documents,
+        directory: Directory.External,
       }).then(download => {
         if (download) {
           this.snackbarService.open('PDF downloaded successfully.', 'Close', {
