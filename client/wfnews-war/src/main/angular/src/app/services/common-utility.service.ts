7f34f70e988f64c1cae78cacdcc794a881f0398c
Lucas Li <35748253+yzlucas@users.noreply.github.com>
2024-03-05 09:07:33 -0800

    consider ipad mini is 768 px width (#1691)

--- client/wfnews-war/src/main/angular/src/app/services/common-utility.service.ts
+++ client/wfnews-war/src/main/angular/src/app/services/common-utility.service.ts
@@ -308,7 +308,7 @@ valueMatch = trimmedAddress.substring(0, valueLength);
 
   checkIfLandscapeMode() {
     // also return true if this is table portrait mode wfnews-2022. 
-    if ((window.innerWidth > window.innerHeight) || (window.innerWidth <= 1024 && window.innerWidth > 768 && window.innerHeight > window.innerWidth) ) {
+    if ((window.innerWidth > window.innerHeight) || (window.innerWidth <= 1024 && window.innerWidth >= 768 && window.innerHeight > window.innerWidth) ) {
       return true;
     } else {
       return false;
