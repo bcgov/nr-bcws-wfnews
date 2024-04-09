abf612c5f52ae56ffb99a91a8cb0d42f52e5d0fd
Sean Sylver <ssylver@VividSolutions.com>
2024-04-08 19:11:14 -0700

    Increase to 5 mins

--- client/wfnews-war/src/main/angular/src/app/components/report-of-fire/title-page/rof-title-page.component.ts
+++ client/wfnews-war/src/main/angular/src/app/components/report-of-fire/title-page/rof-title-page.component.ts
@@ -73,7 +73,7 @@ export class RoFTitlePage extends RoFPage implements OnInit, OnDestroy {
       const taskId = await BackgroundTask.beforeExit(async () => {
         const self = this;
 
-        this.intervalRef = timer(0, 10000).subscribe(() => {
+        this.intervalRef = timer(0, 50000).subscribe(() => {
           self.checkStoredRoF();
         });
 
