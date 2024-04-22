dad0060d2d8b08a6b8232759ebdfccaa6d4c59f7
Chris Preston <cpreston@vividsolutions.com>
2024-04-22 16:50:26 -0700

    Revert "Squashed commit of the following:"
    This reverts commit a2863517edde735044f50c809704f5237e81e900.

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -382,11 +382,7 @@ resource "aws_ecs_task_definition" "wfnews_client" {
         },
         {
           name  = "OPENMAPS_BASE_URL",
-          value = "https://${aws_route53_record.wfnews_openmaps_cache.name}"
-        },
-        {
-          name  = "SERVICES6_BASE_URL",
-          value = "https://${aws_route53_record.wfnews_services6_cache.name}"
+          value = var.openmapsBaseUrl
         },
         {
           name  = "SITEMINDER_URL_PREFIX",
