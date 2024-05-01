1ce58b01b271e843bc96bee4caf24d39c35a7028
ssylver93 <107515688+ssylver93@users.noreply.github.com>
2024-04-30 19:59:47 -0700

    Revert CorsFilter.java (#1868)
    * WFNEWS-2146 Set CORS headers explicitly
    
    * WFNEWS-2146 Allow wildcard in case of null headers
    
    * WFNEWS-2146 Revert CorsFilter change and add appropriate headers
    
    * Return headers from function
    
    * WFNEWS-2146 set all CORS headers explicitly
    
    * WFNEWS-2146 set all CORS headers explicitly
    
    * Remove unused import
    
    * Add brackets
    
    * Clean whitespace
    
    * WFNEWS-2146 Add cross origin methods to situation report endpoint
    
    * Set methods
    
    * Revert CorsFilter.java

--- server/wfnews-api/wfnews-api-rest-endpoints/src/main/java/ca/bc/gov/nrs/wfnews/api/rest/v1/spring/CorsFilter.java
+++ server/wfnews-api/wfnews-api-rest-endpoints/src/main/java/ca/bc/gov/nrs/wfnews/api/rest/v1/spring/CorsFilter.java
@@ -1,8 +1,6 @@
 package ca.bc.gov.nrs.wfnews.api.rest.v1.spring;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Enumeration;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -22,64 +20,29 @@ import org.springframework.core.Ordered;
 @Component
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class CorsFilter implements Filter {
-	private static final Logger logger = LoggerFactory.getLogger(CorsFilter.class);
+  private static final Logger logger = LoggerFactory.getLogger(CorsFilter.class);
 
-	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
-			throws IOException, ServletException {
-		HttpServletResponse response = (HttpServletResponse) res;
-		HttpServletRequest request = (HttpServletRequest) req;
-		response.setHeader("Access-Control-Allow-Credentials", "true");
-		response.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS, HEAD, PUT, POST, DELETE");
-		response.setHeader("Access-Control-Max-Age", "3600");
-		
-		// Set Access-Control-Allow-Headers explicitly
-		Enumeration<String> headerNames = request.getHeaderNames();
-		ArrayList<String> headersList = new ArrayList<String>();
-		
-		if (headerNames != null) {
-			while (headerNames.hasMoreElements()) {
-				headersList.add(headerNames.nextElement());
-				headersList.add("Content-type");
-				headersList.add("Authorization");
-			}
-		}
+  public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+    HttpServletResponse response = (HttpServletResponse) res;
+    HttpServletRequest request = (HttpServletRequest) req;
+    response.setHeader("Access-Control-Allow-Origin", "*");
+    response.setHeader("Access-Control-Allow-Credentials", "true");
+    response.setHeader("Access-Control-Allow-Methods", "*");
+    response.setHeader("Access-Control-Max-Age", "3600");
+    response.setHeader("Access-Control-Allow-Headers", "*");
 
-		if (!headersList.isEmpty()) {
-			String headers = String.join(", ", headersList);
-			if (headers != null) {
-				response.setHeader("Access-Control-Allow-Headers", headers);
-			}else response.setHeader("Access-Control-Allow-Headers", "*");
-		} else
-			response.setHeader("Access-Control-Allow-Headers", "*");
-			
-		// Set Access-Control-Allow-Origin explicitly	
-		String origin = null;
-		
-		if(request.getHeader("Origin") != null) {
-			origin = request.getHeader("Origin");
-		} else origin = request.getHeader("origin");
-		
-		if(origin != null) {
-			response.setHeader("Access-Control-Allow-Origin", origin);
-		} else response.setHeader("Access-Control-Allow-Origin", "*");
-		
+    if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+      response.setStatus(HttpServletResponse.SC_OK);
+    } else {
+      chain.doFilter(req, res);
+    }
+  }
 
-		if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
-			response.setStatus(HttpServletResponse.SC_OK);
-			response.setHeader("Access-Control-Allow-Credentials", "true");
-			response.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS, HEAD, PUT, POST, DELETE");
-			response.setHeader("Access-Control-Allow-Headers", "*");
-			response.setHeader("Access-Control-Allow-Origin", "*");
-		} else {
-			chain.doFilter(req, res);
-		}
-	}
+  public void init(FilterConfig filterConfig) {
+    logger.info("Initializing CORS filter");
+  }
 
-	public void init(FilterConfig filterConfig) {
-		logger.info("Initializing CORS filter");
-	}
-
-	public void destroy() {
-		logger.info("Destructing CORS filter");
-	}
+  public void destroy() {
+    logger.info("Destructing CORS filter");
+  }
 }
