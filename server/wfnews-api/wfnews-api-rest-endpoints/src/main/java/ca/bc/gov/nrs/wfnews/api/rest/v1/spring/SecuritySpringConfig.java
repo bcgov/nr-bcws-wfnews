4a5852d0a8075048e3beaec6b63ee963bf272b54
Sukhpal <6563909+sukhpalp@users.noreply.github.com>
2024-05-01 20:40:47 -0700

    Revert "Fix typos (#1880)"
    This reverts commit db0f540077b3e5a8ee9cce5eb8621d221201c1df.

--- server/wfnews-api/wfnews-api-rest-endpoints/src/main/java/ca/bc/gov/nrs/wfnews/api/rest/v1/spring/SecuritySpringConfig.java
+++ server/wfnews-api/wfnews-api-rest-endpoints/src/main/java/ca/bc/gov/nrs/wfnews/api/rest/v1/spring/SecuritySpringConfig.java
@@ -122,7 +122,7 @@ public class SecuritySpringConfig extends WebSecurityConfigurerAdapter  {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		
-		http.cors().and().csrf().disable()
+		http.cors().and().csrf().disable();
 		.oauth2ResourceServer(oauth2 -> oauth2
 			.authenticationManagerResolver(authenticationManagerResolver())
 		)
@@ -139,16 +139,15 @@ public class SecuritySpringConfig extends WebSecurityConfigurerAdapter  {
 
   @Bean
   public CorsConfigurationSource corsConfigurationSource() {
-	  final CorsConfiguration configuration = new CorsConfiguration();
+    final CorsConfiguration configuration = new CorsConfiguration();
 
-	  configuration.setAllowedOrigins(Collections.unmodifiableList(Arrays.asList("*")));
-	  configuration.setAllowedMethods(
-			  Collections.unmodifiableList(Arrays.asList("HEAD", "GET", "POST", "DELETE", "PUT", "OPTIONS")));
-	  configuration.setAllowedHeaders(Collections.unmodifiableList(Arrays.asList("apikey")));
+		configuration.setAllowedOrigins(Collections.unmodifiableList(Arrays.asList("*")));
+		configuration.setAllowedMethods(
+				Collections.unmodifiableList(Arrays.asList("HEAD", "GET", "POST", "DELETE", "PUT", "OPTIONS")));
+		configuration.setAllowedHeaders(Collections.unmodifiableList(Arrays.asList("apikey")));
 
-	  final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-	  source.registerCorsConfiguration("/**", configuration);
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
 
-	  return source;
-  }
+		return source;
 }
