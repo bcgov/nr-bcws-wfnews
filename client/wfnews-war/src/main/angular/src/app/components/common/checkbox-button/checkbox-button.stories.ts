6199dab7f50eba8472c742c16b6e4bc8635099cb
Sukhpal <6563909+sukhpalp@users.noreply.github.com>
2024-05-07 17:40:42 -0700

    Add stories

--- client/wfnews-war/src/main/angular/src/app/components/common/checkbox-button/checkbox-button.stories.ts
+++ client/wfnews-war/src/main/angular/src/app/components/common/checkbox-button/checkbox-button.stories.ts
@@ -1,17 +1,26 @@
-import type { Meta, StoryObj } from '@storybook/angular';
+import { componentWrapperDecorator, type Meta, type StoryObj } from '@storybook/angular';
 import { CheckboxButtonComponent } from './checkbox-button.component';
 
 const meta: Meta<CheckboxButtonComponent> = {
-  title: 'Common/CheckboxButtonComponent',
+  title: 'Common/CheckboxButton',
   component: CheckboxButtonComponent,
-  tags: ['autodocs'],
+  tags: ['autodocs'], 
 };
 
 export default meta;
 type Story = StoryObj<CheckboxButtonComponent>;
 
-export const orderNoCard: Story = {
+export const fullWidth: Story = {
   args: {
     checked: false,
   },
+};
+
+export const halfWidth: Story = {
+  args: {
+    checked: true,
+  },
+  decorators: [
+    componentWrapperDecorator((story) => `<div style="width: 50%;">${story}</div>`),
+  ],
 };
