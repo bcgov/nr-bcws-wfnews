d05f6954a42ddf5391db29dc834b66f58f1f231a
Sukhpal <6563909+sukhpalp@users.noreply.github.com>
2024-05-08 17:08:21 -0700

    Revert "Wfnews 1333 (#1888)"
    This reverts commit 1bb1155b64cfa26d9110fe666e47ece6161b619c.

--- client/wfnews-war/src/main/angular/src/app/store/wildfiresList/wildfiresList.effects.ts
+++ client/wfnews-war/src/main/angular/src/app/store/wildfiresList/wildfiresList.effects.ts
@@ -102,9 +102,6 @@ export class WildfiresListEffect {
             url = url.concat('&longitude=').concat(long.toString());
             url = url.concat('&radius=').concat((radius * 1000).toString());
           } else if (searchText) {
-            // "[" and "]" are invalid character and will cause java.lang.IllegalArgumentException
-            searchText = searchText.replace('[','');
-            searchText = searchText.replace(']','');
             url += `&searchText=${searchText}`;
           }
 
