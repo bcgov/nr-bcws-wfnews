f0b2810e37bafce24e64ce6ed1aa2519bcafeed0
Lucas Li <yzlucas@outlook.com>
2024-05-21 22:29:38 -0700

    move incident tabs to separate component

--- client/wfnews-war/src/main/angular/src/app/app.module.ts
+++ client/wfnews-war/src/main/angular/src/app/app.module.ts
@@ -245,6 +245,7 @@ import { NotificationSnackbarComponent } from '@app/components/notification-snac
 import { HTTP } from '@ionic-native/http/ngx';
 import { IonicStorageService } from './services/ionic-storage.service';
 import { PublicEventPageComponent } from './components/public-event-page/public-event-page.component';
+import { IncidentTabsComponent } from './components/public-incident-page/incident-tabs/incident-tabs.component';
 
 // Copied from im-external.module  TODO: consolidate in one place
 export const DATE_FORMATS = {
@@ -407,6 +408,7 @@ export const DATE_FORMATS = {
     BaseDialogComponent,
     NotificationSnackbarComponent,
     PublicEventPageComponent,
+    IncidentTabsComponent,
   ],
   imports: [
     MatSortModule,
