ff8615e127d98a5d1112ab4af34e6b88d6221aa7
Lucas Li <35748253+yzlucas@users.noreply.github.com>
2024-05-30 16:07:12 -0700

    Wfnews 2098 new (#1923)
    * 2182 2183 2185
    
    * wfnews-2098
    
    * switch evac layer to openmaps source
    
    * REVERT -switch evac layer to openmaps source
    
    * code review change
    
    * revert appconfig.json

--- client/wfnews-war/src/main/angular/src/app/components/public-event-page/public-event-page.component.ts
+++ client/wfnews-war/src/main/angular/src/app/components/public-event-page/public-event-page.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { AGOLService, AgolOptions } from '@app/services/AGOL-service';
 
 @Component({
   selector: 'wfnews-public-event-page',
   templateUrl: './public-event-page.component.html',
   styleUrls: ['./public-event-page.component.scss']
 })
-export class PublicEventPageComponent {
+export class PublicEventPageComponent implements OnInit {
+  public isLoading = true;
+  public loadingFailed = false;
+  public eventNumber: string;
+  public evac: string;
 
+  constructor(
+    private agolService: AGOLService,
+    private router: ActivatedRoute,
+  ) {}
+
+  ngOnInit(): void {
+    this.router.queryParams.subscribe((params: ParamMap) => {
+      if (params && params['eventNumber'] && 
+          (params['eventType'] === 'Order' || params['eventType'] === 'Alert')) {
+        this.eventNumber = params['eventNumber'];
+        this.populateEvacByID({
+          returnGeometry: true,
+          returnCentroid: true,
+          returnExtent: false,
+        });
+      }
+    });
+  }
+
+
+  async populateEvacByID(options: AgolOptions = null) {
+    this.agolService
+      .getEvacOrdersByEventNumber(
+        this.eventNumber,
+        options
+      )
+      .toPromise()
+      .then((response) => {
+        if (response?.features?.length > 0 && response?.features[0].geometry?.rings?.length > 0) {
+          this.evac = response.features[0];
+          this.isLoading = false;
+        }
+      });
+  }
 }
