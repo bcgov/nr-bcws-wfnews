a0c40414d191b0ed80788d294f67a74005f15f01
Lucas Li <yzlucas@outlook.com>
2024-05-29 22:23:10 -0700

    wfnews-2098

--- client/wfnews-war/src/main/angular/src/app/components/public-incident-page/incident-info-header/incident-header-panel.component.ts
+++ client/wfnews-war/src/main/angular/src/app/components/public-incident-page/incident-info-header/incident-header-panel.component.ts
@@ -5,7 +5,7 @@ import {
   AfterViewInit,
   HostListener,
   Output,
-  EventEmitter,
+  EventEmitter
 } from '@angular/core';
 import { EvacOrderOption } from '../../../conversion/models';
 import * as L from 'leaflet';
@@ -16,7 +16,8 @@ import {
   convertFireNumber,
   ResourcesRoutes,
   setDisplayColor,
-  getStageOfControlLabel
+  getStageOfControlLabel,
+  convertToDateTimeTimeZone
 } from '../../../utils';
 import * as moment from 'moment';
 import { MatDialog } from '@angular/material/dialog';
@@ -26,6 +27,8 @@ import { Location } from '@angular/common';
 import { LocationData } from '@app/components/wildfires-list-header/filter-by-location/filter-by-location-dialog.component';
 import { PublishedIncidentService } from '@app/services/published-incident-service';
 import { toCanvas } from 'qrcode';
+import { AGOLService } from '@app/services/AGOL-service';
+import { CommonUtilityService } from '@app/services/common-utility.service';
 
 
 @Component({
@@ -38,6 +41,7 @@ export class IncidentHeaderPanel implements AfterViewInit {
   @Input() public incident: any;
   @Input() public evacOrders: EvacOrderOption[] = [];
   @Input() public extent: any;
+  @Input() public evac: any;
   @Output() requestPrint = new EventEmitter<void>();
 
   public params: ParamMap;
@@ -45,11 +49,13 @@ export class IncidentHeaderPanel implements AfterViewInit {
 
   convertToFireCentreDescription = convertToFireCentreDescription;
   convertFireNumber = convertFireNumber;
+  convertToDateTimeTimeZone = convertToDateTimeTimeZone;
   getStageOfControlLabel = getStageOfControlLabel;
 
   private map: any;
   incidentEvacOrders = [];
   incidentEvacAlerts = [];
+  bounds = null;
 
   constructor(
     private appConfigService: AppConfigService,
@@ -59,6 +65,9 @@ export class IncidentHeaderPanel implements AfterViewInit {
     private location: Location,
     private publishedIncidentService: PublishedIncidentService,
     private route: ActivatedRoute,
+    private agolService: AGOLService,
+    private commonUtilityService: CommonUtilityService,
+
   ) {
     /* Empty, just here for injection */
   }
@@ -87,11 +96,36 @@ export class IncidentHeaderPanel implements AfterViewInit {
   }
 
   ngAfterViewInit(): void {
-    // Configure the map
-    const location = [
-      Number(this.incident.latitude),
-      Number(this.incident.longitude),
-    ];
+    this.initializeMap();
+  }
+
+  async initializeMap() {
+    let location;
+
+    if (this.incident) {
+      location = [
+        Number(this.incident.latitude),
+        Number(this.incident.longitude),
+      ];
+    } else if (this.evac) {
+      location = [
+        Number(this.evac.centroid?.y),
+        Number(this.evac.centroid?.x),
+      ];
+
+      const response = await this.agolService
+        .getEvacOrdersByEventNumber(this.params['eventNumber'], {
+          returnGeometry: true,
+        }).toPromise();
+
+      if (response?.features?.length > 0 && response?.features[0].geometry?.rings?.length > 0) {
+        const polygonData = this.commonUtilityService.extractPolygonData(response.features[0].geometry.rings);
+        if (polygonData?.length) {
+          this.bounds = this.commonUtilityService.getPolygonBond(polygonData);
+        }
+      }
+    }
+
     this.map = L.map('map', {
       attributionControl: false,
       zoomControl: false,
@@ -101,46 +135,56 @@ export class IncidentHeaderPanel implements AfterViewInit {
       trackResize: false,
       scrollWheelZoom: false,
     }).setView(location, 9);
-    if (!this.isMobileView()){
+
+    if (this.bounds) {
+      this.map.fitBounds(this.bounds);
+    }
+    if (!this.isMobileView()) {
       // only apply these in desktop
       L.control.zoom({
         position: 'topright'
       }).addTo(this.map);
     }
 
-    L.Control.ZoomToExtent = L.Control.extend({
-      onAdd: function(map) {
-        const container = L.DomUtil.create('div', 'leaflet-bar leaflet-control leaflet-control-custom');
-        const btn = L.DomUtil.create('button', '', container);
-        btn.innerHTML = `
-          <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 25 25" fill="none">
-            <path d="M16.586 15.6968C17.8234 14.2274 18.5013 12.3678 18.5 10.4468C18.5 8.81508 18.0161 7.22003 17.1096 5.86333C16.2031 4.50662 14.9146 3.4492 13.4071 2.82477C11.8997 2.20035 10.2409 2.03697 8.64051 2.3553C7.04017 2.67363 5.57016 3.45937 4.41637 4.61315C3.26259 5.76693 2.47685 7.23694 2.15853 8.83729C1.8402 10.4376 2.00357 12.0964 2.628 13.6039C3.25242 15.1114 4.30984 16.3999 5.66655 17.3064C7.02325 18.2129 8.61831 18.6968 10.25 18.6968C12.171 18.6981 14.0306 18.0201 15.5 16.7828L21.1895 22.4468L22.25 21.3863L16.586 15.6968ZM10.25 17.1968C8.91498 17.1968 7.60994 16.8009 6.4999 16.0592C5.38987 15.3175 4.52471 14.2633 4.01382 13.0299C3.50293 11.7965 3.36925 10.4393 3.6297 9.12992C3.89015 7.82055 4.53303 6.61781 5.47703 5.67381C6.42104 4.72981 7.62377 4.08693 8.93314 3.82648C10.2425 3.56603 11.5997 3.6997 12.8331 4.21059C14.0665 4.72149 15.1207 5.58665 15.8624 6.69668C16.6041 7.80671 17 9.11176 17 10.4468C16.998 12.2364 16.2862 13.9521 15.0208 15.2176C13.7553 16.483 12.0396 17.1948 10.25 17.1968Z" fill="#242424"/>
-            <path d="M8 9.69678H6.5V8.19678C6.5004 7.79907 6.65856 7.41777 6.93978 7.13656C7.221 6.85534 7.6023 6.69717 8 6.69678H9.5V8.19678H8V9.69678ZM14 9.69678H12.5V8.19678H11V6.69678H12.5C12.8977 6.69717 13.279 6.85534 13.5602 7.13656C13.8414 7.41777 13.9996 7.79907 14 8.19678V9.69678ZM9.5 14.1968H8C7.6023 14.1964 7.221 14.0382 6.93978 13.757C6.65856 13.4758 6.5004 13.0945 6.5 12.6968V11.1968H8V12.6968H9.5V14.1968ZM12.5 14.1968H11V12.6968H12.5V11.1968H14V12.6968C13.9996 13.0945 13.8414 13.4758 13.5602 13.757C13.279 14.0382 12.8977 14.1964 12.5 14.1968Z" fill="#242424"/>
-          </svg>
-        `;
-        btn.style.backgroundColor = 'white';
-        btn.style.width = '34px';
-        btn.style.height = '34px';
-        btn.style.cursor = 'pointer';
-        btn.style.border = '2px solid darkgrey';
-        btn.style.borderRadius = '4px';
-        btn.style.display = 'flex';
-        btn.style.alignItems = 'center';
-        btn.style.justifyContent = 'center';
-        btn.style.marginTop = '-3px';
-        btn.style.borderTopWidth = '1px';
-        btn.onclick = function() {
-          map.setZoom(9);
-        };
-  
-        return btn;
-      }
-    });
+    if (!this.isMobileView()) {
+      L.Control.ZoomToExtent = L.Control.extend({
+        onAdd: (map) => {
+          const container = L.DomUtil.create('div', 'leaflet-bar leaflet-control leaflet-control-custom');
+          const btn = L.DomUtil.create('button', '', container);
+          btn.innerHTML = `
+            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 25 25" fill="none">
+              <path d="M16.586 15.6968C17.8234 14.2274 18.5013 12.3678 18.5 10.4468C18.5 8.81508 18.0161 7.22003 17.1096 5.86333C16.2031 4.50662 14.9146 3.4492 13.4071 2.82477C11.8997 2.20035 10.2409 2.03697 8.64051 2.3553C7.04017 2.67363 5.57016 3.45937 4.41637 4.61315C3.26259 5.76693 2.47685 7.23694 2.15853 8.83729C1.8402 10.4376 2.00357 12.0964 2.628 13.6039C3.25242 15.1114 4.30984 16.3999 5.66655 17.3064C7.02325 18.2129 8.61831 18.6968 10.25 18.6968C12.171 18.6981 14.0306 18.0201 15.5 16.7828L21.1895 22.4468L22.25 21.3863L16.586 15.6968ZM10.25 17.1968C8.91498 17.1968 7.60994 16.8009 6.4999 16.0592C5.38987 15.3175 4.52471 14.2633 4.01382 13.0299C3.50293 11.7965 3.36925 10.4393 3.6297 9.12992C3.89015 7.82055 4.53303 6.61781 5.47703 5.67381C6.42104 4.72981 7.62377 4.08693 8.93314 3.82648C10.2425 3.56603 11.5997 3.6997 12.8331 4.21059C14.0665 4.72149 15.1207 5.58665 15.8624 6.69668C16.6041 7.80671 17 9.11176 17 10.4468C16.998 12.2364 16.2862 13.9521 15.0208 15.2176C13.7553 16.483 12.0396 17.1948 10.25 17.1968Z" fill="#242424"/>
+              <path d="M8 9.69678H6.5V8.19678C6.5004 7.79907 6.65856 7.41777 6.93978 7.13656C7.221 6.85534 7.6023 6.69717 8 6.69678H9.5V8.19678H8V9.69678ZM14 9.69678H12.5V8.19678H11V6.69678H12.5C12.8977 6.69717 13.279 6.85534 13.5602 7.13656C13.8414 7.41777 13.9996 7.79907 14 8.19678V9.69678ZM9.5 14.1968H8C7.6023 14.1964 7.221 14.0382 6.93978 13.757C6.65856 13.4758 6.5004 13.0945 6.5 12.6968V11.1968H8V12.6968H9.5V14.1968ZM12.5 14.1968H11V12.6968H12.5V11.1968H14V12.6968C13.9996 13.0945 13.8414 13.4758 13.5602 13.757C13.279 14.0382 12.8977 14.1964 12.5 14.1968Z" fill="#242424"/>
+            </svg>
+          `;
+          btn.style.backgroundColor = 'white';
+          btn.style.width = '34px';
+          btn.style.height = '34px';
+          btn.style.cursor = 'pointer';
+          btn.style.border = '2px solid darkgrey';
+          btn.style.borderRadius = '4px';
+          btn.style.display = 'flex';
+          btn.style.alignItems = 'center';
+          btn.style.justifyContent = 'center';
+          btn.style.marginTop = '-3px';
+          btn.style.borderTopWidth = '1px';
+          btn.onclick = () => {
+            if (this.bounds) {
+              this.map.fitBounds(this.bounds);
+            } else {
+              this.map.setZoom(9);
+            }
+          };
 
-    L.control.zoomToExtent = function(opts) {
-      return new L.Control.ZoomToExtent(opts);
+          return btn;
+        }
+      });
+
+      L.control.zoomToExtent = function (opts) {
+        return new L.Control.ZoomToExtent(opts);
+      }
+      L.control.zoomToExtent({ position: 'topright' }).addTo(this.map);
     }
-    L.control.zoomToExtent({ position: 'topright' }).addTo(this.map);
 
     // configure map data
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -278,9 +322,9 @@ export class IncidentHeaderPanel implements AfterViewInit {
       data: {
         fireCentre: convertToFireCentreDescription(
           this.incident.contactOrgUnitIdentifer ||
-            this.incident.fireCentreName ||
-            this.incident.fireCentreCode ||
-            this.incident.fireCentre,
+          this.incident.fireCentreName ||
+          this.incident.fireCentreCode ||
+          this.incident.fireCentre,
         ),
         email: this.incident.contactEmailAddress,
         phoneNumber: this.incident.contactPhoneNumber,
@@ -305,20 +349,20 @@ export class IncidentHeaderPanel implements AfterViewInit {
   back() {
     if (this.params && this.params['source'] && this.params['source'][0]) {
       if (this.params['source'] === 'map' || this.params['source'][0] === 'map') {
-this.backToMap();
-} else if (
+        this.backToMap();
+      } else if (
         this.params['source'][0] === 'full-details' &&
         this.params['sourceId'] &&
         this.params['sourceType']
       ) {
-this.router.navigate([ResourcesRoutes.FULL_DETAILS], {
+        this.router.navigate([ResourcesRoutes.FULL_DETAILS], {
           queryParams: {
             type: this.params['sourceType'],
             id: this.params['sourceId'],
             name: this.params['name']
           },
         });
-} else if (
+      } else if (
         this.params['source'] === 'saved-location' &&
         this.params['sourceName'] &&
         this.params['sourceLongitude'] &&
@@ -333,11 +377,11 @@ this.router.navigate([ResourcesRoutes.FULL_DETAILS], {
           },
         });
       } else {
-this.router.navigate([this.params['source']]);
-}
+        this.router.navigate([this.params['source']]);
+      }
     } else {
-this.router.navigate([ResourcesRoutes.DASHBOARD]);
-}
+      this.router.navigate([ResourcesRoutes.DASHBOARD]);
+    }
   }
 
   shareContent() {
@@ -404,14 +448,25 @@ this.router.navigate([ResourcesRoutes.DASHBOARD]);
     document.body.innerHTML = printContents;
 
     const canvas = document.getElementById('qr-code');
-    toCanvas(canvas, window.location.href, function(error) {
+    toCanvas(canvas, window.location.href, function (error) {
       if (error) {
-console.error(error);
-}
+        console.error(error);
+      }
       window.print();
       document.body.innerHTML = '';
       document.body.appendChild(appRoot);
     });
   }
 
+  navToMap() {
+    setTimeout(() => {
+      this.router.navigate([ResourcesRoutes.ACTIVEWILDFIREMAP], {
+        queryParams: {
+          longitude: this.evac.centroid.x,
+          latitude: this.evac.centroid.y,
+          evacuationAlert: true,
+        },
+      });
+    }, 200);
+  }
 }
