5530449ef2185e59c1c92b3645c08c84b4d206b3
Sukhpal <6563909+sukhpalp@users.noreply.github.com>
2024-06-03 01:58:00 -0700

    Implement WarningBanner

---
+++ client/wfnews-war/src/main/angular/src/app/components/common/warning-banner/warning-banner.stories.ts
@@ -0,0 +1,32 @@
+import type { Meta, StoryObj } from '@storybook/angular';
+import { WarningBannerComponent } from './warning-banner.component';
+
+const meta: Meta<WarningBannerComponent> = {
+  title: 'Banners/WarningBanner',
+  component: WarningBannerComponent,
+  tags: ['autodocs'],
+};
+
+export default meta;
+type Story = StoryObj<WarningBannerComponent>;
+
+export const defaultStyle: Story = {
+  args: {
+    label: 'Only perform the following tasks if there is time and it is safe to do so.',
+    iconPath: '/assets/images/svg-icons/red_warning.svg',
+  },
+};
+
+
+export const warningExample: Story = {
+  args: {
+    label: 'Only perform the following tasks if there is time and it is safe to do so.',
+    iconPath: '/assets/images/svg-icons/red_warning.svg',
+    style: {
+      backgroundColor: '#FEF1F2',
+      labelColor: '#242424',
+      iconColor: '#D8292F',
+      border: '1.5px solid #D8292F'
+    },
+  },
+};
