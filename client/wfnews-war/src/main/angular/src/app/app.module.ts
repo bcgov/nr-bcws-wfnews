d026fdbc1a6b471f2a8c75ec72ae3b7e76fba490
Sukhpal <6563909+sukhpalp@users.noreply.github.com>
2024-06-04 16:15:23 -0700

    Implement EvacAlertDetails

--- client/wfnews-war/src/main/angular/src/app/app.module.ts
+++ client/wfnews-war/src/main/angular/src/app/app.module.ts
@@ -201,10 +201,7 @@ import { AreaRestrictionDetailsComponent } from './components/public-event-page/
 import { EvacAlertDetailsComponent } from './components/public-event-page/evac-alert-details/evac-alert-details.component';
 import { EvacOrderDetailsComponent } from './components/public-event-page/evac-order-details/evac-order-details.component';
 import { FireBanDetailsComponent } from './components/public-event-page/fire-ban-details/fire-ban-details.component';
-import { FireDangerDetailsComponent } from './components/public-event-page/fire-danger-details/fire-danger-details.component';
 import { PublicEventPageComponent } from './components/public-event-page/public-event-page.component';
-import { ImagePanelComponent } from './components/public-incident-page/incident-gallery-panel/image-panel/image-panel.component';
-import { IncidentGalleryAllMediaMobileComponent } from './components/public-incident-page/incident-gallery-panel/incident-gallery-all-media-mobile/incident-gallery-all-media-mobile.component';
 import { IncidentGalleryImagesMobileComponent } from './components/public-incident-page/incident-gallery-panel/incident-gallery-images-mobile/incident-gallery-images-mobile.component';
 import { IncidentGalleryPanelMobileComponent } from './components/public-incident-page/incident-gallery-panel/incident-gallery-panel-mobile/incident-gallery-panel-mobile.component';
 import { IncidentGalleryPanel } from './components/public-incident-page/incident-gallery-panel/incident-gallery-panel.component';
