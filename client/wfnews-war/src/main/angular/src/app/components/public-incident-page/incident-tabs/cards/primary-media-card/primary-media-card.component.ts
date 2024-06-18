c3a70c3ed959e14fbc5a2d25d6c0e7e59d615ca8
Sukhpal <6563909+sukhpalp@users.noreply.github.com>
2024-06-18 12:18:48 -0700

    Use components in details page

--- client/wfnews-war/src/main/angular/src/app/components/public-incident-page/incident-tabs/cards/primary-media-card/primary-media-card.component.ts
+++ client/wfnews-war/src/main/angular/src/app/components/public-incident-page/incident-tabs/cards/primary-media-card/primary-media-card.component.ts
@@ -1,11 +1,6 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { SafeResourceUrl } from '@angular/platform-browser';
 import { YouTubeService } from '@app/services/youtube-service';
-import { InitDetail } from 'lightgallery/lg-events';
-import { LightGallery } from 'lightgallery/lightgallery';
-import lgFullscreen from 'lightgallery/plugins/fullscreen';
-import lgThumbnail from 'lightgallery/plugins/thumbnail';
-import lgZoom from 'lightgallery/plugins/zoom';
 
 @Component({
   selector: 'primary-media-card',
@@ -18,25 +13,7 @@ export class PrimaryMediaCardComponent {
   @Output() openAllPhotosClicked = new EventEmitter<void>();
   @Output() imageError = new EventEmitter<string>();
 
-  index = 0;
-
-  settings = {
-    counter: true,
-    plugins: [lgZoom, lgFullscreen, lgThumbnail],
-    download: true,
-    fullScreen: true,
-    actualSize: true,
-    thumbnail: true,
-    showZoomInOutIcons: true,
-  };
-
-  private lightGallery!: LightGallery;
-
   constructor(private youTubeService: YouTubeService) {}
-
-  onInit = (detail: InitDetail): void => {
-    this.lightGallery = detail.instance;
-  };
 
   handleImageError = () => {
     this.imageError.emit(this.item.href);
@@ -46,8 +23,11 @@ export class PrimaryMediaCardComponent {
     this.openAllPhotosClicked.emit();
   };
 
-  bypassUrlSecurity(url: string): SafeResourceUrl {
-    return this.youTubeService.sanitizeYoutubeUrl(url);
+  bypassUrlSecurity(url): SafeResourceUrl {
+    if (typeof url === 'string') {
+      return this.youTubeService.sanitizeYoutubeUrl(url);
+    }
+    return url;
   }
 }
 
