be8b243a08bbb0e5e984b6192058e5a19dcaf9a2
Lucas Li <yzlucas@outlook.com>
2024-06-19 19:11:54 -0700

    update code review

--- client/wfnews-war/src/main/angular/src/app/components/public-incident-page/incident-tabs/cards/response-update-card/response-update-card.component.ts
+++ client/wfnews-war/src/main/angular/src/app/components/public-incident-page/incident-tabs/cards/response-update-card/response-update-card.component.ts
@@ -1,11 +1,11 @@
-import {AfterViewInit, ChangeDetectorRef, Component, ElementRef, Input, ViewChild } from '@angular/core';
+import {AfterContentChecked, ChangeDetectorRef, Component, ElementRef, Input, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'response-update-card',
   templateUrl: './response-update-card.component.html',
   styleUrls: ['./response-update-card.component.scss']
 })
-export class ResponseUpdateCardComponent implements AfterViewInit {
+export class ResponseUpdateCardComponent implements AfterContentChecked {
   @Input() updateDate?: string;
 
   @ViewChild('responseUpdateContent') contentElementRef: ElementRef;
@@ -14,13 +14,16 @@ export class ResponseUpdateCardComponent implements AfterViewInit {
 
   columnTrigger = 2;
   columnSize = 24;
+  heightThreshold = 200;
+
 
   constructor(private cdr: ChangeDetectorRef) { }
 
-  ngAfterViewInit() {
+  ngAfterContentChecked() {
     const size = this.contentElementRef?.nativeElement?.offsetHeight;
-    this.useColumns = size > (this.columnTrigger * this.columnSize);
-    if (this.useColumns) {
+    const previousUseColumns = this.useColumns;
+    this.useColumns = size > this.heightThreshold;
+    if (this.useColumns !== previousUseColumns ) {
       this.cdr.detectChanges();
     }
   }  
