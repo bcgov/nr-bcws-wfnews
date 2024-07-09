99da6cc02a367ec0b4dedc0943e1577fc2666c8f
Sukh <6563909+sukhpalp@users.noreply.github.com>
2024-07-09 13:07:20 -0700

    Remove line breaks (#2007)
    * Remove line breaks
    
    * Fix zoom to extent icon size iOS

--- client/wfnews-war/src/main/angular/src/app/components/public-incident-page/incident-info-header/danger-rating-header/danger-rating-header.component.ts
+++ client/wfnews-war/src/main/angular/src/app/components/public-incident-page/incident-info-header/danger-rating-header/danger-rating-header.component.ts
@@ -9,13 +9,13 @@ import { ResourcesRoutes, convertToDateTimeTimeZone } from '@app/utils';
   encapsulation: ViewEncapsulation.None
 })
 export class DangerRatingHeaderComponent {
-  convertToDateTimeTimeZone = convertToDateTimeTimeZone;
+  @Input() dangerRating: any;
 
+  convertToDateTimeTimeZone = convertToDateTimeTimeZone;
+  
   constructor(
     private router: Router,
   ) {}
-
-  @Input() dangerRating: any;
 
   navToMap() {
     setTimeout(() => {
@@ -34,13 +34,15 @@ export class DangerRatingHeaderComponent {
       case 'Very Low':
         return 'Dry forest fuels are at a very low risk of catching fire.';
       case 'Low':
-        return 'Fires may start easily and spread quickly but there will be minimal involvement <br>of deeper fuel layers or larger fuels.';
+        return 'Fires may start easily and spread quickly but there will be minimal involvement of deeper fuel layers or larger fuels.';
       case 'Moderate':
-        return 'Forest fuels are drying and there is an increased risk of surface fires starting.<br>  Carry out any forest activities with caution.';
+        // eslint-disable-next-line max-len
+        return 'Forest fuels are drying and there is an increased risk of surface fires starting. Carry out any forest activities with caution.';
       case 'High':
-        return 'Forest fuels are very dry and the fire risk is serious. <br> Extreme caution must be used in any forest activities.';
+        return 'Forest fuels are very dry and the fire risk is serious. Extreme caution must be used in any forest activities.';
       case 'Extreme':
-        return 'Extremely dry forest fuels and the fire risk is very serious.<br> New fires will start easily, spread rapidly, and challenge fire suppression efforts.';
+        // eslint-disable-next-line max-len
+        return 'Extremely dry forest fuels and the fire risk is very serious. New fires will start easily, spread rapidly, and challenge fire suppression efforts.';
     }
   }
 
