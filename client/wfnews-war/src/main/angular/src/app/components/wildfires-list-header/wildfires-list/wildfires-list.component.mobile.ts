479ad4d378e509a2eab952c960a5174dd6c9a29a
Lucas Li <yzlucas@outlook.com>
2024-07-10 11:16:42 -0700

    wfnews-2303

--- client/wfnews-war/src/main/angular/src/app/components/wildfires-list-header/wildfires-list/wildfires-list.component.mobile.ts
+++ client/wfnews-war/src/main/angular/src/app/components/wildfires-list-header/wildfires-list/wildfires-list.component.mobile.ts
@@ -12,7 +12,7 @@ import {
   ResourcesRoutes,
   convertFireNumber,
   convertToStageOfControlDescription,
-  convertToDateTime
+  convertToDateYear
 } from '@app/utils';
 import moment from 'moment';
 import { Observable } from 'rxjs';
@@ -49,7 +49,7 @@ export class WildFiresListComponentMobile {
 
   convertFireNumber = convertFireNumber;
   convertToStageOfControlDescription = convertToStageOfControlDescription;
-  convertToDateTime = convertToDateTime;
+  convertToDateYear = convertToDateYear;
 
   private searchTimer;
 
@@ -103,7 +103,7 @@ export class WildFiresListComponentMobile {
                 traditionalTerritoryDetail: element.traditionalTerritoryDetail,
                 fireOfNoteInd: element.fireOfNoteInd,
                 fireCentreName: element.fireCentreName,
-                discoveryDate: this.convertToDateTime(element.discoveryDate),
+                discoveryDate: this.convertToDateYear(element.discoveryDate),
                 fireYear: element.fireYear,
               });
 
