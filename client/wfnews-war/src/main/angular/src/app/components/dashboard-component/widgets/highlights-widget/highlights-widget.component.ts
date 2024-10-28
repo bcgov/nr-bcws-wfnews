c6b02d5c6988a0340f2636265966115216bd7459
Sean Sylver <ssylver@VividSolutions.com>
2024-10-28 11:24:37 -0700

    WFNEWS-2411 Use CapacitorHttp on mobile to avoid CORS issues in highlights widget

--- client/wfnews-war/src/main/angular/src/app/components/dashboard-component/widgets/highlights-widget/highlights-widget.component.ts
+++ client/wfnews-war/src/main/angular/src/app/components/dashboard-component/widgets/highlights-widget/highlights-widget.component.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { readableDate } from '@app/utils';
 import { Capacitor, CapacitorHttp } from '@capacitor/core';
