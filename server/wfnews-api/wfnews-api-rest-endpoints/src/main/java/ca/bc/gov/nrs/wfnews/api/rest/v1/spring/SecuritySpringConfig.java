a05ecc8efba8f4257293d67188f24b242f0721f8
Chris Preston <cpreston@vividsolutions.com>
2025-12-16 15:29:50 -0800

    Update CORS config, remove unneeded access logging

--- server/wfnews-api/wfnews-api-rest-endpoints/src/main/java/ca/bc/gov/nrs/wfnews/api/rest/v1/spring/SecuritySpringConfig.java
+++ server/wfnews-api/wfnews-api-rest-endpoints/src/main/java/ca/bc/gov/nrs/wfnews/api/rest/v1/spring/SecuritySpringConfig.java
@@ -140,7 +140,7 @@ public class SecuritySpringConfig extends WebSecurityConfigurerAdapter  {
   public CorsConfigurationSource corsConfigurationSource() {
     final CorsConfiguration configuration = new CorsConfiguration();
 
-    configuration.setAllowedOrigins(Collections.unmodifiableList(Arrays.asList("*")));
+    configuration.setAllowedOriginPatterns(Collections.unmodifiableList(Arrays.asList("*")));
     configuration.setAllowedMethods(Collections.unmodifiableList(Arrays.asList("HEAD", "GET", "POST", "DELETE", "PUT", "OPTIONS")));
     configuration.setAllowCredentials(true);
     configuration.setAllowedHeaders(Collections.unmodifiableList(Arrays.asList("*")));
