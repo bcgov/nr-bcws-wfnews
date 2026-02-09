6e123ca9d6511a00f90f7b61da8d835e4096c223
Sukhpal <6563909+sukhpalp@users.noreply.github.com>
2026-02-09 12:54:43 -0800

    Stop sanitization on change detection cycles

--- client/wfnews-war/src/main/angular/src/app/components/public-incident-page/incident-gallery-panel/video-panel/video-panel.component.ts
+++ client/wfnews-war/src/main/angular/src/app/components/public-incident-page/incident-gallery-panel/video-panel/video-panel.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { SafeResourceUrl } from '@angular/platform-browser';
 import { YouTubeService } from '@app/services/youtube-service';
 import { convertToMobileFormat, convertToYoutubeId } from '../../../../utils';
 
@@ -7,14 +8,27 @@ import { convertToMobileFormat, convertToYoutubeId } from '../../../../utils';
   templateUrl: './video-panel.component.html',
   styleUrls: ['./video-panel.component.scss'],
 })
-export class VideoPanelComponent {
+export class VideoPanelComponent implements OnInit, OnChanges {
   @Input() public item;
   convertToMobileFormat = convertToMobileFormat;
   convertToYoutubeId = convertToYoutubeId;
+  safeUrl: SafeResourceUrl;
 
   constructor(private youtubeService: YouTubeService) { }
 
-  getSafeUrl(url: string) {
-    return this.youtubeService.sanitizeYoutubeUrl(url);
+  ngOnInit(): void {
+    this.sanitize();
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes && changes.item) {
+      this.sanitize();
+    }
+  }
+
+  sanitize() {
+    if (this.item && this.item.href) {
+      this.safeUrl = this.youtubeService.sanitizeYoutubeUrl(this.item.href);
+    }
   }
 }
