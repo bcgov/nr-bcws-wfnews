1465e2155182fbeae63e64e290a004e099fd164b
Sukhpal <6563909+sukhpalp@users.noreply.github.com>
2026-02-10 13:27:22 -0800

    Use incident guid to find incident for externalUri update

--- server/wfnews-api/wfnews-api-rest-endpoints/src/main/java/ca/bc/gov/nrs/wfnews/api/rest/v1/endpoints/impl/ExternalUriEndpointImpl.java
+++ server/wfnews-api/wfnews-api-rest-endpoints/src/main/java/ca/bc/gov/nrs/wfnews/api/rest/v1/endpoints/impl/ExternalUriEndpointImpl.java
@@ -13,91 +13,92 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import ca.bc.gov.nrs.common.persistence.dao.DaoException;
-import ca.bc.gov.nrs.wfone.common.rest.endpoints.BaseEndpointsImpl;
 import ca.bc.gov.nrs.common.rest.resource.Messages;
 import ca.bc.gov.nrs.common.service.ConflictException;
 import ca.bc.gov.nrs.common.service.ForbiddenException;
 import ca.bc.gov.nrs.common.service.NotFoundException;
 import ca.bc.gov.nrs.common.service.ValidationFailureException;
-import ca.bc.gov.nrs.wfnews.service.api.v1.IncidentsService;
 import ca.bc.gov.nrs.wfnews.api.rest.v1.endpoints.ExternalUriEndpoint;
 import ca.bc.gov.nrs.wfnews.api.rest.v1.endpoints.security.Scopes;
 import ca.bc.gov.nrs.wfnews.api.rest.v1.resource.ExternalUriResource;
 import ca.bc.gov.nrs.wfnews.api.rest.v1.resource.PublishedIncidentResource;
+import ca.bc.gov.nrs.wfnews.service.api.v1.IncidentsService;
+import ca.bc.gov.nrs.wfone.common.rest.endpoints.BaseEndpointsImpl;
 
 public class ExternalUriEndpointImpl extends BaseEndpointsImpl implements ExternalUriEndpoint {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(ExternalUriEndpointImpl.class);
-	
+
 	@Autowired
 	private IncidentsService incidentsService;
-	
+
 	@Override
 	public Response createExternalUri(ExternalUriResource resource)
 			throws NotFoundException, ForbiddenException, ConflictException {
 		Response response = null;
-		
+
 		logRequest();
-		
-		if(!hasAuthority(Scopes.CREATE_EXTERNAL_URI)) {
+
+		if (!hasAuthority(Scopes.CREATE_EXTERNAL_URI)) {
 			return Response.status(Status.FORBIDDEN).build();
 		}
-		
+
 		try {
 			ExternalUriResource result = incidentsService.createExternalUri(resource, getFactoryContext());
-			
+
 			URI createdUri = URI.create(result.getSelfLink());
-			
+
 			response = Response.created(createdUri).entity(result).tag(result.getUnquotedETag()).build();
-			
+
 			// Now we should also update the Incident
 			// This will ensure we fetch this on update checks
-			PublishedIncidentResource incident = incidentsService.getPublishedIncident(result.getSourceObjectUniqueId(), null, getWebAdeAuthentication(), getFactoryContext());
+			PublishedIncidentResource incident = incidentsService.getPublishedIncidentByIncidentGuid(
+					result.getSourceObjectUniqueId(), getWebAdeAuthentication(), getFactoryContext());
 			incident.setUpdateDate(new Date());
 			incident.setLastUpdatedTimestamp(new Date());
 			incidentsService.updatePublishedWildfireIncident(incident, getFactoryContext());
-		} catch(ValidationFailureException e) {
+		} catch (ValidationFailureException e) {
 			response = Response.status(Status.BAD_REQUEST).entity(new Messages(e.getValidationErrors())).build();
 		} catch (Throwable t) {
 			response = getInternalServerErrorResponse(t);
 		}
-		
+
 		logResponse(response);
 
 		return response;
 	}
 
-
 	@Override
 	public Response updateExternalUri(ExternalUriResource resource, String externalResourceGuid)
 			throws NotFoundException, ForbiddenException, ConflictException {
 		Response response = null;
-		
+
 		logRequest();
-		
-		if(!hasAuthority(Scopes.UPDATE_EXTERNAL_URI)) {
+
+		if (!hasAuthority(Scopes.UPDATE_EXTERNAL_URI)) {
 			return Response.status(Status.FORBIDDEN).build();
 		}
-		
+
 		try {
 			ExternalUriResource result = incidentsService.updateExternalUri(resource, getFactoryContext());
-			
+
 			URI createdUri = URI.create(result.getSelfLink());
-			
+
 			response = Response.created(createdUri).entity(result).tag(result.getUnquotedETag()).build();
-			
+
 			// Now we should also update the Incident
 			// This will ensure we fetch this on update checks
-			PublishedIncidentResource incident = incidentsService.getPublishedIncident(result.getSourceObjectUniqueId(), null, getWebAdeAuthentication(), getFactoryContext());
+			PublishedIncidentResource incident = incidentsService.getPublishedIncidentByIncidentGuid(
+					result.getSourceObjectUniqueId(), getWebAdeAuthentication(), getFactoryContext());
 			incident.setUpdateDate(new Date());
 			incident.setLastUpdatedTimestamp(new Date());
 			incidentsService.updatePublishedWildfireIncident(incident, getFactoryContext());
-		} catch(ValidationFailureException e) {
+		} catch (ValidationFailureException e) {
 			response = Response.status(Status.BAD_REQUEST).entity(new Messages(e.getValidationErrors())).build();
 		} catch (Throwable t) {
 			response = getInternalServerErrorResponse(t);
 		}
-		
+
 		logResponse(response);
 
 		return response;
@@ -106,17 +107,18 @@ public class ExternalUriEndpointImpl extends BaseEndpointsImpl implements Extern
 	@Override
 	public Response deleteExternalUri(String externalUriGuid) throws NotFoundException, ConflictException, DaoException {
 		Response response = null;
-		
+
 		logRequest();
-		
-		if(!hasAuthority(Scopes.DELETE_EXTERNAL_URI)) {
+
+		if (!hasAuthority(Scopes.DELETE_EXTERNAL_URI)) {
 			return Response.status(Status.FORBIDDEN).build();
 		}
-		
-		try{
-			ExternalUriResource current = incidentsService.getExternalUri(externalUriGuid, getWebAdeAuthentication(), getFactoryContext());
+
+		try {
+			ExternalUriResource current = incidentsService.getExternalUri(externalUriGuid, getWebAdeAuthentication(),
+					getFactoryContext());
 			EntityTag currentTag = EntityTag.valueOf(current.getQuotedETag());
-			
+
 			ResponseBuilder responseBuilder = this.evaluatePreconditions(currentTag);
 
 			if (responseBuilder == null) {
@@ -136,13 +138,12 @@ public class ExternalUriEndpointImpl extends BaseEndpointsImpl implements Extern
 		} catch (Throwable t) {
 			response = getInternalServerErrorResponse(t);
 		}
-		
+
 		logResponse(response);
 
 		logger.debug(">deletePublishedIncident " + response.getStatus());
-		
+
 		return response;
 	}
-	
-	
+
 }
