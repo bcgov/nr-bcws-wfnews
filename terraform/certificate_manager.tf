f7578be274e4c409d55f6cbf4fd3508f065381a4
Chris Preston <cpreston@vividsolutions.com>
2026-05-05 11:04:36 -0700

    Add prod-only resources, ability to import

--- terraform/certificate_manager.tf
+++ terraform/certificate_manager.tf
@@ -62,3 +62,41 @@ resource "aws_acm_certificate_validation" "wfnews_ca_certificate_validation" {
     create = "15m"
   }
 }
+
+
+
+//Prod-only legacy certificate, wfnews-prod
+resource "aws_acm_certificate" "wfnews_legacy_us_certificate" {
+  count = var.target_env == "prod" ? 1 : 0
+  domain_name = "wfnews-prod.bcwildfireservices.com"
+  validation_method = "DNS"
+  provider = aws.aws-us
+}
+
+resource "aws_route53_record" "wfnews_legacy_us_certificate_validation" {
+  count = var.target_env == "prod" ? 1 : 0
+  for_each = {
+    for dvo in aws_acm_certificate.wfnews_legacy_us_certificate[0].domain_validation_options: dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.zone.zone_id
+}
+
+resource "aws_acm_certificate_validation" "wfnews_legacy_us_certificate_validation" {
+  count = var.target_env == "prod" ? 1 : 0
+  certificate_arn = aws_acm_certificate.wfnews_legacy_us_certificate[0].arn
+  validation_record_fqdns = [ for record in aws_route53_record.wfnews_us_certificate_validation[0] : record.fqdn]
+  provider = aws.aws-us
+  timeouts {
+    create = "15m"
+  }
+}
--- terraform/route53.tf
+++ terraform/route53.tf
@@ -14,3 +14,20 @@ resource "aws_route53_record" "wfnews_record" {
     evaluate_target_health = true
   }
 }
+
+
+data "aws_route53_zone" "legacy_zone" {
+  count = var.target_env == "prod" ? 1 : 0
+  name = "prod.bcwildfireservices.com"
+}
+
+resource "aws_route53_record" "wfnews_record" {
+  zone_id = data.aws_route53_zone.legacy_zone.id
+  name    = data.aws_route53_zone.legacy_zone.name
+  type    = "A"
+  alias {
+    name                   = aws_cloudfront_distribution.wfnews_legacy_nginx.domain_name
+    zone_id                = aws_cloudfront_distribution.wfnews_legacy_nginx.hosted_zone_id
+    evaluate_target_health = true
+  }
+}
