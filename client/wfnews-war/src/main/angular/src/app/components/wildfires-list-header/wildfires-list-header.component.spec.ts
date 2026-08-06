09560dd968e5564570a75538bc01a062e830f4aa
Sukhpal <6563909+sukhpalp@users.noreply.github.com>
2026-08-06 14:09:00 -0700

    Capacitor update

--- client/wfnews-war/src/main/angular/src/app/components/wildfires-list-header/wildfires-list-header.component.spec.ts
+++ client/wfnews-war/src/main/angular/src/app/components/wildfires-list-header/wildfires-list-header.component.spec.ts
@@ -1,4 +1,11 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
+import { WFMapService } from '@app/services/wf-map.service';
 
 import { WildfiresListHeaderComponent } from './wildfires-list-header.component';
 
@@ -8,7 +15,18 @@ describe('WildfiresListHeaderComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      // AppConfigService (@wf1/core-ui) resolves HttpBackend from the injector;
+      // the rest cover the component's own constructor dependencies.
+      imports: [
+        HttpClientTestingModule,
+        RouterTestingModule,
+        MatDialogModule,
+        MatIconModule,
+        NoopAnimationsModule,
+      ],
       declarations: [WildfiresListHeaderComponent],
+      providers: [{ provide: WFMapService, useValue: {} }],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
     }).compileComponents();
   });
 
